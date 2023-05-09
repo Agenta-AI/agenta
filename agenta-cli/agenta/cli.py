bc619574f073eedfe629a7d18bd8e2f29504e7de
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-05-09 20:52:33 +0200

    init agenta cli

---
+++ agenta-cli/agenta/cli.py
@@ -0,0 +1,39 @@
+import click
+import os
+from pathlib import Path
+
+from .docker_utils import build_and_upload_docker_image
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+@click.argument('folder')
+def up(folder):
+    """Build and upload Docker image to the Agenta registry."""
+    build_and_upload_docker_image(folder)
+
+
+@click.command()
+def init():
+    """Initialize a new Agenta project with the template files."""
+    # Copy the template files to the current directory
+    current_dir = Path.cwd()
+    template_dir = Path(__file__).parent / "templates"
+
+    for file in template_dir.glob("*"):
+        dest = current_dir / file.name
+        if not dest.exists():
+            with dest.open("w") as dest_file, file.open("r") as src_file:
+                dest_file.write(src_file.read())
+
+
+# Add the commands to the CLI group
+cli.add_command(up)
+cli.add_command(init)
+
+if __name__ == '__main__':
+    cli()
---
+++ agenta-cli/agenta/docker/docker_utils.py
@@ -0,0 +1,51 @@
+import docker
+import os
+import shutil
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from pathlib import Path
+import shutil
+
+
+def create_dockerfile(out_folder: Path):
+    """Creates a dockerfile based on the template in the out_folder.
+
+    Arguments:
+        out_folder -- Folder in which to create the Dockerfile.
+    """
+    assert Path(out_folder).exists(), f"Folder {out_folder} does not exist."
+    dockerfile_template = Path(__file__).parent / "Dockerfile.template"
+    dockerfile_path = out_folder / "Dockerfile"
+    shutil.copy(dockerfile_template, dockerfile_path)
+
+
+def build_and_upload_docker_image(folder):
+    # Initialize Docker client
+    client = docker.from_env()
+
+    with TemporaryDirectory() as temp_dir:
+        # Create a Dockerfile for the project
+        dockerfile_path = create_dockerfile(folder)
+
+        # Copy the project files to a temporary directory
+        shutil.copytree(folder, temp_dir, dirs_exist_ok=True)
+
+        # Build the Docker image
+        print("Building Docker image...")
+        image, build_log = client.images.build(
+            path=temp_dir,
+            tag=f"agenta/{folder}:latest",
+            rm=True  # Remove intermediate containers after a successful build
+        )
+
+        # Print the build log
+        for line in build_log:
+            print(line)
+
+        # Upload the Docker image to the Agenta registry
+        print("Uploading Docker image...")
+        client.images.push(repository=f"agenta/{folder}", tag="latest")
+        print("Docker image uploaded successfully.")
+
+        # Clean up the temporary Dockerfile
+        dockerfile_path.unlink()
