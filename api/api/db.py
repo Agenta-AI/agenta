525f74074cbc371363a9de9796b1dc18e2a7ef3e
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-05-12 13:30:02 +0200

    cleanup

--- api/api/db.py
+++
@@ -1,8 +0,0 @@
-from mongoengine import connect
-import os
-
-
-def connect_to_db():
-    mongo_uri = os.environ.get(
-        "MONGODB_URI", "mongodb://username:password@mongo:27017")
-    connect(host=mongo_uri, alias="default")
