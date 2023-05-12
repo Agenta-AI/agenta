035118894d28d4967c9792ee25e936ef834c0886
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-05-12 13:21:50 +0200

    simplified into one route for everything

--- agenta-deploy-server/deploy_server/main.py
+++ agenta-deploy-server/deploy_server/main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
-from deploy_server.routers import models, containers
+from deploy_server.routers import app_version
 from deploy_server.services.docker_registry import start_registry
 
 app = FastAPI()
 
-app.include_router(models.router, prefix='/models', tags=['Models'])
-# app.include_router(containers.router, prefix='/containers',
-#                    tags=['Containers'])
+app.include_router(app_version, prefix='/app_version')
 
 
 @app.on_event("startup")
