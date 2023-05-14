19c142d7d87c06a864dd456c0a863b6cb2262ea8
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-05-14 13:01:54 +0200

    minot

--- agenta-deploy-server/tests/manual_tests.py
+++ agenta-deploy-server/tests/manual_tests.py
@@ -1,7 +1,7 @@
 import os
 import docker
 from deploy_server.config import settings
-from deploy_server.services.db_manager import get_session
+from deploy_server.services.db_manager import get_session, list_app_versions
 
 client = docker.from_env()
 
@@ -10,5 +10,11 @@ def test_get_session():
     assert get_session() is not None
 
 
+def test_list():
+    print(list_app_versions())
+    assert list_app_versions() == []
+
+
 if __name__ == "__main__":
     test_get_session()
+    test_list()
