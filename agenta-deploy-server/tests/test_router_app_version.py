48d65c88b3999a173f61537b4a9c8bc28c89b805
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-05-14 20:14:44 +0200

    updated api and tests

--- agenta-deploy-server/tests/test_router_app_version.py
+++ agenta-deploy-server/tests/test_router_app_version.py
@@ -49,60 +49,61 @@ def image():
     return Image(docker_id=random_string(), tags=random_string())
 
 
-# @pytest.fixture(scope="session")
-# def test_image(docker_client):
-#     # Create a simple Docker image using Python's official image
-#     # This Dockerfile will just create a new image based on python:3.9-slim
-#     dockerfile = """
-#     FROM python:3.9-slim
-#     """
+@pytest.fixture(scope="session")
+def docker_test_image(docker_client):
+    # Create a simple Docker image using Python's official image
+    # This Dockerfile will just create a new image based on python:3.9-slim
+    dockerfile = """
+    FROM python:3.9-slim
+    """
 
-#     image, _ = docker_client.images.build(fileobj=io.BytesIO(
-#         dockerfile.encode('utf-8')), tag="test:latest")
-#     return image
-
-
-# @pytest.fixture(scope="session", autouse=True)
-# def cleanup(docker_client, test_image):
-#     """Fixture that is automatically used before each test."""
-#     yield
-#     # Cleanup: remove the test image after all tests have run
-#     docker_client.images.remove(test_image.id)
+    image, _ = docker_client.images.build(fileobj=io.BytesIO(
+        dockerfile.encode('utf-8')), tag="agenta-server/test:latest")
+    return image
 
 
 def test_list_app_variant():
     response = client.get("/app_variant/list/")
-    print(response.json())
     assert response.status_code == 200
-    assert response.json() == []  # Assuming the db is empty
+    assert response.json() == []
 
 
 def test_list_app_variant_after_manual_add(app_variant, image):
+    # This is the function from db_manager.py
     add_app_variant(app_variant, image)
-
     response = client.get("/app_variant/list/")
-    print(response.json())
     assert response.status_code == 200
-    assert len(response.json()) == 1  # Assuming the db is empty
+    assert len(response.json()) == 1
     result = AppVariant(**response.json()[0])
     assert result.app_name == app_variant.app_name
     assert result.variant_name == app_variant.variant_name
 
 
-def test_add_variant(app_variant, image):
+def test_add_variant(app_variant, docker_test_image):
+    image = Image(docker_id=docker_test_image.id,
+                  tags=docker_test_image.tags[0])
     response = client.post(
         "app_variant/add/", json={"app_variant": app_variant.dict(), "image": image.dict()})
     assert response.status_code == 200
-    response_data = response.json()
-    print(response_data)
     response = client.get("/app_variant/list/")
     assert response.status_code == 200
-    print(response.json())
-    # if image in docker_utils.list_images():
-    #     assert response.status_code == 200
-    # else:
-    #     assert response.status_code == 500
+    assert len(response.json()) == 1
+    result = AppVariant(**response.json()[0])
+    assert result.app_name == app_variant.app_name
+    assert result.variant_name == app_variant.variant_name
 
+
+def test_add_variant_with_wrong_image_tag(app_variant, image):
+    response = client.post(
+        "app_variant/add/", json={"app_variant": app_variant.dict(), "image": image.dict()})
+    assert response.status_code == 500
+
+
+def test_add_variant_not_in_docker_registry(app_variant, image):
+    image.tags = "agenta-server/notexist:latest"
+    response = client.post(
+        "app_variant/add/", json={"app_variant": app_variant.dict(), "image": image.dict()})
+    assert response.status_code == 500
 
 # def test_start_model(app_version, image):
 #     db_manager.add_app_version(app_version, image)
