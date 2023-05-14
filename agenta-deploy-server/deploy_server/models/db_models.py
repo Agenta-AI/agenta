fe56cf7565ee99d30dee2f5da9427a507c57bfb2
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-05-14 18:45:27 +0200

    renamed app version to app variant

--- agenta-deploy-server/deploy_server/models/db_models.py
+++ agenta-deploy-server/deploy_server/models/db_models.py
@@ -3,16 +3,16 @@ from typing import List, Optional
 
 
 class ImageDB(SQLModel, table=True):
-    """Defines the info needed to get an image and connect it to the app version
+    """Defines the info needed to get an image and connect it to the app variant
     """
     id: int = Field(default=None, primary_key=True)
     docker_id: str = Field(...)
     tags: str = Field(...)
 
 
-class AppVersionDB(SQLModel, table=True):
-    """Defines an app version and connects to an image    """
+class AppVariantDB(SQLModel, table=True):
+    """Defines an app variant and connects to an image    """
     id: Optional[int] = Field(default=None, primary_key=True)
     app_name: str = Field(...)
-    version_name: str = Field(...)
+    variant_name: str = Field(...)
     image_id: int = Field(foreign_key="imagedb.id")
