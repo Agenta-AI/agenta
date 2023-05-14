1728eba1c27c1e826d490f8ca2d1c613fa869db3
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-05-14 13:01:18 +0200

    default view playground

--- agenta-web/src/pages/index.tsx
+++ agenta-web/src/pages/index.tsx
@@ -1,10 +1,9 @@
 import Evaluations from '@/components/Evaluations/Evaluations';
 import Layout from '@/components/Layout/Layout';
-
+import Playground from '@/components/Playground/Playground';
 export default function Dashboard() {
   return (
-    <Layout>
-      <Evaluations />
-    </Layout>
+    <Playground />
+
   );
 }
