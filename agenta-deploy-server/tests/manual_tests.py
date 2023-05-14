3e0c82e0630745d0722fcfb1b82f546609f00ae3
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-05-14 18:45:37 +0200

    added tests for router

--- agenta-deploy-server/tests/manual_tests.py
+++ agenta-deploy-server/tests/manual_tests.py
@@ -1,20 +1,12 @@
 import os
 import docker
 from deploy_server.config import settings
-from deploy_server.services.db_manager import get_session, list_app_versions
 
+from deploy_server.services.docker_utils import list_images, start_container, stop_container, delete_container
+from deploy_server.services.db_manager import add_app_variant, list_app_variants, get_image
+from deploy_server.models.api_models import AppVariant, Image, URI
 client = docker.from_env()
-
-
-def test_get_session():
-    assert get_session() is not None
-
-
-def test_list():
-    print(list_app_versions())
-    assert list_app_versions() == []
-
-
-if __name__ == "__main__":
-    test_get_session()
-    test_list()
+app_variant = AppVariant(app_name="test_app", variant_name="v1")
+image = Image(docker_id="test_id", tags="test_tag")
+add_app_variant(app_variant, image)
+print(list_app_variants())
