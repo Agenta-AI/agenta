9efac2b79275a3b97e17b599f5040a365e1a2ac7
aakrem <akrem.abayed@gmail.com>
2023-05-14 18:24:27 +0200

    add logo

--- agenta-web/src/components/Sidebar/Sidebar.tsx
+++ agenta-web/src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
-import { Layout, Menu, Switch } from 'antd';
+import { Layout, Menu, Space, Switch } from 'antd';
 import { FireOutlined, AlignCenterOutlined } from '@ant-design/icons';
+import logoDarkMode from './logo-dark-mode.png'
+import logoWhiteMode from './logo-light-mode.png'
+import Image from 'next/image';
 
 const { Sider } = Layout;
 
@@ -18,19 +21,38 @@ const Sidebar: React.FC = () => {
   };
 
   return (
-    <Sider theme={theme} width={200} style={{ paddingTop: '5px', paddingLeft: '5px', paddingRight: '5px' }}>
+    <Sider theme={theme} width={250} style={{ paddingTop: '5px', paddingLeft: '15px', paddingRight: '15px' }}>
       <Switch
         checked={theme === 'dark'}
         onChange={changeTheme}
         checkedChildren="Dark"
         unCheckedChildren="Light"
-        style={{ marginTop: '10px', marginBottom: '20px',}}
+        style={{ marginTop: '20px' }}
       />
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+      }}>
+        <div style={{
+          padding: 10,
+          marginBottom: 30,
+          marginTop: 30,
+          border: `1px solid ${theme == 'dark' ? '#fff' : '#000'}`,
+          borderRadius: '10px',
+        }}>
+          <Image
+            src={theme == 'dark' ? logoDarkMode : logoWhiteMode}
+            width={160}
+            alt="Picture of the author"
+          />
+        </div>
+      </div>
+
       <Menu
         mode="inline"
         defaultSelectedKeys={['1']}
         defaultOpenKeys={['sub1']}
-        style={{ height: '100%', borderRight: 0 }}
+        style={{ borderRight: 0 }}
         theme={theme}
       >
         <Menu.Item key="1" icon={<AlignCenterOutlined />} onClick={() => navigate('/logs')}>
