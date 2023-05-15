4ccbed624c063c31fff0bb5bf11a5c66474babbc
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-05-15 15:18:54 +0200

    bf - working

--- agenta-deploy-server/deploy_server/main.py
+++ agenta-deploy-server/deploy_server/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from deploy_server.routers import app_variant
 
 # this is the prefix in which we are reverse proxying the api
-app = FastAPI(openapi_prefix="/api")
+app = FastAPI()
 app.include_router(app_variant.router, prefix='/app_variant')
 
 
