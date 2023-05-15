289c7a99af816297f34db75a80c53dd85f6b20a7
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-05-15 15:04:38 +0200

    couple of fixes + added list_images endpoint

--- agenta-deploy-server/deploy_server/routers/app_variant.py
+++ agenta-deploy-server/deploy_server/routers/app_variant.py
@@ -16,7 +16,7 @@ router = APIRouter()
 
 @router.get("/list/", response_model=List[AppVariant])
 async def list_app_variants():
-    """Lists the images from our repository
+    """Lists the app variants from our repository
 
     Raises:
         HTTPException: _description_
@@ -33,28 +33,39 @@ async def list_app_variants():
 
 @router.post("/add/")
 async def add_variant(app_variant: AppVariant, image: Image):
-    # checks if the image is already in the registry
-    # db_manager.add_app_variant(app_variant, image)
+    """Add a variant to the server.
+
+    Arguments:
+        app_variant -- _description_
+        image -- The image tags should start with the registry name (agenta-server) and end with :latest
+
+    Raises:
+        HTTPException: _description_
+        HTTPException: _description_
+        HTTPException: _description_
+    """
+
+    if not image.tags.startswith(settings.registry):
+        raise HTTPException(
+            status_code=500, detail="Image should have a tag starting with the registry name (agenta-server)")
+    elif image not in docker_utils.list_images():
+        raise HTTPException(status_code=500, detail="Image not found")
+
     try:
-        if not image.tags.startswith(settings.registry):
-            raise HTTPException(
-                status_code=500, detail="Image should have a tag starting with the registry name (agenta-server)")
-        elif image not in docker_utils.list_images():
-            raise HTTPException(status_code=500, detail="Image not found")
-        else:
-            db_manager.add_app_variant(app_variant, image)
+        db_manager.add_app_variant(app_variant, image)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.post("/start/")
 async def start_variant(app_variant: AppVariant) -> URI:
-    # try:
-    image: Image = db_manager.get_image(app_variant)
-    uri: URI = docker_utils.start_container(image)
-    return uri
-    # except Exception as e:
-    #     raise HTTPException(status_code=500, detail=str(e))
+    try:
+        image: Image = db_manager.get_image(app_variant)
+        uri: URI = docker_utils.start_container(
+            image_name=image.tags, app_name=app_variant.app_name)
+        return uri
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.post("/stop/")
@@ -64,5 +75,22 @@ async def stop_variant(app_variant: AppVariant):
         docker_utils.stop_container(image)
         docker_utils.delete_container(image)
         return {"detail": "Container stopped and deleted successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get("/list_images/", response_model=List[Image])
+async def list_images():
+    """Lists the images from our repository
+
+    Raises:
+        HTTPException: _description_
+
+    Returns:
+        List[AppVariant]
+    """
+    try:
+        list_images = docker_utils.list_images()
+        return list_images
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
