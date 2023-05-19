3f010cab26ea7e1f0ed4a6c98088b3be1cb8050e
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-05-19 16:18:08 +0200

    updated example pitch_genius

--- examples/pitch_genius/agenta.py
+++ examples/pitch_genius/agenta.py
@@ -1,5 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
+from typing import Callable, Any, Optional
+import functools
+import inspect
+
 app = FastAPI()
 
 origins = [
@@ -14,18 +18,50 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-def post(func):
-    """post decorator
 
-    Arguments:
-        func -- _description_
+class TextParam(str):
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update({"x-parameter": "text"})
 
-    Returns:
-        _description_
-    """
+
+class FloatParam(float):
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update({"x-parameter": "float"})
+
+
+def post(func: Callable[..., Any]):
+    sig = inspect.signature(func)
+    func_params = sig.parameters
+
+    app_params = {name: param.default for name, param in func_params.items()
+                  if isinstance(param.default, (TextParam, FloatParam))}
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        kwargs = {**app_params, **kwargs}
+        return func(*args, **kwargs)
+
+    new_params = []
+    for name, param in sig.parameters.items():
+        if name in app_params:
+            new_params.append(
+                inspect.Parameter(
+                    name,
+                    inspect.Parameter.KEYWORD_ONLY,
+                    default=app_params[name],
+                    annotation=Optional[type(app_params[name])]
+                )
+            )
+        else:
+            new_params.append(param)
+
+    wrapper.__signature__ = sig.replace(parameters=new_params)
+
     route = f"/{func.__name__}"
-    app.post(route)(func)
-    return func
+    app.post(route)(wrapper)
+    return wrapper
 
 
 def get(func):
