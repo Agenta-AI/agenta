9861cb3d1590173468b3420ee80789cea0af6b68
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-05-20 12:07:42 +0200

    added header and project view

--- agenta-web/src/components/Layout/Layout.tsx
+++ agenta-web/src/components/Layout/Layout.tsx
@@ -2,41 +2,50 @@ import React from 'react';
 import { Layout, theme } from 'antd';
 import Sidebar from '../Sidebar/Sidebar';
 import { HeartTwoTone } from '@ant-design/icons';
+import { useRouter } from 'next/router';
+import Header from '../Header/Header';
 
 type LayoutProps = {
   children: React.ReactNode
 }
+type User = {
+  name: string;
+  avatar: string;
+};
 
-const { Header, Content, Footer } = Layout;
+const { Content, Footer } = Layout;
+const mockUser: User = {
+  name: 'John Doe',
+  avatar: 'https://example.com/john-doe.jpg',
+};
 
 const App: React.FC<LayoutProps> = ({ children }) => {
 
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+  const router = useRouter();
 
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-      < Sidebar />
-      {/* <Header style={{ padding: 0, background: colorBgContainer }} /> */}
-      <Layout className="site-layout">
-
-      < Content style={{ margin: '0 5px' }}>
-
-        <div style={{ padding: 24, background: colorBgContainer, minHeight: '100vh' }}>
-          {children}
-        </div>
-      </Content >
-        <Footer style={{ textAlign: 'center' }}>
-          <div>
-            <span>Agenta © 2023. Made with</span>
-            <span> <HeartTwoTone twoToneColor="#eb2f96" /> </span>
-            <span>in Berlin.</span>
+    <Layout >
+      <Header user={mockUser} />
+      <Layout style={{ marginTop: '1px' }}>
+        {router.pathname !== '/' && <Sidebar />}
+        <Content style={{ margin: '0 1px' }}>
+          <div style={{ padding: 20, background: colorBgContainer, minHeight: '100vh' }}>
+            {children}
           </div>
-          </Footer>
+        </Content>
       </Layout>
+      <Footer style={{ textAlign: 'center' }}>
+        <div>
+          <span>Agenta © 2023. Made with</span>
+          <span> <HeartTwoTone twoToneColor="#eb2f96" /> </span>
+          <span>in Berlin.</span>
+        </div>
+      </Footer>
 
-    </Layout >
+    </Layout>
   );
 };
 
