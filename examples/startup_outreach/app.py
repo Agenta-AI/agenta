9dff293f3001d18f069ab22c8134c5c64a77fe9e
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-05-24 11:16:47 +0200

    updated example

---
+++ examples/startup_outreach/app.py
@@ -0,0 +1,24 @@
+from agenta import post, TextParam, FloatParam
+from dotenv import load_dotenv
+from langchain.chains import LLMChain
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+import os
+
+
+default_prompt = """
+    please write a short linkedin message (2 SENTENCES MAX) to an investor pitchin the following startup:
+    startup name: {startup_name}
+    startup idea: {startup_idea}"""
+
+
+@post
+def generate(startup_name: str, startup_idea: str, prompt_template: TextParam = default_prompt, temperature: FloatParam = 0.5) -> str:
+    llm = OpenAI(temperature=temperature)
+    prompt = PromptTemplate(
+        input_variables=["startup_name", "startup_idea"],
+        template=prompt_template)
+
+    chain = LLMChain(llm=llm, prompt=prompt)
+    output = chain.run(startup_name=startup_name, startup_idea=startup_idea)
+    return output
