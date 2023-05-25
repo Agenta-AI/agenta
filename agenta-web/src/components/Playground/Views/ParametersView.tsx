2bfa78c534e33a2058f954539638826ebcf19e02
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-05-25 18:04:17 +0200

    Added button for saving parameters

--- agenta-web/src/components/Playground/Views/ParametersView.tsx
+++ agenta-web/src/components/Playground/Views/ParametersView.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import React from 'react';
 import { Parameter } from '@/services/api';
-import { Input, Slider, Row, Col, InputNumber } from 'antd';
+import { Input, Slider, Row, Col, InputNumber, Button, Tooltip } from 'antd';
 
 interface Props {
     optParams: Parameter[] | null;  // The optional parameters
@@ -14,57 +14,72 @@ const ParametersView: React.FC<Props> = ({ optParams, onOptParamsChange }) => {
         setInputValue(newValue);
         handleParamChange(param.name, newValue)
     };
-    const handleParamChange = (name: string, newVal: any) => {
+    const handleParamChange = (name: string, newVal: any,) => {
         const newOptParams = optParams?.map(param =>
             param.name === name ? { ...param, default: newVal } : param);
         newOptParams && onOptParamsChange(newOptParams, false)
     }
     return (
-        <Row gutter={16}>
-            <Col span={12}>
-                {optParams?.filter(param => (param.type === 'string')).map((param, index) => (
-                    <div key={index}>
-                        <h3>{param.name}</h3>
+        <div>
+            <Row gutter={16}>
+                <Col span={12}>
+                    {optParams?.filter(param => (param.type === 'string')).map((param, index) => (
+                        <div key={index}>
+                            <h3>{param.name}</h3>
 
-                        <Input.TextArea rows={5}
-                            defaultValue={param.default}
-                            onChange={e => handleParamChange(param.name, e.target.value)}
-                        />
-                    </div>
-                ))}
-            </Col>
+                            <Input.TextArea rows={5}
+                                defaultValue={param.default}
+                                onChange={e => handleParamChange(param.name, e.target.value)}
+                            />
+                        </div>
+                    ))}
+                </Col>
 
-            <Col span={12}>
-                {optParams?.filter(param => (!param.input) && (param.type === 'number')).map((param, index) => (
-                    <div key={index}>
-                        <h3>{param.name}</h3>
-                        <Row>
-                            <Col span={12}>
-                                <Slider
-                                    min={0}
-                                    max={1}
-                                    value={typeof param.default === 'number' ? param.default : 0}
-                                    step={0.01}
-                                    onChange={value => onChange(param, value)}
-                                    style={{ marginBottom: 16 }}
-                                />
-                            </Col>
-                            <Col span={4}>
-                                <InputNumber
-                                    min={1}
-                                    max={20}
-                                    style={{ margin: '0 16px' }}
-                                    value={param.default}
-                                    onChange={(value) => onChange(param, value)}
-                                />
-                            </Col>
-                        </Row>
-                    </div>
-                ))}
-            </Col>
+                <Col span={12}>
+                    {optParams?.filter(param => (!param.input) && (param.type === 'number')).map((param, index) => (
+                        <div key={index}>
+                            <h3>{param.name}</h3>
+                            <Row>
+                                <Col span={12}>
+                                    <Slider
+                                        min={0}
+                                        max={1}
+                                        value={typeof param.default === 'number' ? param.default : 0}
+                                        step={0.01}
+                                        onChange={value => onChange(param, value)}
+                                        style={{ marginBottom: 16 }}
+                                    />
+                                </Col>
+                                <Col span={12}>
+                                    <InputNumber
+                                        min={1}
+                                        max={20}
+                                        style={{ margin: '0 16px' }}
+                                        value={param.default}
+                                        onChange={(value) => onChange(param, value)}
+                                    />
+                                </Col>
+                            </Row>
+
+                        </div>
+                    ))}
+                </Col>
 
 
-        </Row>
+            </Row>
+            <Row style={{ marginTop: 40 }}>
+                <Col span={12} style={{ textAlign: 'left' }}>
+                    <Button type="primary" onClick={() => onOptParamsChange(optParams!, true)}>
+                        <Tooltip placement="right" title="Save the new parameters for the variant permanently">
+                            Save changes
+                        </Tooltip>
+                    </Button>
+                </Col>
+                <Col span={12} style={{ textAlign: 'left' }} >
+                </Col>
+
+            </Row>
+        </div>
     );
 };
 export default ParametersView;
