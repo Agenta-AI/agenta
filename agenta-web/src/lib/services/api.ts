8d9468be0c42f76691737950a4a877c16340f3f5
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-05-26 17:31:10 +0200

    remove depracated

--- agenta-web/src/lib/services/api.ts
+++ agenta-web/src/lib/services/api.ts
@@ -27,24 +27,7 @@ export async function fetchVariants(app: string): Promise<Variant[]> {
 
     return [];
 }
-/**
- * DEPREACATED
- * This function runs a variant and returns the result. It calls the right api endpoint.
- * and take care of specifying the right parameters if needed.
- * @param app_name
- * @param variantName
- * @param inputs
- * @returns - the result of the variant as a json object
- */
-export function runVariant(appName: string, variantName: string, inputs: any) {
-    const urlParams = Object.entries(inputs).map(([key, val]) => `${key}=${encodeURIComponent(val)}`).join('&');
-    console.log(urlParams);
-    return axios.post(`${API_BASE_URL}/${appName}/${variantName}/generate?${urlParams}`, {
-        headers: {
-            'accept': 'application/json',
-        }
-    }).then(res => res.data);
-}
+
 
 export function callVariant(inputParamsDict: Record<string, string>, optParams: Parameter[], URIPath: string) {
     const inputParams = Object.keys(inputParamsDict).map(key => `${key}=${encodeURIComponent(inputParamsDict[key])}`).join('&');
@@ -55,27 +38,6 @@ export function callVariant(inputParamsDict: Record<string, string>, optParams: 
         }
     }).then(res => res.data);
 }
-
-
-/**
- * DEPREACTED
- * Parses the openapi.json from a variant and returns the parameters as an array of objects.
- * @param app
- * @param variantName
- * @returns
- */
-export const fetchVariantParameters = async (app: string, variantName: string) => {
-    try {
-        const url = `${API_BASE_URL}/${app}/${variantName}/openapi.json`;
-        const response = await axios.get(url);
-        const APIParams = parseOpenApiSchema(response.data);
-        const initOptParams = APIParams.filter(param => (!param.input)); // contains the default values too!
-        const inputParams = APIParams.filter(param => (param.input)); // don't have input values
-        return { initOptParams, inputParams };
-    } catch (error) {
-        throw error;
-    }
-};
 
 /**
  * Parses the openapi.json from a variant and returns the parameters as an array of objects.
@@ -141,17 +103,17 @@ const eval_endpoint = axios.create({
     baseURL: `${API_BASE_URL}/api/app_evaluations`,
 });
 
-export const updateAppEvaluations = async (comparisonTableId, data) => {
+export const updateAppEvaluations = async (comparisonTableId: string, data) => {
     const response = await eval_endpoint.put(`/${comparisonTableId}`, data);
     return response.data;
 };
 
-export const updateEvaluationRow = async (comparisonTableId, evaluationRowId, data) => {
+export const updateEvaluationRow = async (comparisonTableId: string, evaluationRowId: string, data) => {
     const response = await eval_endpoint.put(`/${comparisonTableId}/evaluation_row/${evaluationRowId}`, data);
     return response.data;
 };
 
-export const postEvaluationRow = async (comparisonTableId, data) => {
+export const postEvaluationRow = async (comparisonTableId: string, data) => {
     const response = await eval_endpoint.post(`/${comparisonTableId}/evaluation_row`, data);
     return response.data;
 };
