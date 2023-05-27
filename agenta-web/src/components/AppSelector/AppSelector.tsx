727ab5f9fb597b46c1abceb895d8fba76eb2d0e6
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-05-27 20:32:08 +0200

    fix spacing

--- agenta-web/src/components/AppSelector/AppSelector.tsx
+++ agenta-web/src/components/AppSelector/AppSelector.tsx
@@ -1,7 +1,7 @@
 // components/AppSelector.tsx
 import { useContext, useState } from 'react';
 import { useRouter } from 'next/router';
-import { Button, Input, Row, Card, Col, Modal, Tag, Tooltip } from 'antd';
+import { Button, Input, Row, Space, Col, Modal, Tag, Tooltip } from 'antd';
 import useSWR from 'swr'
 import Link from 'next/link';
 
@@ -33,26 +33,24 @@ const AppSelector = () => {
 
     return (
         <div style={{ margin: "50px 150px" }}>
-            <Row gutter={10}> {/* gutter adds spacing between columns */}
+            <Space>
                 {data.map((app, index) => (
-                    <Col key={index} span={6}>
+                    <div key={index}>
                         <Link href={`/apps/${app.app_name}/playground`}>
                             <Button type="default" style={{ height: '200px', width: '220px' }}>
                                 <h3 style={{ fontWeight: 'bold' }}>{`${app.app_name}`}</h3>
                             </Button>
                         </Link>
-                    </Col>
+                    </div>
 
                 ))}
-                <Col span={6}>
-                    <Button type="default" onClick={showModal} style={{ height: '200px', width: '220px' }} disabled={true}>
-                        <Tooltip placement="right" title="Currently, the only way to add new apps is through the CLI.">
+                <Button type="default" onClick={showModal} style={{ height: '200px', width: '220px' }} disabled={true}>
+                    <Tooltip placement="right" title="Currently, the only way to add new apps is through the CLI.">
 
-                            <h3 style={{ fontWeight: 'bold' }}>New app</h3><Tag color="warning">soon</Tag>
-                        </Tooltip>
-                    </Button>
-                </Col>
-            </Row>
+                        <h3 style={{ fontWeight: 'bold' }}>New app</h3><Tag color="warning">soon</Tag>
+                    </Tooltip>
+                </Button>
+            </Space>
             <Modal title="Add new app from template" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
                 <Input
                     placeholder="New app name"
