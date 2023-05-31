a76ff55524fedc70981543c9c1c6d4215c0a4fe7
aakrem <akrem.abayed@gmail.com>
2023-05-31 05:42:53 +0200

    fix indentation to 4 spaces

--- agenta-web/src/lib/Types.ts
+++ agenta-web/src/lib/Types.ts
@@ -1,26 +1,26 @@
 export interface Dataset {
-  id: string;
-  name: string;
-  created_date?: string;
+    id: string;
+    name: string;
+    created_date?: string;
 }
 
 export interface AppVariant {
-  id: number;
-  name: string;
-  endpoint: string;
+    id: number;
+    name: string;
+    endpoint: string;
 }
 
 export interface Variant {
-  variantName: string;
-  templateVariantName: string | null; // template name of the variant in case it has a precursor. Needed to compute the URI path
-  persistent: boolean;  // whether the variant is persistent in the backend or not
-  parameters: Record<string, string> | null;  // parameters of the variant. Only set in the case of forked variants
+    variantName: string;
+    templateVariantName: string | null; // template name of the variant in case it has a precursor. Needed to compute the URI path
+    persistent: boolean;  // whether the variant is persistent in the backend or not
+    parameters: Record<string, string> | null;  // parameters of the variant. Only set in the case of forked variants
 }
 
 export interface Parameter {
-  name: string;
-  type: string;
-  input: boolean;
-  required: boolean;
-  default?: any;
+    name: string;
+    type: string;
+    input: boolean;
+    required: boolean;
+    default?: any;
 }
