ae763136c12b7e23d92f411d1f29c642d0c346ef
aakrem <akrem.abayed@gmail.com>
2023-05-31 05:42:53 +0200

    fix indentation to 4 spaces

--- agenta-web/dev.Dockerfile
+++ agenta-web/dev.Dockerfile
@@ -5,12 +5,12 @@ WORKDIR /app
 # Install dependencies based on the preferred package manager
 COPY package.json yarn.lock* package-lock.json* pnpm-lock.yaml* ./
 RUN \
-  if [ -f yarn.lock ]; then yarn --frozen-lockfile; \
-  elif [ -f package-lock.json ]; then npm ci; \
-  elif [ -f pnpm-lock.yaml ]; then yarn global add pnpm && pnpm i; \
-  # Allow install without lockfile, so example works even without Node.js installed locally
-  else echo "Warning: Lockfile not found. It is recommended to commit lockfiles to version control." && yarn install; \
-  fi
+    if [ -f yarn.lock ]; then yarn --frozen-lockfile; \
+    elif [ -f package-lock.json ]; then npm ci; \
+    elif [ -f pnpm-lock.yaml ]; then yarn global add pnpm && pnpm i; \
+    # Allow install without lockfile, so example works even without Node.js installed locally
+    else echo "Warning: Lockfile not found. It is recommended to commit lockfiles to version control." && yarn install; \
+    fi
 
 COPY src ./src
 COPY public ./public
@@ -26,8 +26,8 @@ COPY postcss.config.js .
 
 # Start Next.js in development mode based on the preferred package manager
 CMD \
-  if [ -f yarn.lock ]; then yarn dev; \
-  elif [ -f package-lock.json ]; then npm run dev; \
-  elif [ -f pnpm-lock.yaml ]; then pnpm dev; \
-  else yarn dev; \
-  fi
+    if [ -f yarn.lock ]; then yarn dev; \
+    elif [ -f package-lock.json ]; then npm run dev; \
+    elif [ -f pnpm-lock.yaml ]; then pnpm dev; \
+    else yarn dev; \
+    fi
--- agenta-web/src/components/Layout/Layout.tsx
+++ agenta-web/src/components/Layout/Layout.tsx
@@ -5,44 +5,44 @@ import { HeartTwoTone } from '@ant-design/icons';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 type LayoutProps = {
-  children: React.ReactNode
+    children: React.ReactNode
 }
 
 const { Content, Footer } = Layout;
 
 const App: React.FC<LayoutProps> = ({ children }) => {
-  const router = useRouter();
-  const { app_name } = router.query
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+    const router = useRouter();
+    const { app_name } = router.query
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
 
-  return (
-    <Layout>
-      <Layout hasSider>
-        <Sidebar />
-        <Content >
-          <div style={{ padding: 20, background: colorBgContainer, minHeight: '95vh' }}>
-            <Breadcrumb
-              style={{ marginTop: '20px', marginBottom: '40px' }}
-              items={[
-                { title: <Link href="/apps">Apps</Link> },
-                { title: app_name }
-              ]}
-            />
-            {children}
-          </div>
-        </Content>
-      </Layout>
-      <Footer style={{ textAlign: 'center', padding: '10px 50px' }}>
-        <div>
-          <span>Agenta © 2023. Made with</span>
-          <span> <HeartTwoTone twoToneColor="#eb2f96" /> </span>
-          <span>in Berlin.</span>
-        </div>
-      </Footer>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Layout hasSider>
+                <Sidebar />
+                <Content >
+                    <div style={{ padding: 20, background: colorBgContainer, minHeight: '95vh' }}>
+                        <Breadcrumb
+                            style={{ marginTop: '20px', marginBottom: '40px' }}
+                            items={[
+                                { title: <Link href="/apps">Apps</Link> },
+                                { title: app_name }
+                            ]}
+                        />
+                        {children}
+                    </div>
+                </Content>
+            </Layout>
+            <Footer style={{ textAlign: 'center', padding: '10px 50px' }}>
+                <div>
+                    <span>Agenta © 2023. Made with</span>
+                    <span> <HeartTwoTone twoToneColor="#eb2f96" /> </span>
+                    <span>in Berlin.</span>
+                </div>
+            </Footer>
+        </Layout>
+    );
 };
 
 export default App;
