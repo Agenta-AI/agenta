7b4b7f0f3d2123168e87f468626778e1160a6d19
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-06-05 11:54:56 +0200

    small improvements. update with backend is now running before each command

--- agenta-cli/agenta/cli/helper.py
+++ agenta-cli/agenta/cli/helper.py
@@ -1,5 +1,7 @@
+from pathlib import Path
 from typing import Any, List, MutableMapping
 
+import toml
 from agenta.client import client
 from agenta.client.api_models import AppVariant
 
@@ -18,3 +20,18 @@ def update_variants_from_backend(app_name: str,
     variants: List[AppVariant] = client.list_variants(app_name)
     config['variants'] = [variant.variant_name for variant in variants]
     return config
+
+
+def update_config_from_backend(config_file: Path):
+    """Updates the config file with new information from the backend
+
+    Arguments:
+        config_file -- the path to the config file
+    """
+    assert config_file.exists(), "Config file does not exist!"
+    config = toml.load(config_file)
+    app_name = config['app-name']
+    if 'variants' not in config:
+        config['variants'] = []
+    config = update_variants_from_backend(app_name, config)
+    toml.dump(config, config_file.open('w'))
