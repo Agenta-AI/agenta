e514c3a0a8efc2256ea588a469215fa8be59d793
aakrem <akrem.abayed@gmail.com>
2023-06-10 12:14:55 +0200

    update setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 source .env
 echo $DOMAIN_NAME
 echo $AGENTA_API_URL
@@ -11,11 +12,13 @@ update_env_var() {
     # check if the key exists in the file
     if grep -q $key $file; then
         # the key exists, replace the line with new key value pair
-        sed -i '' "s/^$key=.*/$key=$new_value/" $file
+        sed -i'' "s#^$key=.*#$key=$new_value#" $file
     else
         # the key does not exist, add to the file
         echo "$key=$new_value" >> $file
     fi
 }
 
-update_env_var "NEXT_PUBLIC_AGENTA_API_URL" $AGENTA_API_URL "agenta-web/.env.development.local"
+update_env_var "NEXT_PUBLIC_AGENTA_API_URL" "$AGENTA_API_URL" "./agenta-web/.env.development.local"
+
+update_env_var "ALLOW_ORIGINS" "$ALLOW_ORIGINS" "./agenta-cli/agenta/config.toml"
