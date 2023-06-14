de4947d13e9378cdd16e35b2507fbde048730eb5
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-06-14 20:14:14 +0200

    backup

--- examples/ingestion_v0/ingest.py
+++ examples/ingestion_v0/ingest.py
@@ -2,28 +2,28 @@ import agenta
 from llama_index import VectorStoreIndex, SimpleDirectoryReader, load_index_from_storage, StorageContext, Prompt
 import os
 from pathlib import Path
+from context import Context, save_context
 
 
-def test(index):
-    default_prompt = (
-        "We have provided context information below. \n"
-        "---------------------\n"
-        "{context_str}"
-        "\n---------------------\n"
-        "Given this information, please answer the question: {query_str}\n"
-    )
+# def test(index):
+#     default_prompt = (
+#         "We have provided context information below. \n"
+#         "---------------------\n"
+#         "{context_str}"
+#         "\n---------------------\n"
+#         "Given this information, please answer the question: {query_str}\n"
+#     )
 
-    QA_TEMPLATE = Prompt(default_prompt)
-    #
-    query_engine = index.as_query_engine(text_qa_template=QA_TEMPLATE)
-    response = query_engine.query("what are the pains of the customer?")
-    return str(response)
+#     QA_TEMPLATE = Prompt(default_prompt)
+#     #
+#     query_engine = index.as_query_engine(text_qa_template=QA_TEMPLATE)
+#     response = query_engine.query("what are the pains of the customer?")
+#     return str(response)
 
 
 @agenta.ingest
 def ingest(file: agenta.InFile):
     context_name = file.file_name
-    print(context_name)
     persist_dir = f"./{context_name}"
     if not os.path.exists(persist_dir):
         documents = SimpleDirectoryReader(input_files=[file.file_path]).load_data()
@@ -32,4 +32,6 @@ def ingest(file: agenta.InFile):
     else:
         storage_context = StorageContext.from_defaults(persist_dir=persist_dir)
         index = load_index_from_storage(storage_context)
-    return test(index)
+    c = Context(persist_dir=context_name)
+    save_context(c)
+    return Context(persist_dir=context_name)
