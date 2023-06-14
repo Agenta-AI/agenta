ddd589a62a7326ae9d20efaf04b78639a9444b79
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-06-14 18:25:46 +0200

    added ingest importing

--- agenta-cli/agenta/docker/docker-assets/main.py
+++ agenta-cli/agenta/docker/docker-assets/main.py
@@ -1,12 +1,11 @@
-from dotenv import load_dotenv
-load_dotenv()
-
 from uvicorn import run
 import agenta
 import app  # This will register the routes with the FastAPI application
 
+try:
+    import ingest
+except ImportError:
+    pass
+
 if __name__ == "__main__":
-    
-
     run("agenta:app", host="0.0.0.0", port=80)
-
