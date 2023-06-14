39557df4e1bfac051d5b1fd5a3bc623a4709b176
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-06-14 19:57:54 +0200

    backup

--- examples/ingestion_v0/ingest.py
+++ examples/ingestion_v0/ingest.py
@@ -1,13 +1,35 @@
+import agenta
 from llama_index import VectorStoreIndex, SimpleDirectoryReader, load_index_from_storage, StorageContext, Prompt
+import os
+from pathlib import Path
 
 
-def ingest(file_path):
-    if not os.path.exists("./storage"):
-        documents = SimpleDirectoryReader('data').load_data()
+def test(index):
+    default_prompt = (
+        "We have provided context information below. \n"
+        "---------------------\n"
+        "{context_str}"
+        "\n---------------------\n"
+        "Given this information, please answer the question: {query_str}\n"
+    )
+
+    QA_TEMPLATE = Prompt(default_prompt)
+    #
+    query_engine = index.as_query_engine(text_qa_template=QA_TEMPLATE)
+    response = query_engine.query("what are the pains of the customer?")
+    return str(response)
+
+
+@agenta.ingest
+def ingest(file: agenta.InFile):
+    context_name = file.file_name
+    print(context_name)
+    persist_dir = f"./{context_name}"
+    if not os.path.exists(persist_dir):
+        documents = SimpleDirectoryReader(input_files=[file.file_path]).load_data()
         index = VectorStoreIndex.from_documents(documents)
-        index.storage_context.persist()
+        index.storage_context.persist(persist_dir=persist_dir)
     else:
-        storage_context = StorageContext.from_defaults(persist_dir="./storage")
-        # rebuild storage context
+        storage_context = StorageContext.from_defaults(persist_dir=persist_dir)
         index = load_index_from_storage(storage_context)
-    return index
+    return test(index)
