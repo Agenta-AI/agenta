e3810c70ce25ad7a4dfddf9d5f9bbe742054f6cb
aakrem <akrem.abayed@gmail.com>
2023-06-17 17:38:30 +0200

    instance with ebs

--- self-host/terraform/agenta_instance.tf
+++ self-host/terraform/agenta_instance.tf
@@ -1,19 +1,32 @@
-# resource "aws_eip" "agenta_eip" {
-#   domain = "vpc"
+resource "aws_eip" "agenta_eip" {
+  domain = "vpc"
 
-#   instance = aws_instance.example.id
-# }
+  instance = aws_instance.agenta.id
+}
 
-# resource "aws_instance" "agenta" {
-#   ami           = data.aws_ami.ubuntu.id
-#   instance_type = "t2.medium"
-#   key_name      = "dev-agenta-keypair"
+resource "aws_instance" "agenta" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.medium"
+  key_name      = "agenta-dev"
 
-# #   user_data = file("setup.sh")
+  vpc_security_group_ids = [aws_security_group.agenta_instance_sg.id]
 
-#   vpc_security_group_ids = [aws_security_group.agenta_instance_sg.id]
+  tags = {
+    Name = "agenta-instance"
+  }
+}
 
-#   tags = {
-#     Name = "agenta-instance"
-#   }
-# }
+resource "aws_ebs_volume" "agenta_ebs" {
+  availability_zone = aws_instance.agenta.availability_zone
+  size              = 100
+  type              = "gp2"  # General Purpose SSD
+  tags = {
+    Name = "agenta-volume"
+  }
+}
+
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.agenta_ebs.id
+  instance_id = aws_instance.agenta.id
+}
