623753eeeb43f840239fa96017026e7567a89e77
aakrem <akrem.abayed@gmail.com>
2023-06-19 14:39:09 +0200

    remove commented lines

--- agenta-backend/agenta_backend/services/docker_utils.py
+++ agenta-backend/agenta_backend/services/docker_utils.py
@@ -44,12 +44,6 @@ def start_container(image_name, app_name, variant_name) -> URI:
         f"traefik.http.services.{app_name}-{variant_name}.loadbalancer.server.port": "80",
         f"traefik.http.middlewares.{app_name}-{variant_name}-strip-prefix.stripprefix.prefixes": f"/{app_name}/{variant_name}",
         f"traefik.http.routers.{app_name}-{variant_name}.middlewares": f"{app_name}-{variant_name}-strip-prefix",
-        # this line connects the router to the service
-        # f"traefik.http.middlewares.{app_name}-{variant_name}-openapi.redirectregex.regex": "^/openapi.json$",
-        # f"traefik.http.middlewares.{app_name}-{variant_name}-openapi.redirectregex.replacement": f"/{app_name}/openapi.json",
-        # f"traefik.http.middlewares.{app_name}-{variant_name}-openapi.redirectregex.permanent": "true",
-        # f"traefik.http.routers.{app_name}-{variant_name}-openapi.rule": "Path(`/openapi.json`)",
-        # f"traefik.http.routers.{app_name}-{variant_name}-openapi.middlewares": f"{app_name}-{variant_name}-openapi",
         f"traefik.http.routers.{app_name}-{variant_name}.service": f"{app_name}-{variant_name}",
     }
 
