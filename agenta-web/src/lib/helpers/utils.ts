4c8e837989cf4e2233256c20b72108177e2f4cf0
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-06-20 11:10:47 +0200

    minor ui

--- agenta-web/src/lib/helpers/utils.ts
+++ agenta-web/src/lib/helpers/utils.ts
@@ -1,1 +1,9 @@
 export const renameVariables = (name: string) => { return name.charAt(0).toUpperCase() + name.slice(1).replace(/_/g, ' '); };
+
+export const renameVariablesCapitalizeAll = (name: string) => {
+    const words = name.split('_');
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+    return words.join(' ');
+};
