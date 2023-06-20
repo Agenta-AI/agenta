ebff9f0bad0ff068aed06e2dc2b37872afc3d08a
aakrem <akrem.abayed@gmail.com>
2023-06-20 17:48:48 +0200

    move all terraform files to aws and small improvements

--- self-host/aws/agenta_instance_sg.tf
+++ self-host/aws/agenta_instance_sg.tf
@@ -19,14 +19,6 @@ resource "aws_security_group" "agenta_instance_sg" {
   }
 
   ingress {
-    description = "Allow HTTPS inbound traffic"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     description = "Treafik Dashboard inbound traffic"
     from_port   = 8080
     to_port     = 8080
