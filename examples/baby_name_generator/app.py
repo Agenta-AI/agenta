84ba50677a79ab6e1a758276a8194f532a9fe04a
aakrem <akrem.abayed@gmail.com>
2023-06-29 11:27:19 +0200

    adjust baby_name_generator example to accept body parameters instead of query parameters

--- examples/baby_name_generator/app.py
+++ examples/baby_name_generator/app.py
@@ -1,17 +1,18 @@
 import os
 
 import openai
-from agenta import FloatParam, TextParam, post
+from agenta import post
+from fastapi import Body
 from jinja2 import Template
 
 default_prompt = "Give me five cool names for a baby from this country {{country}} with this gender {{gender}}!!!!"
 
 
 @post
-def generate(country: str, gender: str, temperature: FloatParam = 0.9, prompt_template: TextParam = default_prompt) -> str:
+def generate(body_params: dict = Body(...)) -> str:
 
-    template = Template(prompt_template)
-    prompt = template.render(country=country, gender=gender)
+    template = Template(body_params['prompt_template'])
+    prompt = template.render(country=body_params['country'], gender=body_params['gender'])
 
     openai.api_key = os.environ.get("OPENAI_API_KEY")  # make sure to set this manually!
     chat_completion = openai.ChatCompletion.create(
