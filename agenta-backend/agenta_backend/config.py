90cb6de8d98467b4679d58cac071bd1b819ae2ca
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-07-06 13:37:40 +0200

    rm environemnt variable from config

--- agenta-backend/agenta_backend/config.py
+++ agenta-backend/agenta_backend/config.py
@@ -7,16 +7,15 @@ import toml
 toml_config = toml.load("agenta_backend/config.toml")
 
 # Set the environment variables from the TOML configurations
-os.environ["ENVIRONMENT"] = toml_config.get("environment", "development")
 os.environ["DOCKER_REGISTRY_URL"] = toml_config["docker_registry_url"]
 os.environ["REGISTRY"] = toml_config["registry"]
 os.environ["DATABASE_URL"] = toml_config["database_url"]
 
 
 class Settings(BaseSettings):
-    environment: str
     docker_registry_url: str
     registry: str
     database_url: str
+
 
 settings = Settings()
