08b1339a915523865548a207e08e5d2c787cfb70
aakrem <akrem.abayed@gmail.com>
2023-07-11 07:48:44 +0100

    add code snippets for upload and add testsets

---
+++ agenta-web/src/code_snippets/testsets/create_with_json/python.ts
@@ -0,0 +1,19 @@
+export default function pythonCode(uri: string, params: string): string {
+    return `import requests
+import json
+
+url = '${uri}'
+data = {
+    "name": "your_dataset_name",
+    "csvdata": [
+        {"column1": "value1", "column2": "value2"},
+        {"column1": "value3", "column2": "value4"}
+    ]
+}
+
+response = requests.post(url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+
+print(response.status_code)
+print(response.json())
+`;
+}
