be3d80d67c074698da6cf626826f4826971badf6
aakrem <akrem.abayed@gmail.com>
2023-07-11 07:50:59 +0100

    refactor [variant_id] page to use dynamiccodeblock

--- agenta-web/src/pages/apps/[app_name]/endpoints/[variant_id]/index.tsx
+++ agenta-web/src/pages/apps/[app_name]/endpoints/[variant_id]/index.tsx
@@ -1,8 +1,5 @@
-import CodeBlock from "@/components/CodeBlock/CodeBlock";
 import { Parameter, Variant } from "@/lib/Types";
 import { useVariant } from "@/lib/hooks/useVariant";
-import { DownOutlined } from "@ant-design/icons";
-import { Button, Dropdown, MenuProps, Space } from "antd";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
@@ -10,20 +7,12 @@ import pythonCode from '../../../../../code_snippets/endpoints/python';
 import cURLCode from '../../../../../code_snippets/endpoints/curl';
 import tsCode from '../../../../../code_snippets/endpoints/typescript';
 
-type LanguageItem = { displayName: string; languageKey: string };
+import DynamicCodeBlock from '../../../../../components/DynamicCodeBlock/DynamicCodeBlock';
 
 export default function VariantEndpoint() {
     const router = useRouter();
     const appName = router.query.app_name?.toString() || "";
     const variantName = router.query.variant_id?.toString() || "";
-
-    const supportedLanguages: LanguageItem[] = [
-        { displayName: 'Python', languageKey: 'python' },
-        { displayName: 'cURL', languageKey: 'bash' },
-        { displayName: 'TypeScript', languageKey: 'typescript' },
-    ];
-
-    const [selectedLanguage, setSelectedLanguage] = useState<LanguageItem>(supportedLanguages[0]);
 
     const createVariant = (variantName: string): Variant => {
         return {
@@ -60,19 +49,10 @@ export default function VariantEndpoint() {
 
     const uri = `${process.env.NEXT_PUBLIC_AGENTA_API_URL}/${URIPath}/generate`
 
-    const codeStrings: Record<string, string> = {
+    const codeSnippets: Record<string, string> = {
         Python: pythonCode(uri, params),
         cURL: cURLCode(uri, params),
         TypeScript: tsCode(uri, params),
-    };
-
-    const items: MenuProps['items'] = supportedLanguages.map((languageItem, index) => ({
-        key: (index + 1).toString(),
-        label: languageItem.displayName,
-    }));
-
-    const handleMenuClick = ({ key }: { key: string }) => {
-        setSelectedLanguage(supportedLanguages[parseInt(key, 10) - 1]);
     };
 
     return (
@@ -80,26 +60,9 @@ export default function VariantEndpoint() {
             <div>
                 <h3>Variant Name: {variant.variantName}</h3>
             </div>
-            <div style={{ backgroundColor: '#282c34', borderRadius: 10 }}>
-                <div style={{ paddingTop: '7px', paddingRight: '10px', display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
-                    <Space direction="vertical">
-                        <Space wrap>
-                            <Dropdown
-                                menu={{ items, onClick: handleMenuClick }}
-                                placement="bottomLeft"
-                            >
-                                <Button size="small">
-                                    <Space>
-                                        {selectedLanguage.displayName}
-                                        <DownOutlined />
-                                    </Space>
-                                </Button>
-                            </Dropdown>
-                        </Space>
-                    </Space>
-                </div>
-                <CodeBlock language={selectedLanguage.languageKey} value={codeStrings[selectedLanguage.displayName]} />
-            </div>
+            <DynamicCodeBlock
+                codeSnippets={codeSnippets}
+            />
         </div>
     );
 }
