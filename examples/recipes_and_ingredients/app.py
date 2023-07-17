553ff0a4d2e4435bd16b85764dbe525cc596556f
aakrem <akrem.abayed@gmail.com>
2023-07-17 14:22:21 +0100

    remove strip from agenta-web and move it to the llm app

--- examples/recipes_and_ingredients/app.py
+++ examples/recipes_and_ingredients/app.py
@@ -6,7 +6,15 @@ from langchain.prompts import PromptTemplate
 import os
 
 
-default_prompt = """What is the dominant ingredient in this recipe {recipe_name}? Answer in one word!!!!"""
+default_prompt = """
+What is the dominant ingredient in this recipe {recipe_name}?
+Answer in one word that contains only aphabetical letters and without a dot at the end.
+Remove dots, spaces, return to line, tab space characters and all invisible non printable before the word.
+Remove dots, spaces, return to line, tab space characters and all invisible non printable after the word.
+Remove all Carriage Return (ASCII 13, \r ) Line Feed (ASCII 10, \n ) characters.
+ANSWER IN ONE SINGLE WORD WITHOUT ANY POSSIBLE INVISIBLE CHARACTER!!!.
+REMOVE ALL NEWLINE CHARACTER, LINE BREAK, ENDOF LINE (EOL) OR "\n"
+"""
 
 
 @post
@@ -19,4 +27,4 @@ def generate(recipe_name: str, prompt_template: TextParam = default_prompt, temp
 
     chain = LLMChain(llm=llm, prompt=prompt)
     output = chain.run(recipe_name=recipe_name)
-    return output
+    return output.strip()
