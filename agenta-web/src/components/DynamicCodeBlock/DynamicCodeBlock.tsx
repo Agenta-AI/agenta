d73797065c7802e9d79b1328f65ea1ef961151c5
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-07-23 17:39:13 +0200

    style improvements

--- agenta-web/src/components/DynamicCodeBlock/DynamicCodeBlock.tsx
+++ agenta-web/src/components/DynamicCodeBlock/DynamicCodeBlock.tsx
@@ -1,6 +1,6 @@
-import CodeBlock from "@/components/CodeBlock/CodeBlock";
-import { MenuProps, Dropdown, Button, Space } from "antd";
-import { DownOutlined } from "@ant-design/icons";
+import CodeBlock from "@/components/DynamicCodeBlock/CodeBlock";
+import { MenuProps, Dropdown, Button, Row, Col, Space } from "antd";
+import { DownOutlined, ApiOutlined } from "@ant-design/icons";
 import { useEffect, useState } from "react";
 import { LanguageItem, Variant } from "@/lib/Types";
 
@@ -50,45 +50,63 @@ const DynamicCodeBlock: React.FC<DynamicCodeBlockProps> = ({ codeSnippets, inclu
             onVariantChange?.(key);
         }
     };
-
+    const copyToClipboard = async (e: React.MouseEvent) => {
+        e.preventDefault();
+        try {
+            await navigator.clipboard.writeText(codeSnippets[selectedLanguage.displayName]);
+        } catch (err) {
+            console.error("Failed to copy text to clipboard");
+        }
+    };
     return (
-        <div style={{ backgroundColor: '#282c34', borderRadius: 10 }}>
-            <div style={{ paddingTop: '7px', paddingRight: '10px', display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
-                <Space direction="vertical">
-                    <Space wrap>
+
+        <div style={{ backgroundColor: '#FFFFFF', borderRadius: 10, display: 'flex', flexDirection: 'column' }}>
+            <div style={{ fontSize: '1.5em', marginBottom: '25px' }}> {/* Large font similar to h3 */}
+                <ApiOutlined /> API endpoint
+            </div>
+            <div style={{ margin: '5px 0px' }}>
+                Select a variant then use this endpoint to send requests to the LLM app.
+            </div>
+            <div style={{ paddingTop: '20px', marginBottom: '5px', display: 'flex', justifyContent: 'space-between' }}>
+                <div style={{ display: 'flex', alignItems: 'center', fontSize: '1.2em', width: '20%' }}> {/* Larger font */}
+                    <div style={{ marginRight: '10px' }}>Variant:</div>
+                    {includeVariantsDropdown &&
+                        <Dropdown menu={{ items: variantsItems, onClick: handleVariantClick }}>
+                            <Button style={{ marginLeft: 5, width: '100%' }} size='small'>
+                                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
+                                    {selectedVariant?.variantName || 'Select a variant'}
+                                    <DownOutlined />
+                                </div>
+                            </Button>
+                        </Dropdown>}
+                </div>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', width: '50%', marginRight: '10px' }}>
+                    <div style={{ fontSize: '1em', marginRight: '10px' }}>Language:</div>
                     {selectedLanguage && (
-                        <Dropdown
-                            menu={{ items, onClick: handleMenuClick }}
-                            placement="bottomLeft"
-                        >
+                        <Dropdown menu={{ items, onClick: handleMenuClick }} placement="bottomLeft">
                             <Button size="small">
                                 <Space>
                                     {selectedLanguage.displayName}
                                     <DownOutlined />
                                 </Space>
                             </Button>
-                        </Dropdown>)}
-                        {includeVariantsDropdown &&
-                            <Dropdown menu={{ items: variantsItems, onClick: handleVariantClick }}>
-                                <Button style={{ marginRight: 10, width: '100%' }} size='small'>
-                                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
-                                        {selectedVariant?.variantName || 'Select a variant'}
-                                        <DownOutlined />
-                                    </div>
-                                </Button>
-                            </Dropdown>}
-                    </Space>
-                </Space>
+                        </Dropdown>
+                    )}
+                    <Button type="primary" onClick={copyToClipboard} size="small" style={{ marginLeft: '15px' }}>
+                        Copy
+                    </Button>
+                </div>
             </div>
             {selectedLanguage && (
-            <CodeBlock
-                key={selectedLanguage.languageKey + selectedVariant?.variantName}
-                language={selectedLanguage.languageKey}
-                value={codeSnippets[selectedLanguage.displayName]}
-            />
+                <CodeBlock
+                    key={selectedLanguage.languageKey + selectedVariant?.variantName}
+                    language={selectedLanguage.languageKey}
+                    value={codeSnippets[selectedLanguage.displayName]}
+                />
             )}
         </div>
     );
 };
+
 
 export default DynamicCodeBlock;
