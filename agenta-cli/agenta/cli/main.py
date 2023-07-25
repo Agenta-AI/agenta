31a2b71467c6932da3d9a44f0f5c7f023b0ea3b6
Abram <israelvictory87@gmail.com>
2023-07-25 23:55:15 +0100

    Removed .env and .venv from default environment files to exclude

--- agenta-cli/agenta/cli/main.py
+++ agenta-cli/agenta/cli/main.py
@@ -95,7 +95,7 @@ def init(app_name: str):
         sys.exit(0)
     
     # Create a .gitignore file and add some default environment folder names to it
-    gitignore_content = "# Environments \n.env\n.venv\nenv/\nvenv/\nENV/\nenv.bak/\nvenv.bak/"
+    gitignore_content = "# Environments \nenv/\nvenv/\nENV/\nenv.bak/\nvenv.bak/"
     with open('.gitignore', 'w') as gitignore_file:
         gitignore_file.write(gitignore_content)
         
