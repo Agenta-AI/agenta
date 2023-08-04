816f2e195fea9d6b43d947b6932ebcdafb48f258
witehound <netvid00@gmail.com>
2023-08-04 14:55:55 +0100

    Added agenta's logo in the favicon and set a title

--- agenta-web/src/components/Logo/Logo.tsx
+++ agenta-web/src/components/Logo/Logo.tsx
@@ -1,16 +1,10 @@
 import Image from "next/image"
 import {useMemo} from "react"
 import {useAppTheme} from "../Layout/ThemeContextProvider"
+import useLogo from "@/lib/hooks/useLogo"
 const Logo: React.FC = () => {
-    const {appTheme} = useAppTheme()
+    const logoSrc = useLogo()
 
-    const logoSrc = useMemo(
-        () =>
-            appTheme === "dark"
-                ? "/assets/dark-complete-transparent-CROPPED.png"
-                : "/assets/light-complete-transparent-CROPPED.png",
-        [appTheme],
-    )
     return <Image src={logoSrc} alt="Agenta Logo" width={129} height={42.5} />
 }
 
