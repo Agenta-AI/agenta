8d9c7951b372f84875a1d49e31c9c213bc4993c7
witehound <netvid00@gmail.com>
2023-08-05 00:29:22 +0100

    fixed  New evaluation form is not responsive #202"

--- agenta-web/src/components/Evaluations/Evaluations.tsx
+++ agenta-web/src/components/Evaluations/Evaluations.tsx
@@ -27,6 +27,7 @@ export default function Evaluations() {
     const [areAppVariantsLoading, setAppVariantsLoading] = useState(false)
     const [isError, setIsError] = useState<boolean | string>(false)
     const [variants, setVariants] = useState<any[]>([])
+    const [screenSize, setScreenSize] = useState("")
     const [columnsCount, setColumnsCount] = useState(2)
     const [selectedTestset, setSelectedTestset] = useState<{
         _id?: string
@@ -91,6 +92,13 @@ export default function Evaluations() {
             setTestsetsList(testsets)
         }
     }, [testsets, isTestsetsLoadingError])
+
+    useEffect(() => {
+        window.addEventListener("resize", detectSize)
+        return () => {
+            window.removeEventListener("resize", detectSize)
+        }
+    }, [screenSize])
 
     // TODO: move to api.ts
     const createNewEvaluation = async (
@@ -267,6 +275,12 @@ export default function Evaluations() {
         setSliderValue(value)
     }
 
+    const detectSize = () => {
+        setScreenSize(
+            window.innerWidth < 850 ? "120px" : window.innerWidth < 1030 ? "100px" : "56px",
+        )
+    }
+
     return (
         <div>
             <div>
@@ -345,36 +359,47 @@ export default function Evaluations() {
                         </Radio.Group>
                     </Col>
                     <Col span={8}>
-                        <div>
-                            <Title level={4}>2. Which variants would you like to evaluate</Title>
-                            {Array.from({length: numberOfVariants}).map((_, index) => (
-                                <Dropdown key={index} menu={getVariantsDropdownMenu(index)}>
-                                    <Button
+                        <Title
+                            level={4}
+                            style={{
+                                minHeight: screenSize,
+                            }}
+                        >
+                            2. Which variants would you like to evaluate
+                        </Title>
+                        {Array.from({length: numberOfVariants}).map((_, index) => (
+                            <Dropdown key={index} menu={getVariantsDropdownMenu(index)}>
+                                <Button
+                                    style={{
+                                        marginRight: 10,
+                                        marginTop: index === 0 ? 40 : 10,
+                                        width: "100%",
+                                    }}
+                                >
+                                    <div
                                         style={{
-                                            marginRight: 10,
-                                            marginTop: index === 0 ? 40 : 10,
+                                            display: "flex",
+                                            justifyContent: "space-between",
+                                            alignItems: "center",
                                             width: "100%",
                                         }}
                                     >
-                                        <div
-                                            style={{
-                                                display: "flex",
-                                                justifyContent: "space-between",
-                                                alignItems: "center",
-                                                width: "100%",
-                                            }}
-                                        >
-                                            {selectedVariants[index]?.variantName ||
-                                                "Select a variant"}
-                                            <DownOutlined />
-                                        </div>
-                                    </Button>
-                                </Dropdown>
-                            ))}
-                        </div>
+                                        {selectedVariants[index]?.variantName || "Select a variant"}
+                                        <DownOutlined />
+                                    </div>
+                                </Button>
+                            </Dropdown>
+                        ))}
                     </Col>
                     <Col span={8}>
-                        <Title level={4}>3. Which testset you want to use?</Title>
+                        <Title
+                            level={4}
+                            style={{
+                                minHeight: screenSize,
+                            }}
+                        >
+                            3. Which testset you want to use?
+                        </Title>
 
                         <Dropdown menu={getTestsetDropdownMenu()}>
                             <Button style={{marginRight: 10, marginTop: 40, width: "100%"}}>
