8c9a0b77e0c35f754d9b651ed769895fe26daf59
sohaibanwaar <sohaibanwaar36@gmail.com>
2023-08-04 14:11:22 +0500

    fix: AB Testing fixed

--- agenta-web/src/lib/services/api.ts
+++ agenta-web/src/lib/services/api.ts
@@ -307,7 +307,7 @@ export const deleteAppEvaluations = async (ids: string[]) => {
     }
 }
 
-export const loadEvaluationsRows = async (
+export const loadEvaluationsScenarios = async (
     evaluationTableId: string,
     appEvaluation: AppEvaluation,
 ) => {
