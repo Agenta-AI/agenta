7918730044dec704298a570f793f7b36004f77e4
Abram <israelvictory87@gmail.com>
2023-08-06 22:10:57 +0100

    Refactored code into separate modules - routers/containers_router.py and services/containers_manager.py

--- agenta-backend/agenta_backend/services/container_manager.py
+++ agenta-backend/agenta_backend/services/container_manager.py
@@ -1,18 +1,14 @@
-from fastapi import FastAPI, UploadFile, HTTPException, BackgroundTasks, APIRouter
-from fastapi.responses import JSONResponse
-from pathlib import Path
-from docker import DockerClient
-import tarfile
-import os
-import uuid
-import logging
 import docker
+import tarfile
+import logging
+from pathlib import Path
+from fastapi import HTTPException
 from agenta_backend.models.api.api_models import Image
+
 
 client = docker.from_env()
 
 
-router = APIRouter()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
@@ -67,29 +63,3 @@ def build_image_job(
         raise HTTPException(status_code=500, detail=str(ex) + "\n" + log)
     except Exception as ex:
         raise HTTPException(status_code=500, detail=str(ex))
-
-
-@router.post("/build_image/")
-async def build_image(app_name: str, variant_name: str, tar_file: UploadFile) -> Image:
-    """Takes a tar file and builds a docker image from it
-
-    Arguments:
-        app_name -- _description_
-        variant_name -- _description_
-        tar_file -- _description_
-
-    Returns:
-        _description_
-    """
-    # Create a unique temporary directory for each upload
-    temp_dir = Path(f"/tmp/{uuid.uuid4()}")
-    temp_dir.mkdir(parents=True, exist_ok=True)
-
-    # Save uploaded file to the temporary directory
-    tar_path = temp_dir / tar_file.filename
-    with tar_path.open("wb") as buffer:
-        buffer.write(await tar_file.read())
-
-    image_name = f"agenta-server/{app_name.lower()}_{variant_name.lower()}:latest"
-
-    return build_image_job(app_name, variant_name, tar_path, image_name, temp_dir)
