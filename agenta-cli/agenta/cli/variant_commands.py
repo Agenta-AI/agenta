8e612bb6f9d2a5f4d0a9c584a3ef2ccce33b23bb
Abram <israelvictory87@gmail.com>
2023-08-06 20:53:52 +0100

    Update - modify CLI progress updates

--- agenta-cli/agenta/cli/variant_commands.py
+++ agenta-cli/agenta/cli/variant_commands.py
@@ -89,13 +89,11 @@ def add_variant(app_folder: str, file_name: str, host: str) -> str:
     if not overwrite:
         config["variants"].append(variant_name)
     try:
-        click.echo(click.style(f"\n\nPreparing variant {variant_name} into a tar file...", fg="yellow"))
+        click.echo(click.style(f"Preparing variant {variant_name} into a tar file...", fg="yellow"))
         tar_path = build_tar_docker_container(folder=app_path, file_name=file_name)
-        click.echo(click.style(f"Successfully prepared variant {variant_name} into a tar file!", fg="green"))
         
-        click.echo(click.style(f"\n\nBuilding variant {variant_name} into a docker image...", fg="yellow"))
+        click.echo(click.style(f"Building variant {variant_name} into a docker image...", fg="yellow"))
         image: Image = client.send_docker_tar(app_name, variant_name, tar_path, host)
-        click.echo(click.style(f"Successfully built variant {variant_name} into a docker image!", fg="green"))
         # docker_image: DockerImage = build_and_upload_docker_image(
         #     folder=app_path, app_name=app_name, variant_name=variant_name)
     except Exception as ex:
@@ -103,13 +101,11 @@ def add_variant(app_folder: str, file_name: str, host: str) -> str:
         return None
     try:
         if overwrite:
-            click.echo(click.style(f"\n\nUpdating variant {variant_name} image...", fg="yellow"))
+            click.echo(click.style(f"Updating variant {variant_name} to server...", fg="yellow"))
             client.update_variant_image(app_name, variant_name, image, host)
-            click.echo(click.style(f"Successfully updated and added variant {variant_name} to server!", fg="green"))
         else:
-            click.echo(click.style(f"\n\nAdding variant {variant_name} to server...", fg="yellow"))
+            click.echo(click.style(f"Adding variant {variant_name} to server...", fg="yellow"))
             client.add_variant_to_server(app_name, variant_name, image, host)
-            click.echo(click.style(f"Successfully added variant {variant_name} to server!\n\n", fg="green"))
     except Exception as ex:
         if overwrite:
             click.echo(click.style(f"Error while updating variant: {ex}", fg="red"))
@@ -265,7 +261,7 @@ def config_check(app_folder: str):
         app_folder -- the app folder
     """
     
-    click.echo(click.style("\n\nChecking and updating config file...", fg="yellow"))
+    click.echo(click.style("\nChecking and updating config file...", fg="yellow"))
     app_folder = Path(app_folder)
     config_file = app_folder / "config.toml"
     if not config_file.exists():
@@ -277,9 +273,7 @@ def config_check(app_folder: str):
         )
         return
     host = get_host(app_folder)  # TODO: Refactor the whole config thing
-
     helper.update_config_from_backend(config_file, host=host)
-    click.echo(click.style("Successfully updated config from backend!", fg="green"))
 
 
 def get_host(app_folder: str) -> str:
@@ -317,9 +311,6 @@ def serve_cli(app_folder: str, file_name: str):
         click.echo(click.style(f"{error_msg}", fg='red'))
         sys.exit(0)
         
-    click.echo(
-        click.style(f"Serving variant {file_name.removesuffix('.py')} to Playground...", fg="yellow")
-    )
     try:
         config_check(app_folder)
         host = get_host(app_folder)
