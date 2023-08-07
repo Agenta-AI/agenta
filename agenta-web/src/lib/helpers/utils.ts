10241acfa481f00f7935723d40fef03e02d265c5
Suad Suljovic <suad.suljovic92@gmail.com>
2023-08-07 12:26:28 +0200

    create open ai functions

--- agenta-web/src/lib/helpers/utils.ts
+++ agenta-web/src/lib/helpers/utils.ts
@@ -1,5 +1,7 @@
 import {EvaluationType} from "../enums"
 
+const openAItoken = "openAiToken"
+
 export const renameVariables = (name: string) => {
     return name.charAt(0).toUpperCase() + name.slice(1).replace(/_/g, " ")
 }
@@ -18,4 +20,26 @@ export const EvaluationTypeLabels: Record<EvaluationType, string> = {
     [EvaluationType.auto_ai_critique]: "AI Critic",
     [EvaluationType.human_a_b_testing]: "A/B testing",
     [EvaluationType.human_scoring]: "Scoring single variant",
+}
+
+export const saveOpenAIKey = (key: string) => {
+    if (typeof window !== "undefined") {
+        // TODO: add encryption here
+        localStorage.setItem(openAItoken, key)
+    }
+}
+
+export const getOpenAIKey = (): string => {
+    let token: string | null = ""
+
+    if (typeof window !== "undefined") {
+        // TODO: add decryption here
+        token = localStorage.getItem(openAItoken)
+    }
+
+    return token ?? ""
+}
+
+export const removeOpenAIKey = () => {
+    localStorage.removeItem(openAItoken)
 }
