cc6e6517749c6f6544e2bca4ab9abc167f53a1fe
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-08-07 13:27:34 +0200

    Update SDK

--- agenta-cli/agenta/__init__.py
+++ agenta-cli/agenta/__init__.py
@@ -1,4 +1,4 @@
 from . import sdk
 from .sdk import post, ingest, app
-from .sdk.types import TextParam, FloatParam, InFile, Context, MultipleChoiceParam
+from .sdk.types import TextParam, FloatParam, InFile, Context, MultipleChoiceParam, DictInput
 from .sdk.context import save_context, get_contexts
