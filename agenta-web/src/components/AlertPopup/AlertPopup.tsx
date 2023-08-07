857fe104b7b25ee8d81eb0505c55de6e76e89013
MohammedMaaz <maazkhawar25@gmail.com>
2023-08-07 14:18:19 +0500

    added alert popup to block navigation

---
+++ agenta-web/src/components/AlertPopup/AlertPopup.tsx
@@ -0,0 +1,49 @@
+import React from "react"
+import {Modal, ModalFuncProps} from "antd"
+import {ExclamationCircleOutlined} from "@ant-design/icons"
+
+function handleCb(cb: AlertPopupProps["onOk"]) {
+    if (typeof cb !== "function") return cb
+    return function () {
+        const res = cb()
+        if (res instanceof Promise) {
+            return new Promise((_res) => {
+                res.catch(console.error).finally(() => _res(undefined))
+            })
+        }
+        return res
+    }
+}
+
+export type AlertPopupProps = ModalFuncProps & {
+    message: string
+    cancellable?: boolean
+}
+
+export default function AlertPopup({
+    title,
+    message,
+    okText = "Yes",
+    cancelText = "Cancel",
+    onOk,
+    onCancel,
+    cancellable = true,
+    ...ModalProps
+}: AlertPopupProps) {
+    return Modal.confirm({
+        title,
+        content: message,
+        okText,
+        cancelText,
+        onOk: handleCb(onOk),
+        onCancel: handleCb(onCancel),
+        closable: cancellable,
+        maskClosable: cancellable,
+        cancelButtonProps: {
+            style: cancelText === null ? {display: "none"} : undefined,
+        },
+        icon: <ExclamationCircleOutlined />,
+        okType: "default",
+        ...ModalProps,
+    })
+}
