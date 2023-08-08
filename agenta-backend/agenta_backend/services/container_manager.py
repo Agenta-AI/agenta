e76f908ff9772554380e03f5c2ec7db1eb84f205
Abram <israelvictory87@gmail.com>
2023-08-08 14:51:45 +0100

    Update - clean up container_manager module

--- agenta-backend/agenta_backend/services/container_manager.py
+++ agenta-backend/agenta_backend/services/container_manager.py
@@ -1,5 +1,5 @@
+import shutil
 import docker
-import tarfile
 import logging
 from pathlib import Path
 from fastapi import HTTPException
@@ -14,7 +14,11 @@ logger.setLevel(logging.INFO)
 
 
 def build_image_job(
-    app_name: str, variant_name: str, tar_path: Path, image_name: str, temp_dir: Path
+    app_name: str,
+    variant_name: str,
+    tar_path: Path,
+    image_name: str,
+    temp_dir: Path
 ) -> Image:
     """Business logic for building a docker image from a tar file
     TODO: This should be a background task
@@ -35,31 +39,25 @@ def build_image_job(
     Returns:
         _description_
     """
+
     # Extract the tar file
-    with tarfile.open(tar_path) as tar:
-        tar.extractall(path=temp_dir)
+    shutil.unpack_archive(tar_path, temp_dir)
 
     # Build the docker image
     try:
         image, build_log = client.images.build(
             path=str(temp_dir),
             tag=image_name,
-            buildargs={
-                "ROOT_PATH": f"/{app_name}/{variant_name}"
-            },  # needed for /docs to work
-            rm=True,  # Remove intermediate containers after a successful build
+            buildargs={"ROOT_PATH": f"/{app_name}/{variant_name}"},
+            rm=True,
         )
-        # response = [line for line in build_log]
         for line in build_log:
             logger.info(line)
         return Image(docker_id=image.id, tags=image.tags[0])
-        # TODO: Add remove the temp dir and the tar file
-        # return JSONResponse(content={"message": "Image built successfully", "image": str(response)})
     except docker.errors.BuildError as ex:
         log = "Error building Docker image:\n"
-        for line in ex.build_log:
-            log += str(line) + "\n"
+        log += str(ex) + "\n"
         logger.error(log)
-        raise HTTPException(status_code=500, detail=str(ex) + "\n" + log)
+        raise HTTPException(status_code=500, detail=log)
     except Exception as ex:
         raise HTTPException(status_code=500, detail=str(ex))
