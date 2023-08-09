400be73349329a06116c51e4be6bb772d4491271
Kaosiso <mac@MACs-MacBook-Pro.local>
2023-08-09 01:15:40 +0100

    modifies Apps view look

--- agenta-web/src/components/AppSelector/AppSelector.tsx
+++ agenta-web/src/components/AppSelector/AppSelector.tsx
@@ -1,8 +1,11 @@
 import {useState} from "react"
 import {useRouter} from "next/router"
-import {Input, Space, Modal} from "antd"
+import {Input, Space, Modal, ConfigProvider, theme, Spin, Button} from "antd"
 import useSWR from "swr"
 import AppCard from "./AppCard"
+import Logo from "../Logo/Logo"
+import {useAppTheme} from "../Layout/ThemeContextProvider"
+import {CloseCircleFilled} from "@ant-design/icons"
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
@@ -10,6 +13,7 @@ const AppSelector: React.FC = () => {
     const [newApp, setNewApp] = useState("")
     const router = useRouter()
     const [isModalOpen, setIsModalOpen] = useState(false)
+    const {appTheme} = useAppTheme()
 
     const showAddModal = () => {
         setIsModalOpen(true)
@@ -29,18 +33,156 @@ const AppSelector: React.FC = () => {
         fetcher,
     )
 
-    if (error) return <div>failed to load</div>
-    if (isLoading) return <div>loading...</div>
-
     return (
-        <div>
-            <div style={{margin: "20px 20px"}}>
-                <Space size={20} wrap direction="horizontal">
-                    {Array.isArray(data) &&
-                        data.map((app: any, index: number) => (
-                            <AppCard appName={app.app_name} key={index} index={index} />
-                        ))}
-                </Space>
+        <ConfigProvider
+            theme={{
+                algorithm: appTheme === "dark" ? theme.darkAlgorithm : theme.defaultAlgorithm,
+            }}
+        >
+            <div
+                style={{
+                    maxWidth: "1000px",
+                    margin: "10px auto",
+                    width: "100%",
+                    color: appTheme === "dark" ? "#fff" : "#000",
+                }}
+            >
+                <div>
+                    <Logo />
+
+                    <h1 style={{fontSize: "42px", margin: "20px 0"}}>
+                        Welcome to <span style={{color: "#0e9c1a"}}>Agenta</span>
+                    </h1>
+                    <h2
+                        style={{
+                            fontSize: "24px",
+                            margin: "20px 0",
+                            borderBottom: "1px solid #0e9c1a",
+                            paddingBottom: "1rem",
+                        }}
+                    >
+                        The developer-first open source LLMOps platform.
+                    </h2>
+
+                    <div
+                        style={{
+                            padding: "0 20px",
+                            lineHeight: 1.7,
+                            marginBottom: "2rem",
+                        }}
+                    >
+                        <p>
+                            Agenta is an open-source developer first LLMOps platform to streamline
+                            the process of building LLM-powered applications. Building LLM-powered
+                            apps is an iterative process with lots of prompt-engineering and testing
+                            multiple variants.
+                            <br />
+                            Agenta brings the CI/CD platform to this process by enabling you to
+                            quickly iterate, experiment, evaluate, and optimize your LLM apps. All
+                            without imposing any restrictions on your choice of framework, library,
+                            or model.
+                            <br />
+                        </p>
+
+                        <div>
+                            <h2
+                                style={{
+                                    fontSize: "20px",
+                                }}
+                            >
+                                How Agenta works:
+                            </h2>
+
+                            <ul>
+                                <li>
+                                    Develop your LLM-powered application as you would normally do.
+                                    Feel free to use any framework, library, or model (langchain,
+                                    llma_index, GPT-3, or open-source models).
+                                </li>
+                                <li>
+                                    With two lines of code, specify the parameters for your
+                                    experiment.
+                                </li>
+                                <li>Deploy your app using the Agenta CLI.</li>
+                                <li>
+                                    You or your team can iterate, version parameters, test different
+                                    versions, and run systematic evaluations via a user-friendly web
+                                    platform.
+                                </li>
+                            </ul>
+
+                            <span
+                                style={{
+                                    fontWeight: 600,
+                                    fontSize: 15,
+                                    textTransform: "uppercase",
+                                }}
+                            >
+                                Read{" "}
+                                <a href="https://docs.agenta.ai/introduction" target="_blank">
+                                    Documentation
+                                </a>{" "}
+                                on how to get started.
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+                <div
+                    style={{
+                        borderTop: "1px solid #0e9c1a",
+                        paddingTop: "1rem",
+                        margin: "20px 0",
+                    }}
+                >
+                    <h1 style={{fontSize: 24}}>LLM Applications</h1>
+
+                    {isLoading ? (
+                        <div className="appSelectorMssg">
+                            <Spin />
+                            <h1>loading...</h1>
+                        </div>
+                    ) : error ? (
+                        <div className="appSelectorMssg">
+                            <CloseCircleFilled style={{fontSize: 20, color: "red"}} />
+                            <h1>failed to load</h1>
+                        </div>
+                    ) : Array.isArray(data) && data.length ? (
+                        <Space size={20} wrap direction="horizontal">
+                            {Array.isArray(data) &&
+                                data.map((app: any, index: number) => (
+                                    <AppCard appName={app.app_name} key={index} index={index} />
+                                ))}
+                        </Space>
+                    ) : (
+                        <div
+                            className="appSelectorEmpty"
+                            style={{backgroundColor: appTheme === "dark" ? "#111a2c" : "#e6f4ff"}}
+                        >
+                            <h1 style={{fontSize: 20}}>Get started creating your first LLM App</h1>
+
+                            <p>
+                                This guide assumes you have completed the installation process. If
+                                not, please follow our{" "}
+                                <a href="https://docs.agenta.ai/installation" target="_blank">
+                                    installation guide
+                                </a>
+                                .
+                            </p>
+
+                            <Button
+                                style={{
+                                    backgroundColor: "#1677ff",
+                                    border: "none",
+                                    color: "#fff",
+                                }}
+                            >
+                                Create New App
+                            </Button>
+                        </div>
+                    )}
+                </div>
+
                 <Modal
                     title="Add new app from template"
                     open={isModalOpen}
@@ -54,7 +196,7 @@ const AppSelector: React.FC = () => {
                     />
                 </Modal>
             </div>
-        </div>
+        </ConfigProvider>
     )
 }
 
