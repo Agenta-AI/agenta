9b72da2de31c91c9525738283ad54fbc15023e9a
MohammedMaaz <maazkhawar25@gmail.com>
2023-08-10 13:31:21 +0500

    feat: cypress setup, add testset test suite

--- agenta-web/src/lib/helpers/utils.ts
+++ agenta-web/src/lib/helpers/utils.ts
@@ -51,3 +51,13 @@ export const capitalize = (s: string) => {
         .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
         .join(" ")
 }
+
+export const randString = (len: number) =>
+    window
+        .btoa(
+            Array.from(window.crypto.getRandomValues(new Uint8Array(len * 2)))
+                .map((b) => String.fromCharCode(b))
+                .join(""),
+        )
+        .replace(/[+/]/g, "")
+        .substring(0, len)
