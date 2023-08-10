351aa728486dbcd958434e1d91c5f7d235d9ea37
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-08-10 20:10:05 +0100

    modifies mdx image to display based on appTheme

--- agenta-web/src/components/AppSelector/TipsAndFeatures.tsx
+++ agenta-web/src/components/AppSelector/TipsAndFeatures.tsx
@@ -7,8 +7,9 @@ import {MDXProvider} from "@mdx-js/react"
 import slide1 from "./TipsMarkdown/tip1.mdx"
 import slide2 from "./TipsMarkdown/tip2.mdx"
 import slide3 from "./TipsMarkdown/tip3.mdx"
+import slide4 from "./TipsMarkdown/tip4.mdx"
 
-const slides = [slide1, slide2, slide3]
+const slides = [slide3, slide1, slide2, slide4]
 
 const TipsAndFeatures = () => {
     const {appTheme} = useAppTheme()
@@ -18,10 +19,12 @@ const TipsAndFeatures = () => {
         setActiveIndex(index)
     }
 
-    const getImagePath = () => {
-        return appTheme === "dark"
-            ? "/assets/tipsImages/testsets-dark.png"
-            : "/assets/tipsImages/testsets-light.png"
+    const getImagePath = (img: any) => {
+        const theme = appTheme === "dark" ? "dark" : "light"
+
+        const imgSrc = `/assets/tipsImages/${img}-${theme}.png`
+
+        return imgSrc
     }
 
     return (
@@ -60,15 +63,14 @@ const TipsAndFeatures = () => {
             <div
                 style={{
                     borderRadius: 10,
-                    //   maxWidth: 800,
                     margin: "10px auto",
                     width: "100%",
-                    //   padding: "10px 30px",
                     lineHeight: 1.6,
-                    //   backgroundColor: appTheme === "dark" ? "rgb(20, 20, 20)" : "rgba(0,0,0,0.1)",
                 }}
             >
-                <MDXProvider components={{img: (props) => <img {...props} src={getImagePath()} />}}>
+                <MDXProvider
+                    components={{img: (props) => <img {...props} src={getImagePath(props.src)} />}}
+                >
                     {slides.map((Slide, index) => (
                         <div
                             key={index}
