3e0dd20bf13d8b46a9e0c3aca43305576dac97f1
Abram <israelvictory87@gmail.com>
2023-08-18 00:28:31 +0100

    Update - added default values for timestamp model fields

--- agenta-backend/agenta_backend/models/api/auth_models.py
+++ agenta-backend/agenta_backend/models/api/auth_models.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel, EmailStr, Optional
+from pydantic import BaseModel, EmailStr, Optional, Field
 from datetime import datetime
 
 
 class TimestampModel(BaseModel):
-    created_at: datetime
-    updated_at: datetime
+    created_at: datetime = Field(datetime.utcnow())
+    updated_at: datetime = Field(datetime.utcnow())
 
 
 class User(TimestampModel):
