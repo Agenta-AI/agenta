e1a7731dd7847a15a26795a5b1f0dc53f49d6eb0
Akrem Abayed <aakrem@users.noreply.github.com>
2023-08-20 11:55:01 +0200

    run prettier and black

--- agenta-web/src/pages/_app.tsx
+++ agenta-web/src/pages/_app.tsx
@@ -3,10 +3,10 @@ import type {AppProps} from "next/app"
 import Layout from "@/components/Layout/Layout"
 import ThemeContextProvider from "@/components/Layout/ThemeContextProvider"
 
-import SuperTokensReact, { SuperTokensWrapper } from 'supertokens-auth-react'
-import { frontendConfig } from './../config/frontendConfig';
+import SuperTokensReact, {SuperTokensWrapper} from "supertokens-auth-react"
+import {frontendConfig} from "./../config/frontendConfig"
 
-if (typeof window !== 'undefined') {
+if (typeof window !== "undefined") {
     SuperTokensReact.init(frontendConfig())
 }
 export default function App({Component, pageProps}: AppProps) {
--- agenta-web/src/pages/apps/index.tsx
+++ agenta-web/src/pages/apps/index.tsx
@@ -1,6 +1,5 @@
 import AppSelector from "@/components/AppSelector/AppSelector"
-import { SessionAuth } from 'supertokens-auth-react/recipe/session'
-
+import {SessionAuth} from "supertokens-auth-react/recipe/session"
 
 export default function Apps() {
     return (
