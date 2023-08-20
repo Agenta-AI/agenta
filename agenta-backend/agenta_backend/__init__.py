53420f680ad2cc46ef3090ee5924e853619a52ce
Akrem Abayed <aakrem@users.noreply.github.com>
2023-08-20 11:55:01 +0200

    run prettier and black

--- agenta-backend/agenta_backend/__init__.py
+++ agenta-backend/agenta_backend/__init__.py
@@ -2,22 +2,34 @@ from supertokens_python import init, InputAppInfo, SupertokensConfig
 from supertokens_python.recipe import passwordless, session, dashboard
 
 from supertokens_python.recipe.passwordless import ContactEmailOnlyConfig
-from supertokens_python.recipe.passwordless.interfaces import APIInterface, APIOptions, ConsumeCodePostOkResult
+from supertokens_python.recipe.passwordless.interfaces import (
+    APIInterface,
+    APIOptions,
+    ConsumeCodePostOkResult,
+)
 from typing import Any, Dict, Union
 
 
 def override_passwordless_apis(original_implementation: APIInterface):
     original_consume_code_post = original_implementation.consume_code_post
 
-    async def consume_code_post(pre_auth_session_id: str,
-                                user_input_code: Union[str, None],
-                                device_id: Union[str, None],
-                                link_code: Union[str, None],
-                                api_options: APIOptions,
-                                user_context: Dict[str, Any]):
-
+    async def consume_code_post(
+        pre_auth_session_id: str,
+        user_input_code: Union[str, None],
+        device_id: Union[str, None],
+        link_code: Union[str, None],
+        api_options: APIOptions,
+        user_context: Dict[str, Any],
+    ):
         # First we call the original implementation of consume_code_post.
-        response = await original_consume_code_post(pre_auth_session_id, user_input_code, device_id, link_code, api_options, user_context)
+        response = await original_consume_code_post(
+            pre_auth_session_id,
+            user_input_code,
+            device_id,
+            link_code,
+            api_options,
+            user_context,
+        )
 
         # Post sign up response, we check if it was successful
         if isinstance(response, ConsumeCodePostOkResult):
@@ -30,11 +42,10 @@ def override_passwordless_apis(original_implementation: APIInterface):
                 # create a new organisation
                 # create a new user(with the same id?)
 
-
-                pass # TODO: Post sign up logic
+                pass  # TODO: Post sign up logic
             else:
                 print("------------- holaaaaa sign iiiin")
-                pass # TODO: Post sign in logic
+                pass  # TODO: Post sign in logic
 
         return response
 
@@ -50,22 +61,20 @@ init(
         # the fact that both are localhost is causing problems with
         # displaying the dashboard to manage users
         api_base_path="/auth/",
-        website_base_path="/auth"
+        website_base_path="/auth",
     ),
     supertokens_config=SupertokensConfig(
         connection_uri="http://supertokens:3567",
     ),
-    framework='fastapi',
+    framework="fastapi",
     recipe_list=[
         session.init(),
         passwordless.init(
             flow_type="USER_INPUT_CODE",
             contact_config=ContactEmailOnlyConfig(),
-            override=passwordless.InputOverrideConfig(
-                apis=override_passwordless_apis
-            ),
+            override=passwordless.InputOverrideConfig(apis=override_passwordless_apis),
         ),
         dashboard.init(),
     ],
-    mode='asgi'
+    mode="asgi",
 )
