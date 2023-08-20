3212f0af6ecb7f90bb8477a3fdfdae3312786c67
Abram <israelvictory87@gmail.com>
2023-08-20 01:30:20 +0100

    Feat - created ISession interface

--- agenta-web/src/lib/Types.ts
+++ agenta-web/src/lib/Types.ts
@@ -123,3 +123,19 @@ export interface AppTemplate {
         OPENAI_API_KEY: string
     }
 }
+
+export interface ISession {
+    loading: boolean
+    doesSessionExist: boolean
+    userId: string
+    invalidClaims: Array<any>
+    accessTokenPayload: {
+        exp: number
+        iat: number
+        iss: string
+        parentRefreshTokenHash1: string
+        refreshTokenHash1: string
+        sessionHandle: string
+        sub: string
+    }
+}
