881a339db871289dcafb0a97e842a6ba0a050838
Abram <israelvictory87@gmail.com>
2023-08-21 21:38:35 +0100

    Update - modified generate_key bashcript

--- agenta-backend/generate_key.sh
+++ agenta-backend/generate_key.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-export ENCRYPTION_KEY=$(openssl rand -hex 32)
-echo "ENCRYPTION_KEY=$ENCRYPTION_KEY"
+
+# Generate the encryption key
+ENCRYPTION_KEY=$(openssl rand -hex 16)
+# Create a JSON file with the encryption key
+echo "{\"encryption_key\": \"$ENCRYPTION_KEY\"}" > encryption_key.json
