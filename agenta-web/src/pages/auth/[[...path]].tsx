c219edab4285e6082f62399bc5214f40dc46de8f
Akrem Abayed <aakrem@users.noreply.github.com>
2023-08-22 18:17:17 +0200

    small fixes

--- agenta-web/src/pages/auth/[[...path]].tsx
+++ agenta-web/src/pages/auth/[[...path]].tsx
@@ -1,18 +1,18 @@
 import React, {useEffect} from "react"
 import dynamic from "next/dynamic"
-import {PasswordlessPreBuiltUI} from "supertokens-auth-react/recipe/passwordless/prebuiltui"
+import { ThirdPartyPasswordlessPreBuiltUI } from "supertokens-auth-react/recipe/thirdpartypasswordless/prebuiltui";
 import {redirectToAuth} from "supertokens-auth-react"
 import {canHandleRoute, getRoutingComponent} from "supertokens-auth-react/ui"
 
 const SuperTokensComponentNoSSR = dynamic<{}>(
-    new Promise((res) => res(() => getRoutingComponent([PasswordlessPreBuiltUI]))),
+    new Promise((res) => res(() => getRoutingComponent([ThirdPartyPasswordlessPreBuiltUI]))),
     {ssr: false},
 )
 
 export default function Auth() {
     // if the user visits a page that is not handled by us (like /auth/random), then we redirect them back to the auth page.
     useEffect(() => {
-        if (canHandleRoute([PasswordlessPreBuiltUI]) === false) {
+        if (canHandleRoute([ThirdPartyPasswordlessPreBuiltUI]) === false) {
             redirectToAuth()
         }
     }, [])
