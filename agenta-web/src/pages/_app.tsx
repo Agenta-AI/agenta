e416ad17125b46d32457cadfc79f72ff5a2f58b0
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-08-22 19:32:34 +0200

    fix post merge

--- agenta-web/src/pages/_app.tsx
+++ agenta-web/src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import "@/styles/globals.css"
-import type { AppProps } from "next/app"
+import type {AppProps} from "next/app"
 import Layout from "@/components/Layout/Layout"
 import ThemeContextProvider from "@/components/Layout/ThemeContextProvider"
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({Component, pageProps}: AppProps) {
     return (
         <ThemeContextProvider>
             <Layout>
