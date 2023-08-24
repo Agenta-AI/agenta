52d85191e73bc2528e441a6f71842c6d889134e1
Abram <israelvictory87@gmail.com>
2023-08-24 10:51:30 +0100

    Feat - implemented create_accounts logic

---
+++ agenta-backend/agenta_backend/services/commoners.py
@@ -0,0 +1,36 @@
+from typing import Union
+from agenta_backend.models.api.user_models import User
+from agenta_backend.models.api.organization_models import Organization
+from agenta_backend.services.user_service import create_new_user
+from agenta_backend.services.organization_service import (
+    create_new_organization,
+)
+
+
+async def create_accounts(payload: dict) -> Union[str, str]:
+    """Creates a user account and an associated organization based on the
+    provided payload.
+    
+    Arguments:
+        payload (dict): The required payload. It consists of; user_id and user_email
+        
+    Returns:
+        a tuple containing the user_id and org_id as strings.
+    """
+    
+    user_dict = {
+        "id": payload["user_id"],
+        "email": payload["user_email"],
+        "username": payload["user_email"].split("@")[0],
+    }
+    organization = Organization(**{"name": user_dict["username"]})
+    org = await create_new_organization(organization)
+
+    if org is not None:
+        user_dict["organization_id"] = str(org.inserted_id)
+        user = User(**user_dict)
+        created_user = await create_new_user(user)
+        user_id, org_id = str(created_user["id"]), str(
+            created_user["organization_id"]
+        )
+        return user_id, org_id
