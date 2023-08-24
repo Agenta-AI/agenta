07c9068e3096474c8e299025caffb6f4d1848569
Abram <israelvictory87@gmail.com>
2023-08-24 13:58:00 +0100

    Update - modified create_accounts logic

--- agenta-backend/agenta_backend/services/commoners.py
+++ agenta-backend/agenta_backend/services/commoners.py
@@ -1,5 +1,6 @@
 from typing import Union
 from agenta_backend.models.api.user_models import User
+from agenta_backend.services.selectors import user_exists
 from agenta_backend.models.api.organization_models import Organization
 from agenta_backend.services.user_service import create_new_user
 from agenta_backend.services.organization_service import (
@@ -23,10 +24,16 @@ async def create_accounts(payload: dict) -> Union[str, str]:
         "email": payload["user_email"],
         "username": payload["user_email"].split("@")[0],
     }
-    organization = Organization(**{"name": user_dict["username"]})
-    org = await create_new_organization(organization)
+    
+    does_user_exist = await user_exists(user_dict["email"])
+    if not does_user_exist:
+        print("================ SIGNUP ====================")
+        organization = Organization(**{"name": user_dict["username"]})
+        org = await create_new_organization(organization)
 
-    if org is not None:
-        user_dict["organization_id"] = str(org.inserted_id)
-        user = User(**user_dict)
-        await create_new_user(user)
+        if org is not None:
+            user_dict["organization_id"] = str(org.inserted_id)
+            user = User(**user_dict)
+            await create_new_user(user)
+            
+    print("================ LOGIN ====================")
