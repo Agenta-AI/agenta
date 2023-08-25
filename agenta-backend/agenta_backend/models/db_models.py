98b465792d837e62075b8ea529952650df4fec95
Abram <israelvictory87@gmail.com>
2023-08-25 15:55:35 +0100

    Update - refactor remove old template from db

--- agenta-backend/agenta_backend/models/db_models.py
+++ agenta-backend/agenta_backend/models/db_models.py
@@ -1,44 +1,68 @@
 from datetime import datetime
-from typing import List, Optional, Dict
-from sqlmodel import SQLModel, Field, JSON, Column
+from typing import Optional, Dict, Any
+from odmantic import Field, Model, EmbeddedModel, Reference
 
 
-class ImageDB(SQLModel, table=True):
+class OrganizationDB(EmbeddedModel):
+    name: str
+    description: str
+    
+    class Config:
+        collection = "organizations"
+        
+
+class UserDB(EmbeddedModel):
+    uid: str = Field(default=0, unique=True, index=True)
+    username: str
+    email: str = Field(unique=True)
+    organization_id: OrganizationDB
+
+    class Config:
+        collection = "users"
+        
+
+class ImageDB(Model):
     """Defines the info needed to get an image and connect it to the app variant"""
+    
+    id: Optional[str] = Field(primary_field=True)
+    docker_id: str = Field(index=True)
+    tags: str
+    user_id: UserDB = Reference(key_name="user")
+    created_at: datetime = Field(default=datetime.utcnow())
+    updated_at: datetime = Field(default=datetime.utcnow())
+    
+    class Config:
+        collection = "images"
+    
 
-    id: int = Field(default=None, primary_key=True)
-    docker_id: str = Field(...)
-    tags: str = Field(...)
-    user_id: str = Field(default=None)
-    organization_id: str = Field(default=None)
-
-
-class AppVariantDB(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    app_name: str = Field(...)
-    variant_name: str = Field(...)
-    image_id: int = Field(foreign_key="imagedb.id")
-    user_id: str = Field(default=None)
-    organization_id: str = Field(default=None)
-    parameters: Dict = Field(sa_column=Column(JSON))
-    previous_variant_name: Optional[str] = Field(default=None)
+class AppVariantDB(Model):
+    app_name: str
+    variant_name: str
+    image_id: ImageDB = Reference()
+    user_id: UserDB = Reference(key_name="user")
+    parameters: Dict[str, Any]
+    previous_variant_name: Optional[str]
     is_deleted: bool = Field(
         default=False
     )  # soft deletion for using the template variants
 
+    class Config:
+        collection = "app_variants"
+        
+        
 
-class TemplateDB(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    template_id: int = Field(...)
-    name: str = Field(...)
-    repo_name: str = Field(...)
-    architecture: str = Field(...)
-    title: str = Field(...)
-    description: str = Field(...)
-    user_id: str = Field(default=None)
-    organization_id: str = Field(default=None)
-    size: int = Field(...)
-    digest: str = Field(...)
-    status: str = Field(...)
-    media_type: str = Field()
-    last_pushed: datetime = Field(...)
+class TemplateDB(Model):
+    template_id: int
+    name: str
+    repo_name: str
+    architecture: str
+    title: str
+    description: str
+    size: int
+    digest: str
+    status: str
+    media_type: str
+    last_pushed: datetime
+    
+    class Config:
+        collection = "templates"
