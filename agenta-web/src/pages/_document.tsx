e724787ab9073dc953c082a121682c401525c092
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-08-30 16:03:41 +0100

    fixes margin bug

--- agenta-web/src/pages/_document.tsx
+++ agenta-web/src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
                 <title>Agenta: The LLMOps platform.</title>
                 <link rel="shortcut icon" href="/assets/favicon.ico" />
             </Head>
-            <body>
+            <body style={{margin:0}}>
                 <Main />
                 <NextScript />
             </body>
