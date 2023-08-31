9490c8c5df1e81883d884eeee5d509b3dd3a1d6c
witehound <netvid00@gmail.com>
2023-08-31 22:04:08 +0100

    Fix - prettier

--- agenta-web/src/lib/helpers/copyToClipboard.ts
+++ agenta-web/src/lib/helpers/copyToClipboard.ts
@@ -1,4 +1,4 @@
-export const copyToClipboard = async (e: React.MouseEvent, text : string)  =>  {
+export const copyToClipboard = async (e: React.MouseEvent, text: string) => {
     e.preventDefault()
     try {
         await navigator.clipboard.writeText(text)
