e6704037bc8bbc0641b9da7b358fff364986cf42
Abram <israelvictory87@gmail.com>
2023-09-07 10:17:08 +0100

    Feat - implemented database engine

---
+++ agenta-backend/agenta_backend/models/db_engine.py
@@ -0,0 +1,44 @@
+import os
+import logging
+
+from odmantic import AIOEngine
+from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class DBEngine(object):
+    """Database engine to initialize client and return engine based on mode"""
+
+    def __init__(self, mode: str) -> None:
+        self.mode = mode
+        self.db_url = os.environ["MONGODB_URI"]
+
+    @property
+    def initialize_client(self) -> AsyncIOMotorClient:
+        """Returns an instance of `AsyncIOMotorClient` initialized \
+            with the provided `db_url`.
+        """
+        
+        client = AsyncIOMotorClient(self.db_url)
+        return client
+
+    def engine(self) -> AIOEngine:
+        """Returns an AIOEngine object with a specified database name based on the mode.
+        """
+        
+        aio_engine = AIOEngine(client=self.initialize_client)
+        if self.mode == "test":
+            aio_engine.database = "agenta_test"
+            logger.info("Using test database...")
+        elif self.mode == "default":
+            aio_engine.database = "agenta"
+            logger.info("Using default database")
+        return aio_engine
+    
+    def remove_db(self) -> None:
+        client = MongoClient(self.db_url)
+        client.drop_database("agenta_test")
