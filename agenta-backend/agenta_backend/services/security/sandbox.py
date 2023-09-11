7cc58a812c8174ad470ab732eaec66bd5c17424e
Abram <israelvictory87@gmail.com>
2023-09-11 23:19:24 +0100

    Update - improve security in sandbox environment

--- agenta-backend/agenta_backend/services/security/sandbox.py
+++ agenta-backend/agenta_backend/services/security/sandbox.py
@@ -12,12 +12,13 @@ from RestrictedPython.Guards import (
 )
 
 
-def is_import_safe(module: str) -> bool:
+def is_import_safe(module: str, allowed_imports: List[str]) -> bool:
     """Checks if a given package import contains any disallowed patterns of code.
-    
+
     Args:
         module (str) -- The module to check for potentially dangerous code
-    
+        allowed_imports (list) -- List of modules or objects that can be imported
+
     Returns:
         bool - module is secured or not
     """
@@ -26,25 +27,32 @@ def is_import_safe(module: str) -> bool:
     disallowed_patterns = [
         r"import\s+os",
         r"import\s+subprocess",
+        r"import\s+threading",
+        r"import\s+multiprocessing"
         # Add more patterns as needed
     ]
 
     # Check if any disallowed patterns are present
     for pattern in disallowed_patterns:
-        if re.search(pattern, module):
-            return False
+        match = re.search(pattern, module)
+        if match:
+            # Get the module name from the match object
+            module = match.group("module")
+
+            # Check if the module is in the list of allowed modules
+            if module not in allowed_imports:
+                return False
     return True
 
 
 def execute_code_safely(
-    code: Text, allowed_imports: List[str], inputs: Dict[str, Any]
+    code: Text, inputs: Dict[str, Any]
 ) -> Union[float, None]:
     """
     Execute the provided Python code safely using RestrictedPython.
 
     Args:
     - code (str): The Python code to be executed.
-    - allowed_imports (list): List of modules or objects that can be imported.
     - inputs (dict): Inputs to be used during code execution.
 
     Returns:
@@ -53,18 +61,44 @@ def execute_code_safely(
     """
     # Define the available built-ins
     local_builtins = safe_builtins.copy()
-    
+
     # Add the __import__ built-in function to the local builtins
     local_builtins["__import__"] = __import__
 
+    # Define supported packages
+    allowed_imports = [
+        "math",
+        "random",
+        "datetime",
+        "json",
+        "jsonschema",
+        "sqlglot",
+        "requests",
+        "marvin",
+        "numpy",
+    ]
+
     # Create a dictionary to simulate allowed imports
     allowed_modules = {}
-    for item in allowed_imports:
-        module_safe = is_import_safe(item)
+    for package_name in allowed_imports:
+        # Check if the package
+        module_safe = is_import_safe(package_name, allowed_imports)
         if not module_safe:
-            raise Exception("")
-        else:
-            allowed_modules[item] = __import__(item)
+            raise Exception(
+                f"We're sorry, but we don't currently support the {package_name} module. Please, try again."
+            )
+
+        try:
+            __import__(package_name, globals(), locals(), ["*"], 0)
+        except ImportError:
+            import pip
+
+            pip.install(
+                package_name, "-t", "/usr/local/lib/python3.9/lib-dynload/"
+            )
+        allowed_modules[package_name] = __import__(
+            package_name, globals(), locals(), ["*"], 0
+        )
 
     # Add the allowed modules to the local built-ins
     local_builtins.update(allowed_modules)
@@ -76,7 +110,7 @@ def execute_code_safely(
         "_iter_unpack_sequence_": guarded_iter_unpack_sequence,
         "_write_": full_write_guard,
         "inputs": inputs,
-        "__builtins__": local_builtins
+        "__builtins__": local_builtins,
     }
 
     # Compile the code in a restricted environment
