e23e92aebccc1cda02f32355c61d34bf1fad6adc
Abram <israelvictory87@gmail.com>
2023-09-12 08:58:46 +0100

    Feat - implemented helper function to include dynamic values

--- agenta-backend/agenta_backend/services/helpers.py
+++ agenta-backend/agenta_backend/services/helpers.py
@@ -1,3 +1,7 @@
+import json
+from typing import List, Dict, Any
+
+
 def print_app_variant(app_variant):
     print(f"App Variant ID: {app_variant.id}")
     print(f"App Variant Name: {app_variant.variant_name}")
@@ -14,3 +18,43 @@ def print_image(image):
     print(f"Docker ID: {image.docker_id}")
     print(f"Tags: {image.tags}")
     print("------------------------")
+
+
+def format_list_of_dictionaries(list_of_dictionaries: List[Dict[str, Any]]) -> Dict:
+  """
+  Formats a list of dictionaries into a dictionary using the input_name as the key, 
+  and the input_value as the value.
+
+  Args:
+    list_of_dictionaries: A list of dictionaries.
+
+  Returns:
+    A dictionary.
+  """
+  
+  formatted_dictionary = {}
+  for dictionary in list_of_dictionaries:
+    formatted_dictionary[dictionary['input_name']] = dictionary['input_value']
+  return formatted_dictionary
+
+
+def include_dynamic_values(json_data: Dict, inputs: Dict[str, Any]) -> Dict:
+  """
+  Includes the dynamic values in the JSON before it gets executed.
+
+  Args:
+    json_data: The JSON data.
+    inputs: The dynamic values.
+
+  Returns:
+    The modified JSON data.
+  """
+  
+  # Get the inputs dictionary.
+  inputs_dictionary = json.loads(inputs)
+
+  # Replace the `{inputs}` placeholder in the JSON data with the inputs dictionary.
+  for key, value in inputs_dictionary.items():
+    json_data = json_data.replace(f"{key}", value)
+
+  return json_data
