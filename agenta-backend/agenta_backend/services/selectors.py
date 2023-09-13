180d1c86354fac3ca8982e4e3c80ad62255b4f4b
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2023-09-13 13:44:46 +0100

    switch to odmantic

--- agenta-backend/agenta_backend/services/selectors.py
+++ agenta-backend/agenta_backend/services/selectors.py
@@ -1,5 +1,5 @@
 from typing import Tuple, Dict, List
-from agenta_backend.services.db_mongo import users
+from agenta_backend.services.db_manager import engine, UserDB
 
 
 async def get_user_and_org_id(session) -> Dict[str, str]:
@@ -22,16 +22,15 @@ async def get_user_objectid(user_id: str) -> Tuple[str, List]:
         user_id (str): The unique identifier of a user
 
     Returns:
-        a tuple containing the string representation of the user's ObjectId and the string
-    representation of the user's organization_id.
+        a tuple containing the string representation of the user's ObjectId and the List
+        of the user's organization_ids.
     """
 
-    user = await users.find_one({"id": user_id})
+    user = await engine.find_one(UserDB, UserDB.id == user_id)
 
     if user:
-        user_id = str(user["id"])
-        organization_ids = (
-            [str(org.id) for org in user.organizations] if user.organizations else []
-        )
+        user_id = str(user.id)
+        organization_ids = [str(org.id) for org in user.organizations] if user.organizations else []
         return user_id, organization_ids
-    return None, None
+
+    return None, []
