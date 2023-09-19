10e88809ad98a9f33624138dfcb643b8fbbbe213
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2023-09-19 19:40:55 +0100

    move token function to ee

--- agenta-backend/agenta_backend/utills/common.py
+++ agenta-backend/agenta_backend/utills/common.py
@@ -1,8 +1,4 @@
-import secrets
 from agenta_backend.models.db_engine import DBEngine
 engine = DBEngine(mode="default").engine()
 
 
-def generate_invitation_token(token_length: int = 16):
-    token = secrets.token_urlsafe(token_length)
-    return token
