e8c7bc1df2d149b5c4f060e55cc64589608a1453
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-09-26 19:27:57 +0200

    endpoint

--- agenta-backend/agenta_backend/routers/testset_router.py
+++ agenta-backend/agenta_backend/routers/testset_router.py
@@ -121,7 +121,7 @@ async def upload_file(
 async def import_testset(
     endpoint: str = Form(None),
     testset_name: str = Form(None),
-    app_name: str = Form(None),
+    app_id: str = Form(None),
     stoken_session: SessionContainer = Depends(verify_session()),
 ):
     """
@@ -134,6 +134,17 @@ async def import_testset(
     Returns:
         dict: The result of the import process.
     """
+    user_org_data: dict = await get_user_and_org_id(stoken_session)
+    access_app = await check_access_to_app(
+        kwargs=user_org_data, app_id=app_id, check_owner=False
+    )
+    if not access_app:
+        error_msg = f"You do not have access to this app: {app_id}"
+        return JSONResponse(
+            {"detail": error_msg},
+            status_code=400,
+        )
+    app_ref = await new_db_manager.fetch_app_by_id(app_id=app_id)
 
     try:
         response = requests.get(endpoint, timeout=10)
@@ -147,7 +158,8 @@ async def import_testset(
         document = {
             "created_at": datetime.now().isoformat(),
             "name": testset_name,
-            "app_name": app_name,
+            "app_id": app_ref,
+            "organization_id": app_ref.organization_id,
             "csvdata": [],
         }
 
@@ -156,8 +168,7 @@ async def import_testset(
         for row in json_response:
             document["csvdata"].append(row)
 
-        kwargs: dict = await get_user_and_org_id(stoken_session)
-        user = await get_user_object(kwargs["uid"])
+        user = await get_user_object(user_org_data["uid"])
         testset_instance = TestSetDB(**document, user=user)
         result = await engine.save(testset_instance)
 
