d2c6b1934d5bad9e6ef4dbeea0c5a2c58a38e13f
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2023-09-27 13:48:19 +0100

    define test create appvariant from template

---
+++ agenta-backend/agenta_backend/tests/test_app_variant_router.py
@@ -0,0 +1,59 @@
+import httpx
+from odmantic import query
+import pytest
+from agenta_backend.models.db_engine import DBEngine
+from agenta_backend.models.db_models import (
+    UserDB,
+    OrganizationDB,
+)
+from agenta_backend.routers.app_variant import add_app_variant_from_template
+
+# Initialize database engine
+engine = DBEngine().engine()
+
+# Initialize http client
+test_client = httpx.AsyncClient()
+
+
+@pytest.mark.asyncio
+async def test_successfully_creates_new_app_variant(get_first_user_object):
+
+    user = await get_first_user_object
+    query_expression = (OrganizationDB.type == "default") & (
+        OrganizationDB.owner == str(user.id)
+    )
+    user_organization = await engine.find_one(OrganizationDB, query_expression)
+
+    # Prepare test data
+    payload = {
+        "app_name": "myapp",
+        "image_id": "12345",
+        "image_tag": "latest",
+        "env_vars": {
+            "ENV_VAR1": "value1",
+            "ENV_VAR2": "value2"
+        },
+        "organization_id": str(user_organization.id),
+    }
+
+    # Invoke the function
+    response = await test_client.get(
+        f"http://localhost:8000/app_variant/feedbacks/add/from_template/",
+        json=payload,
+    )
+
+    # Assertions
+    assert response.status_code == 200
+    assert response == {
+        "app_id": "12345",
+        "variant_id": "13579",
+        "variant_name": "app",
+        "parameters": {},
+        "previous_variant_name": None,
+        "organization_id": str(user_organization.id),
+        "user_id": "54321",
+        "base_name": "app",
+        "base_id": None,
+        "config_name": "default",
+        "config_id": None
+    }
