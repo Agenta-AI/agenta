98512fa3c1292f97d3f378a6b0e68e1464b0f2d0
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-09-28 14:03:37 +0200

    minor fix and renaming

--- agenta-backend/agenta_backend/utils/common.py
+++ agenta-backend/agenta_backend/utils/common.py
@@ -68,7 +68,7 @@ async def check_user_org_access(
 async def check_access_to_app(
     user_org_data: Dict[str, Union[str, list]],
     app: Optional[AppDB] = None,
-    app_id: Optional[ObjectId] = None,
+    app_id: Optional[str] = None,
     check_owner: bool = False,
 ) -> bool:
     """
@@ -77,7 +77,7 @@ async def check_access_to_app(
     Args:
         user_org_data (Dict[str, Union[str, list]]): User-specific information.
         app (Optional[AppDB]): An instance of the AppDB model representing the application.
-        app_id (Optional[ObjectId]): The ID of the application.
+        app_id (Optional[str]): The ID of the application.
         check_owner (bool): Whether to check if the user is the owner of the application.
 
     Returns:
@@ -92,7 +92,7 @@ async def check_access_to_app(
 
     # Fetch the app if only app_id is provided.
     if app is None:
-        app = await engine.find_one(AppDB, AppDB.id == app_id)
+        app = await engine.find_one(AppDB, AppDB.id == ObjectId(app_id))
         if app is None:
             logger.error("App not found")
             return False
