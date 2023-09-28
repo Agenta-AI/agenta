7271a841065118cc49e24114946c8e86ab7dd0a7
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2023-09-28 11:02:48 +0100

    Tests: lists empty apps & create appvariant

--- agenta-backend/agenta_backend/tests/test_app_variant_router.py
+++ agenta-backend/agenta_backend/tests/test_app_variant_router.py
@@ -53,6 +53,11 @@ new_object_id = ObjectId()
 
 @pytest.mark.asyncio
 async def test_list_empty_apps():
+    response = await app_router.list_apps()
+    assert response == []
+    
+@pytest.mark.asyncio
+async def test_list_empty_appvariants():
     response = await app_router.list_app_variants()
     assert response == []
     
@@ -74,3 +79,58 @@ async def test_create_app(get_first_user_object):
     
     org_app_response = await app_router.list_apps(str(organization.id))
     assert len(org_app_response) == 1
+    
+    await engine.delete(app)
+    
+    
+@pytest.mark.asyncio
+async def test_create_app_variant(get_first_user_object):
+    user = await get_first_user_object
+    organization = await new_db_manager.get_user_own_org(user.uid)
+    
+    app = AppDB(
+        app_name="test_app",
+        organization_id=organization,
+        user_id=user,
+    )
+    await engine.save(app)
+    
+    db_image = ImageDB(
+        docker_id="sha256:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+        tags="agentaai/templates:local_test_prompt",
+        user_id=user,
+        organization_id=organization,
+    )
+    await engine.save(db_image)
+    
+    db_config = ConfigDB(
+        config_name="default",
+        parameters={},
+    )
+    await engine.save(db_config)
+    
+    db_base = BaseDB(
+        base_name="app",
+        image_id=db_image,
+    )
+    await engine.save(db_base)
+    
+    appvariant = AppVariantDB(
+        app_id=app,
+        variant_name="app",
+        image_id=db_image,
+        user_id=user,
+        organization_id=organization,
+        parameters={},
+        base_name="app",
+        config_name="default",
+        base_id=db_base,
+        config_id=db_config,
+    )
+    await engine.save(appvariant)
+    
+    response = await app_router.list_app_variants(app_id=str(app.id))
+    assert len(response) == 1
+    
+    await engine.delete(app)
+    await engine.delete(appvariant)
