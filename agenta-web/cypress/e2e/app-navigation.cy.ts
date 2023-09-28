9880ce8d0fd80873728a5da78c721da5fb7663dd
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-09-28 23:34:43 +0100

    fix: failed test in testset after clicking save button

---
+++ agenta-web/cypress/e2e/app-navigation.cy.ts
@@ -0,0 +1,79 @@
+describe("App Navigation without errors", () => {
+    beforeEach(() => {
+        cy.visit("/apps")
+        cy.clickLinkAndWait('[data-cy="app-card-link"]')
+    })
+
+    const variant = "capitals"
+
+    it.only("should route to /playground", () => {
+        cy.clickLinkAndWait('[data-cy="app-playground-link"]').as("sign")
+        cy.request({
+            url: `/apps/${variant}/playground`,
+            method: "GET",
+        }).should((response) => {
+            expect(response.status).to.eq(200)
+        })
+        cy.location("pathname").should("include", "/playground")
+        cy.get('[data-cy="playground-title"]').within(() => {
+            cy.get("h2").should("contain.text", "1. Modify Parameters")
+        })
+    })
+
+    it("should route to /testsets", () => {
+        cy.clickLinkAndWait('[data-cy="app-testsets-link"]')
+        cy.request({
+            url: `/apps/${variant}/testsets`,
+            method: "GET",
+        }).should((response) => {
+            expect(response.status).to.eq(200)
+        })
+        cy.location("pathname").should("include", "/testsets")
+        cy.get('[data-cy="app-testset-list"]').should("exist")
+    })
+
+    it("should route to /evaluations", () => {
+        cy.clickLinkAndWait('[data-cy="app-evaluations-link"]')
+        cy.request({
+            url: `/apps/${variant}/evaluations`,
+            method: "GET",
+        }).should((response) => {
+            expect(response.status).to.eq(200)
+        })
+        cy.location("pathname").should("include", "/evaluations")
+        cy.get('[data-cy="evaluations-container"]').within(() => {
+            cy.contains("1. Select an evaluation type")
+            cy.contains("2. Which variants would you like to evaluate")
+            cy.contains("3. Which testset you want to use?")
+        })
+    })
+
+    it("should route to /endpoints", () => {
+        cy.clickLinkAndWait('[data-cy="app-endpoints-link"]')
+        cy.request({
+            url: `/apps/${variant}/endpoints`,
+            method: "GET",
+        }).should((response) => {
+            expect(response.status).to.eq(200)
+        })
+        cy.location("pathname").should("include", "/endpoints")
+        cy.get('[data-cy="endpoints"]').within(() => {
+            cy.contains("API endpoint")
+        })
+    })
+
+    it("should route to /apikeys", () => {
+        cy.clickLinkAndWait('[data-cy="apikeys-link"]')
+        cy.request({
+            url: `/apikeys`,
+            method: "GET",
+        }).should((response) => {
+            expect(response.status).to.eq(200)
+        })
+        cy.location("pathname").should("include", "/apikeys")
+        cy.get('[data-cy="apikeys"]').within(() => {
+            cy.contains("API Keys")
+            cy.contains("LLM providers")
+        })
+    })
+})
