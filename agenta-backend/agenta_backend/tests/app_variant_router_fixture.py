871a071410548d5b3d6c6f4699a2fad20720622a
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2023-09-28 16:06:23 +0100

    format black

--- agenta-backend/agenta_backend/tests/app_variant_router_fixture.py
+++ agenta-backend/agenta_backend/tests/app_variant_router_fixture.py
@@ -23,6 +23,7 @@ engine = DBEngine().engine()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+
 @pytest.fixture(scope="function")
 async def get_first_user_object():
     """Get the user object from the database or create a new one if not found."""
@@ -45,20 +46,17 @@ async def get_first_user_object():
             return user
     except Exception as e:
         pytest.fail(f"Failed to get or create the first user: {e}")
-        
+
 
 @pytest.fixture(scope="function")
 async def get_second_user_object():
     """Create a secind user object."""
-    
+
     try:
-        
         user = await engine.find_one(UserDB, UserDB.uid == "1")
         if user is None:
             create_user = UserDB(
-                uid="1",
-                username="test_user1",
-                email="test_user1@email.com"
+                uid="1", username="test_user1", email="test_user1@email.com"
             )
             await engine.save(create_user)
 
@@ -72,14 +70,13 @@ async def get_second_user_object():
             return create_user
         else:
             return user
-        
+
     except Exception as e:
         pytest.fail(f"Failed to get or create the second user: {e}")
-        
+
 
 @pytest.fixture(scope="function")
 async def get_first_user_app():
-
     user = await engine.find_one(UserDB, UserDB.uid == "0")
     if user is None:
         user = UserDB(uid="0")
@@ -91,12 +88,12 @@ async def get_first_user_app():
         user.organizations.append(organization.id)
         await engine.save(user)
         await engine.save(organization)
-        
+
     organization = await new_db_manager.get_user_own_org(user.uid)
-    
+
     app = AppDB(app_name="myapp", organization_id=organization, user_id=user)
     await engine.save(app)
-            
+
     db_image = ImageDB(
         docker_id="sha256:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
         tags="agentaai/templates:local_test_prompt",
@@ -104,20 +101,19 @@ async def get_first_user_app():
         organization_id=organization,
     )
     await engine.save(db_image)
-    
+
     db_config = ConfigDB(
         config_name="default",
         parameters={},
     )
     await engine.save(db_config)
-    
+
     db_base = BaseDB(
         base_name="app",
         image_id=db_image,
     )
     await engine.save(db_base)
-    
-    
+
     appvariant = AppVariantDB(
         app_id=app,
         variant_name="app",
@@ -132,5 +128,4 @@ async def get_first_user_app():
     )
     await engine.save(appvariant)
 
-    return appvariant
-
+    return appvariant, user, organization, app, db_image, db_config, db_base
