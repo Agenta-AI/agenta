49466403b7e67e0bb4635c8f5aab581ba82c90ff
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-10-01 13:54:57 +0100

    feat: Add test for handling no OpenAI key input

--- agenta-web/cypress/e2e/playground.cy.ts
+++ agenta-web/cypress/e2e/playground.cy.ts
@@ -21,6 +21,32 @@ describe("run a simple prompt", () => {
         cy.get('[data-cy="enter-app-name-modal-text-warning"]').should("exist")
     })
 
+    it("no open ai key is entered", () => {
+        cy.visit("/apps")
+        cy.get('.ant-notification').should("not.exist")
+        cy.get('[data-cy="add-new-app-modal"]').should("not.exist")
+        cy.get('[data-cy="choose-template-modal"]').should("not.exist")
+        cy.get('[data-cy="enter-app-name-modal"]').should("not.exist")
+        cy.get('[data-cy="create-new-app-button"]').click()
+        cy.get('[data-cy="add-new-app-modal"]').should("exist")
+        cy.get('[data-cy="create-from-template"]').click()
+        cy.get('[data-cy="choose-template-modal"]').should("exist")
+        cy.get('[data-cy="create-app-button"]').click()
+        const appName = randString(5)
+    
+        cy.get('[data-cy="enter-app-name-modal"]')
+            .should("exist")
+            .within(() => {
+                cy.get("input").type(appName)
+            })
+    
+        cy.get('[data-cy="enter-app-name-modal-button"]').click()
+        cy.get('.ant-notification').should("exist")
+        cy.url().should("include", "apikeys")
+        cy.wait(5000)
+        cy.get('.ant-notification').should("not.exist")
+    })
+
     it("should create a new app variant and run playground prompt", () => {
         cy.visit("/apikeys")
         // Update your cypress.json file to include your OPENAI API KEY
