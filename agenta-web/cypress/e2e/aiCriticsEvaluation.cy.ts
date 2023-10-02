e84b78ee55376526959eb96d63c879c7071321c8
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-10-02 10:05:37 +0100

    test: run prompts without apikey

--- agenta-web/cypress/e2e/aiCriticsEvaluation.cy.ts
+++ agenta-web/cypress/e2e/aiCriticsEvaluation.cy.ts
@@ -5,16 +5,23 @@ describe("AI Critics Evaluation workflow", () => {
         cy.clickLinkAndWait('[data-cy="app-evaluations-link"]')
     })
     
-    it("successfully navigate to evaluation path", () => {
-        cy.url().should("include", "/evaluations")
+    context("When successfully navigating to the evaluation path", () => {
+        it("should navigate to evaluation page", () => {
+            cy.url().should("include", "/evaluations")
+        })
+    })
+
+    context("when selecting evaluation without apikey", () => {
+        it("select", () => {
+            cy.get('[data-cy="evaluation-error-modal"]').should("not.exist")
+            cy.get('[data-cy="ai-critic-button"]').click()
+            cy.get('[data-cy="variants-dropdown"]').eq(0).click()
+            cy.get("li.ant-dropdown-menu-item").eq(0).click()
+            cy.get('[data-cy="selected-testset"]').click()
+            cy.get("li.ant-dropdown-menu-item").eq(0).click()
+            cy.clickLinkAndWait('[data-cy="start-new-evaluation-button"]')
+            cy.get('[data-cy="evaluation-error-modal"]').should("exist")
+        })
     })
     
-    it("select", () => {
-        cy.get('[data-cy="ai-critic-button"]').click()
-        cy.get('[data-cy="variants-dropdown"]').eq(0).click()
-        cy.get("li.ant-dropdown-menu-item").eq(0).click()
-        cy.get('[data-cy="selected-testset"]').click()
-        cy.get("li.ant-dropdown-menu-item").eq(0).click()
-        cy.clickLinkAndWait('[data-cy="start-new-evaluation-button"]')
-    })
 })
