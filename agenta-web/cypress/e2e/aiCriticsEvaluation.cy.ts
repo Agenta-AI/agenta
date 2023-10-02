586c87331fd6d527e466d5ecf58c8beb6647ab59
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-10-02 09:21:10 +0100

    select evaluation, variant and testset

--- agenta-web/cypress/e2e/aiCriticsEvaluation.cy.ts
+++ agenta-web/cypress/e2e/aiCriticsEvaluation.cy.ts
@@ -4,10 +4,17 @@ describe("AI Critics Evaluation workflow", () => {
         cy.clickLinkAndWait('[data-cy="app-card-link"]')
         cy.clickLinkAndWait('[data-cy="app-evaluations-link"]')
     })
-
+    
     it("successfully navigate to evaluation path", () => {
         cy.url().should("include", "/evaluations")
     })
-
-    it("select", () => {})
+    
+    it("select", () => {
+        cy.get('[data-cy="ai-critic-button"]').click()
+        cy.get('[data-cy="variants-dropdown"]').eq(0).click()
+        cy.get("li.ant-dropdown-menu-item").eq(0).click()
+        cy.get('[data-cy="selected-testset"]').click()
+        cy.get("li.ant-dropdown-menu-item").eq(0).click()
+        cy.clickLinkAndWait('[data-cy="start-new-evaluation-button"]')
+    })
 })
