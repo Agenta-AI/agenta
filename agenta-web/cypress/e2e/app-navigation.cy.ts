156c243d916e80211b81928ff742c42e4d00bd32
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-10-03 04:12:54 +0100

    improved test

--- agenta-web/cypress/e2e/app-navigation.cy.ts
+++ agenta-web/cypress/e2e/app-navigation.cy.ts
@@ -1,72 +1,56 @@
 describe("App Navigation without errors", () => {
-    beforeEach(() => {
-        cy.visit("/apps")
-        cy.clickLinkAndWait('[data-cy="app-card-link"]')
+    context("when the user has clicked on an app variant", () => {
+        beforeEach(() => {
+            cy.visit("/apps")
+            cy.clickLinkAndWait('[data-cy="app-card-link"]')
+        })
+
+        it("should navigate to playground and check if it's successful", () => {
+            cy.clickLinkAndWait('[data-cy="app-playground-link"]')
+            cy.location("pathname").should("include", "/playground")
+            cy.get('[data-cy="playground-header"]').within(() => {
+                cy.get("h2").should("contain.text", "1. Modify Parameters")
+                cy.get("button").should("have.length", 3)
+            })
+        })
+
+        it("should navigate to testsets and check if it's successful", () => {
+            cy.clickLinkAndWait('[data-cy="app-testsets-link"]')
+            cy.location("pathname").should("include", "/testsets")
+            cy.get('[data-cy="app-testset-list"]').should("exist")
+        })
+
+        it("should navigate to evaluations and check if it's successful", () => {
+            cy.clickLinkAndWait('[data-cy="app-evaluations-link"]')
+            cy.location("pathname").should("include", "/evaluations")
+            cy.get('[data-cy="evaluations-container"]').within(() => {
+                cy.contains("1. Select an evaluation type")
+                cy.contains("2. Which variants would you like to evaluate")
+                cy.contains("3. Which testset you want to use?")
+            })
+        })
+
+        it("should navigate to endpoints and check if it's successful", () => {
+            cy.clickLinkAndWait('[data-cy="app-endpoints-link"]')
+            cy.location("pathname").should("include", "/endpoints")
+            cy.get('[data-cy="endpoints"]').within(() => {
+                cy.contains("API endpoint")
+            })
+        })
     })
 
-    const variant = "capitals"
-
-    it("Should navigate to all pages without errors", () => {
-        cy.clickLinkAndWait('[data-cy="app-playground-link"]')
-        cy.request({
-            url: `/apps/${variant}/playground`,
-            method: "GET",
-        }).should((response) => {
-            expect(response.status).to.eq(200)
-        })
-        cy.location("pathname").should("include", "/playground")
-        cy.get('[data-cy="playground-header"]').within(() => {
-            cy.get("h2").should("contain.text", "1. Modify Parameters")
-            cy.get("button").should("have.length", 3)
+    context("when accessing the apikeys from apps page", () => {
+        before(() => {
+            cy.visit("/apps")
         })
 
-        cy.clickLinkAndWait('[data-cy="app-testsets-link"]')
-        cy.request({
-            url: `/apps/${variant}/testsets`,
-            method: "GET",
-        }).should((response) => {
-            expect(response.status).to.eq(200)
-        })
-        cy.location("pathname").should("include", "/testsets")
-        cy.get('[data-cy="app-testset-list"]').should("exist")
-
-        cy.clickLinkAndWait('[data-cy="app-evaluations-link"]')
-        cy.request({
-            url: `/apps/${variant}/evaluations`,
-            method: "GET",
-        }).should((response) => {
-            expect(response.status).to.eq(200)
-        })
-        cy.location("pathname").should("include", "/evaluations")
-        cy.get('[data-cy="evaluations-container"]').within(() => {
-            cy.contains("1. Select an evaluation type")
-            cy.contains("2. Which variants would you like to evaluate")
-            cy.contains("3. Which testset you want to use?")
-        })
-
-        cy.clickLinkAndWait('[data-cy="app-endpoints-link"]')
-        cy.request({
-            url: `/apps/${variant}/endpoints`,
-            method: "GET",
-        }).should((response) => {
-            expect(response.status).to.eq(200)
-        })
-        cy.location("pathname").should("include", "/endpoints")
-        cy.get('[data-cy="endpoints"]').within(() => {
-            cy.contains("API endpoint")
-        })
-
-        cy.clickLinkAndWait('[data-cy="apikeys-link"]')
-        cy.request({
-            url: `/apikeys`,
-            method: "GET",
-        }).should((response) => {
-            expect(response.status).to.eq(200)
-        })
-        cy.location("pathname").should("include", "/apikeys")
-        cy.get('[data-cy="apikeys"]').within(() => {
-            cy.contains("API Keys")
-            cy.contains("LLM providers")
+        it("should navigate to apikeys and check if it's successful", () => {
+            cy.clickLinkAndWait('[data-cy="apikeys-link"]')
+            cy.location("pathname").should("include", "/apikeys")
+            cy.get('[data-cy="apikeys"]').within(() => {
+                cy.contains("API Keys")
+                cy.contains("LLM providers")
+            })
         })
     })
 })
