406e68077c51da204b1f63f193a2defe6031c966
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-10-03 11:57:34 +0100

    feat: test for the requests and update the result

--- agenta-web/cypress.config.ts
+++ agenta-web/cypress.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     },
     env: {
         baseApiURL: "http://localhost/api",
-        OPENAI_API_KEY: "your_api_key_here",
+        OPENAI_API_KEY: "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",
         localBaseUrl: "http://localhost",
     },
 })
--- agenta-web/cypress/e2e/aiCriticsEvaluation.cy.ts
+++ agenta-web/cypress/e2e/aiCriticsEvaluation.cy.ts
@@ -56,12 +56,56 @@ describe("AI Critics Evaluation workflow", () => {
             cy.clickLinkAndWait('[data-cy="start-new-evaluation-button"]')
             cy.get('[data-cy="evaluation-error-modal"]').should("not.exist")
             cy.url().should("include", "/auto_ai_critique")
+            cy.get('[data-cy="ai-critic-evaluation-result"]').should(
+                "contain.text",
+                "Run evaluation to see results!",
+            )
+            cy.get(".ant-message-notice-content").should("not.exist")
             cy.wait(1000)
             cy.clickLinkAndWait('[data-cy="ai-critic-run-evaluation"]')
-            cy.intercept("POST", "http://localhost/65144f7b461438d1bef09f50/0QYa9/v1/generate").as("postRequest")
-            cy.wait("@postRequest", {requestTimeout: 15000}).then((interception) => {
-                expect(interception.response.statusCode).to.eq(200)
+
+            cy.request({
+                url: "http://localhost/api/evaluations/?app_name=await",
+                method: "GET",
+            }).then((response) => {
+                expect(response.status).to.equal(200)
+                cy.request({
+                    url: `http://localhost/api/evaluations/${
+                        response.body[response.body.length - 1].id
+                    }/evaluation_scenarios`,
+                    method: "GET",
+                }).then((getResponse) => {
+                    expect(getResponse.status).to.equal(200)
+                })
+                cy.request({
+                    url: `http://localhost/api/evaluations/${
+                        response.body[response.body.length - 1].id
+                    }`,
+                    method: "PUT",
+                    body: {
+                        status: "EVALUATION_FINISHED",
+                    },
+                }).then((putResponse) => {
+                    expect(putResponse.status).to.equal(200)
+                })
+                cy.intercept(
+                    "GET",
+                    `http://localhost/api/evaluations/${
+                        response.body[response.body.length - 1].id
+                    }/results`,
+                ).as("getReq")
+                cy.wait("@getReq", {requestTimeout: 15000}).then((interception) => {
+                    expect(interception.response.statusCode).to.eq(200)
+                    cy.get('[data-cy="ai-critic-evaluation-result"]').should(
+                        "contain.text",
+                        "Results Data:",
+                    )
+                })
             })
+            cy.get(".ant-message-notice-content").should("exist")
+            cy.wait(3000)
+            cy.get(".ant-message-notice-content").should("not.exist")
+            cy.clickLinkAndWait('[data-cy="app-evaluations-link"]')
         })
     })
 })
