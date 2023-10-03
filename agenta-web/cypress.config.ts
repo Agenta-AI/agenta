a9479bbfba40cd20f26d845e1d15caaa3a78286a
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-10-03 11:58:03 +0100

    minor fix

--- agenta-web/cypress.config.ts
+++ agenta-web/cypress.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     },
     env: {
         baseApiURL: "http://localhost/api",
-        OPENAI_API_KEY: "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",
+        OPENAI_API_KEY: "your_api_key_here",
         localBaseUrl: "http://localhost",
     },
 })
