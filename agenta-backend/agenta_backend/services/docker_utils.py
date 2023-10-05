b83d4a7255ca774ee3d635f902691308b429cf1f
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-10-05 17:40:45 +0200

    refactored start_container

--- agenta-backend/agenta_backend/services/docker_utils.py
+++ agenta-backend/agenta_backend/services/docker_utils.py
@@ -61,44 +61,40 @@ def list_images() -> List[Image]:
 
 
 def start_container(
-    image_name, app_name, base_name, env_vars: DockerEnvVars, organization_id: str
+    image_name: str, uri_path: str, container_name: str, env_vars: DockerEnvVars
 ) -> Dict:
     try:
         image = client.images.get(f"{image_name}")
 
-        # Set user backend url path and container name
-        user_backend_url_path = f"{organization_id}/{app_name}/{base_name}"
-        user_backend_container_name = f"{app_name}-{base_name}-{organization_id}"
-
         # Default labels
         labels = {
-            f"traefik.http.services.{user_backend_container_name}.loadbalancer.server.port": "80",
-            f"traefik.http.middlewares.{user_backend_container_name}-strip-prefix.stripprefix.prefixes": f"/{user_backend_url_path}",
-            f"traefik.http.routers.{user_backend_container_name}.middlewares": f"{user_backend_container_name}-strip-prefix",
-            f"traefik.http.routers.{user_backend_container_name}.service": f"{user_backend_container_name}",
+            f"traefik.http.services.{container_name}.loadbalancer.server.port": "80",
+            f"traefik.http.middlewares.{container_name}-strip-prefix.stripprefix.prefixes": f"/{uri_path}",
+            f"traefik.http.routers.{container_name}.middlewares": f"{container_name}-strip-prefix",
+            f"traefik.http.routers.{container_name}.service": f"{container_name}",
         }
 
         # Merge the default labels with environment-specific labels
         if os.environ["ENVIRONMENT"] == "production":
             # Production specific labels
             production_labels = {
-                f"traefik.http.routers.{user_backend_container_name}.rule": f"Host(`{os.environ['BARE_DOMAIN_NAME']}`) && PathPrefix(`/{user_backend_url_path}`)",
+                f"traefik.http.routers.{container_name}.rule": f"Host(`{os.environ['BARE_DOMAIN_NAME']}`) && PathPrefix(`/{uri_path}`)",
             }
             labels.update(production_labels)
 
             if "https" in os.environ["DOMAIN_NAME"]:
                 # SSL specific labels
                 ssl_labels = {
-                    f"traefik.http.routers.{user_backend_container_name}.entrypoints": "web-secure",
-                    f"traefik.http.routers.{user_backend_container_name}.tls": "true",
-                    f"traefik.http.routers.{user_backend_container_name}.tls.certresolver": "myResolver",
+                    f"traefik.http.routers.{container_name}.entrypoints": "web-secure",
+                    f"traefik.http.routers.{container_name}.tls": "true",
+                    f"traefik.http.routers.{container_name}.tls.certresolver": "myResolver",
                 }
                 labels.update(ssl_labels)
         else:
             # Development specific labels
             development_labels = {
-                f"traefik.http.routers.{user_backend_container_name}.rule": f"PathPrefix(`/{user_backend_url_path}`)",
-                f"traefik.http.routers.{user_backend_container_name}.entrypoints": "web",
+                f"traefik.http.routers.{container_name}.rule": f"PathPrefix(`/{uri_path}`)",
+                f"traefik.http.routers.{container_name}.entrypoints": "web",
             }
 
             labels.update(development_labels)
@@ -109,7 +105,7 @@ def start_container(
             detach=True,
             labels=labels,
             network="agenta-network",
-            name=user_backend_container_name,
+            name=container_name,
             environment=env_vars,
         )
         # Check the container's status
@@ -119,15 +115,15 @@ def start_container(
             logs = container.logs().decode("utf-8")
             raise Exception(f"Container exited immediately. Docker Logs: {logs}")
         return {
-            "uri": f"http://{os.environ['BARE_DOMAIN_NAME']}/{user_backend_url_path}",
-            "uri_path": f"/{user_backend_url_path}",
+            "uri": f"http://{os.environ['BARE_DOMAIN_NAME']}/{uri_path}",
+            "uri_path": f"/{uri_path}",
             "container_id": container.id,
-            "container_name": user_backend_container_name,
+            "container_name": container_name,
         }
     except docker.errors.APIError as error:
         # Container failed to run, get the logs
         try:
-            failed_container = client.containers.get(user_backend_container_name)
+            failed_container = client.containers.get(container_name)
             logs = failed_container.logs().decode("utf-8")
             raise Exception(f"Docker Logs: {logs}") from error
         except Exception as e:
