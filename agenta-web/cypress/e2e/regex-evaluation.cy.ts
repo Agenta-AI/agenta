1b31758a2a2a3d22f47662f55287b8c1d229eabf
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-10-07 20:37:44 +0100

    test to select options

--- agenta-web/cypress/e2e/regex-evaluation.cy.ts
+++ agenta-web/cypress/e2e/regex-evaluation.cy.ts
@@ -7,4 +7,24 @@ describe("Regex Evaluation workflow", () => {
             cy.url().should("include", "/evaluations")
         })
     })
+
+    context("workflow", () => {
+        it("workflow", () => {
+            cy.visit("/apps")
+            cy.clickLinkAndWait('[data-cy="app-card-link"]')
+            cy.clickLinkAndWait('[data-cy="app-evaluations-link"]')
+            cy.url().should("include", "/evaluations")
+            cy.clickLinkAndWait('[data-cy="regex-button"]')
+
+            cy.get('[data-cy="variants-dropdown-0"]').trigger("mouseover")
+            cy.get('[data-cy="variant-0"]').click()
+            cy.get('[data-cy="variants-dropdown-0"]').trigger("mouseout")
+
+            cy.get('[data-cy="selected-testset"]').trigger("mouseover")
+            cy.get('[data-cy="testset-0"]').click()
+            cy.get('[data-cy="selected-testset"]').trigger("mouseout")
+
+            cy.clickLinkAndWait('[data-cy="start-new-evaluation-button"]')
+        })
+    })
 })
