5d408a845326e3cdfcb65673291dfd806a52e4cb
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-10-07 20:52:14 +0100

    test when user runs without selecting options

--- agenta-web/cypress/e2e/regex-evaluation.cy.ts
+++ agenta-web/cypress/e2e/regex-evaluation.cy.ts
@@ -8,8 +8,51 @@ describe("Regex Evaluation workflow", () => {
         })
     })
 
-    context("workflow", () => {
-        it("workflow", () => {
+    context("When user tries to start without selecting all options", () => {
+        beforeEach(() => {
+            cy.visit("/apps")
+            cy.clickLinkAndWait('[data-cy="app-card-link"]')
+            cy.clickLinkAndWait('[data-cy="app-evaluations-link"]')
+            cy.url().should("include", "/evaluations")
+        })
+
+        it("Should show modal", () => {
+            cy.clickLinkAndWait('[data-cy="start-new-evaluation-button"]')
+            cy.get(".ant-message-notice-content")
+                .should("contain.text", "Please select a variant")
+                .should("exist")
+            cy.wait(3000)
+            cy.get(".ant-message-notice-content").should("not.exist")
+        })
+
+        it("Should show modal", () => {
+            cy.clickLinkAndWait('[data-cy="regex-button"]')
+            cy.clickLinkAndWait('[data-cy="start-new-evaluation-button"]')
+            cy.get(".ant-message-notice-content")
+                .should("contain.text", "Please select a variant")
+                .should("exist")
+            cy.wait(3000)
+            cy.get(".ant-message-notice-content").should("not.exist")
+        })
+
+        it("Should show modal", () => {
+            cy.clickLinkAndWait('[data-cy="regex-button"]')
+
+            cy.get('[data-cy="variants-dropdown-0"]').trigger("mouseover")
+            cy.get('[data-cy="variant-0"]').click()
+            cy.get('[data-cy="variants-dropdown-0"]').trigger("mouseout")
+
+            cy.clickLinkAndWait('[data-cy="start-new-evaluation-button"]')
+            cy.get(".ant-message-notice-content")
+                .should("contain.text", "Please select a testset")
+                .should("exist")
+            cy.wait(3000)
+            cy.get(".ant-message-notice-content").should("not.exist")
+        })
+    })
+
+    context("When it works", () => {
+        it("Should work", () => {
             cy.visit("/apps")
             cy.clickLinkAndWait('[data-cy="app-card-link"]')
             cy.clickLinkAndWait('[data-cy="app-evaluations-link"]')
@@ -25,6 +68,8 @@ describe("Regex Evaluation workflow", () => {
             cy.get('[data-cy="selected-testset"]').trigger("mouseout")
 
             cy.clickLinkAndWait('[data-cy="start-new-evaluation-button"]')
+
+            cy.location("pathname").should("include", "/auto_regex_test")
         })
     })
 })
