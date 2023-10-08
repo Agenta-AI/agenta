322923db04185e7d46b93410a0a08fb7dafa71bd
Abram <israelvictory87@gmail.com>
2023-10-08 12:34:57 +0100

    Update - added more test cases for variant router

--- agenta-backend/agenta_backend/tests/variants_router/test_app_variant_router.py
+++ agenta-backend/agenta_backend/tests/variants_router/test_app_variant_router.py
@@ -51,16 +51,18 @@ async def test_create_app(get_first_user_object):
 
 
 @pytest.mark.asyncio
+async def test_list_apps():
+    response = await test_client.get(f"{BACKEND_API_HOST}/apps/")
+
+    assert response.status_code == 200
+    assert len(response.json()) == 2
+
+
+@pytest.mark.asyncio
 async def test_create_app_variant(get_first_user_object):
     user = await get_first_user_object
     organization = await selectors.get_user_own_org(user.uid)
-
-    app = AppDB(
-        app_name="test_app",
-        organization=organization,
-        user=user,
-    )
-    await engine.save(app)
+    app = await engine.find_one(AppDB, AppDB.app_name == "app_variant_test")
 
     db_image = ImageDB(
         docker_id="sha256:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
@@ -104,6 +106,17 @@ async def test_create_app_variant(get_first_user_object):
 
 
 @pytest.mark.asyncio
+async def test_list_app_variants():
+    app_db = await engine.find_one(AppDB, AppDB.app_name == "app_variant_test")
+    response = await test_client.get(
+        f"{BACKEND_API_HOST}/apps/{str(app_db.id)}/variants/"
+    )
+
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+
+
+@pytest.mark.asyncio
 async def test_delete_app_without_permission(get_second_user_object):
     user2 = await get_second_user_object
     user2_organization = await selectors.get_user_own_org(user2.uid)
@@ -123,6 +136,17 @@ async def test_delete_app_without_permission(get_second_user_object):
 
 
 @pytest.mark.asyncio
+async def test_list_environments():
+    app = await engine.find_one(AppDB, AppDB.app_name == "app_variant_test")
+    response = await test_client.get(
+        f"{BACKEND_API_HOST}/apps/{str(app.id)}/environments/"
+    )
+
+    assert response.status_code == 200
+    assert len(response.json()) == 3
+
+
+@pytest.mark.asyncio
 async def test_get_variant_by_env(get_first_user_app):
     _, _, _, app, _, _, _ = await get_first_user_app
     environments = await db_manager.list_environments(app_id=str(app.id))
@@ -132,3 +156,12 @@ async def test_get_variant_by_env(get_first_user_app):
             app_id=str(app.id), environment=environment.name
         )
         assert response == []
+
+
+@pytest.mark.asyncio
+async def test_remove_app():
+    app = await engine.find_one(AppDB, AppDB.app_name == "app_variant_test")
+    await engine.delete(app)
+
+    app = await engine.find_one(AppDB, AppDB.app_name == "app_variant_test")
+    assert app == None
