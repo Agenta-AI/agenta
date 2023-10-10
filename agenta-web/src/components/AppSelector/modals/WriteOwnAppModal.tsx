62ab57fed66cfcb828a54db66d51dab61ffb8ee6
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-10-10 07:24:43 +0100

    feat: Add installation instructions and styles

--- agenta-web/src/components/AppSelector/modals/WriteOwnAppModal.tsx
+++ agenta-web/src/components/AppSelector/modals/WriteOwnAppModal.tsx
@@ -1,12 +1,25 @@
+import CopyButton from "@/components/CopyButton/CopyButton"
+import {useAppTheme} from "@/components/Layout/ThemeContextProvider"
 import {Modal, Typography} from "antd"
 import React, {useEffect, useRef} from "react"
 import {createUseStyles} from "react-jss"
 import YouTube, {YouTubeProps} from "react-youtube"
 
+type StyleProps = {
+    themeMode: "dark" | "light"
+}
+
 const useStyles = createUseStyles({
     modal: {
         "& .ant-modal-close": {
             top: 23,
+        },
+        "& .ant-modal": {
+            width: "auto !important",
+        },
+        "& .ant-modal-body": {
+            display: "flex",
+            gap: 10,
         },
     },
     title: {
@@ -16,6 +29,39 @@ const useStyles = createUseStyles({
         marginTop: 16,
         height: 360,
     },
+    wrapper: {
+        width: 450,
+    },
+    copyBtn: ({themeMode}: StyleProps) => ({
+        border: "none",
+        backgroundColor: "transparent",
+        alignSelf: "flex-start",
+        color: themeMode === "light" ? "#389e0d" : "#d89614",
+        width: "auto !important",
+        height: "auto !important",
+    }),
+    container: {
+        margin: "20px 0",
+        "& li": {
+            fontSize: 16,
+            fontWeight: 600,
+            marginBottom: 10,
+        },
+    },
+    command: ({themeMode}: StyleProps) => ({
+        minHeight: 30,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        backgroundColor: themeMode === "light" ? "#f6ffed" : "#2b2111",
+        padding: "3px 10px",
+        borderRadius: 5,
+        border: `1px solid ${themeMode === "light" ? "#389e0d" : "#d89614"}`,
+        color: themeMode === "light" ? "#389e0d" : "#d89614",
+        "& span": {
+            letterSpacing: 0.3,
+        },
+    }),
 })
 
 const {Title} = Typography
@@ -23,7 +69,8 @@ const {Title} = Typography
 type Props = React.ComponentProps<typeof Modal> & {}
 
 const WriteOwnAppModal: React.FC<Props> = ({...props}) => {
-    const classes = useStyles()
+    const {appTheme} = useAppTheme()
+    const classes = useStyles({themeMode: appTheme} as StyleProps)
     const youtubePlayer = useRef<YouTube | null>(null)
 
     const onPlayerReady: YouTubeProps["onStateChange"] = (event) => {
@@ -48,7 +95,6 @@ const WriteOwnAppModal: React.FC<Props> = ({...props}) => {
                     Write your own app
                 </Title>
             }
-            width={688}
             {...props}
             afterClose={() => {
                 if (youtubePlayer.current) {
@@ -63,6 +109,52 @@ const WriteOwnAppModal: React.FC<Props> = ({...props}) => {
                     youtubePlayer.current = youtube
                 }}
             />
+            <div className={classes.wrapper}>
+                <ol>
+                    <div className={classes.container}>
+                        <li>Install Agenta Library</li>
+                        <div className={classes.command}>
+                            <span>pip install agenta</span>
+                            <CopyButton
+                                text="pip install agenta"
+                                icon={true}
+                                buttonText={""}
+                                className={classes.copyBtn}
+                            />
+                        </div>
+                    </div>
+                    <div className={classes.container}>
+                        <li>Clone and start Agenta</li>
+                        <div className={classes.command}>
+                            <span>
+                                git clone agenta <br />
+                                docker-compose up
+                            </span>
+                            <CopyButton
+                                text={"git clone agenta \ndocker-compose up"}
+                                icon={true}
+                                buttonText={""}
+                                className={classes.copyBtn}
+                            />
+                        </div>
+                    </div>
+                    <div className={classes.container}>
+                        <li>Start a variant</li>
+                        <div className={classes.command}>
+                            <span>
+                                agenta init <br />
+                                agenta variant serve --file_name app.py
+                            </span>
+                            <CopyButton
+                                text={"agenta init \nagenta variant serve --file_name app.py"}
+                                icon={true}
+                                buttonText={""}
+                                className={classes.copyBtn}
+                            />
+                        </div>
+                    </div>
+                </ol>
+            </div>
         </Modal>
     )
 }
