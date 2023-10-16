374e8a99a2eaf8c999c33b232246f380a72e1b39
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-10-16 15:46:25 +0100

    feat: custom Cypress command for creating variants and testsets

--- agenta-web/cypress/support/commands/index.ts
+++ agenta-web/cypress/support/commands/index.ts
@@ -6,6 +6,7 @@ declare global {
     namespace Cypress {
         interface Chainable {
             clickLinkAndWait(selector: string): Chainable<void>
+            createVariantsAndTestsets(): Chainable<void>
         }
     }
 }
