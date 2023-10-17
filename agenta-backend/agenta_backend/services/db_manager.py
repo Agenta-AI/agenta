8b0e8630712ee79bc6980cc09c0cc396b914ee25
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-10-17 19:21:36 +0200

    fixed import deploymentDB

--- agenta-backend/agenta_backend/services/db_manager.py
+++ agenta-backend/agenta_backend/services/db_manager.py
@@ -33,7 +33,7 @@ from agenta_backend.models.db_models import (
 if os.environ["FEATURE_FLAG"] in ["cloud"]:
     from agenta_backend.ee.models.db_models import DeploymentDB
 else:
-    from agenta_backend.db_models import DeploymentDB
+    from agenta_backend.models.db_models import DeploymentDB
 
 from agenta_backend.utils.common import check_user_org_access, engine
 from bson import ObjectId
