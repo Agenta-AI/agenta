6edfe19110c3a19ebebd40b2c9ceb56e234f7752
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2023-10-17 16:37:55 +0100

    set BACKEND_API_HOST to use container name

--- agenta-backend/agenta_backend/tests/user_profile_router/test_user_profile.py
+++ agenta-backend/agenta_backend/tests/user_profile_router/test_user_profile.py
@@ -15,7 +15,7 @@ test_client = httpx.AsyncClient()
 timeout = httpx.Timeout(timeout=5, read=None, write=5)
 
 # Set global variables
-BACKEND_API_HOST = "http://localhost:8000"
+BACKEND_API_HOST = "http://agenta-backend-1:8000"
 
 
 @pytest.mark.asyncio
