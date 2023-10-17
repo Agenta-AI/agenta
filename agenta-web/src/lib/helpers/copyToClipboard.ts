c9a76e0ded9207e768213b661270828484eada44
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-10-17 11:17:26 +0100

    build fix

--- agenta-web/src/lib/helpers/copyToClipboard.ts
+++ agenta-web/src/lib/helpers/copyToClipboard.ts
@@ -5,7 +5,9 @@ export const copyToClipboard = async (text: string, showToast = true) => {
     try {
         await navigator.clipboard.writeText(text)
         if (showToast) message.success("Copied to clipboard!")
+        return false
     } catch (err) {
         console.error("Failed to copy text to clipboard")
+        return false
     }
 }
