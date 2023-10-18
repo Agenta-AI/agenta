2e54ccc5a0c611d4fab211513adb2aae4ba60979
Abram <israelvictory87@gmail.com>
2023-10-18 13:27:28 +0100

    Refactor - modified retrieve templates info from s3 function

--- agenta-backend/agenta_backend/services/cache_manager.py
+++ agenta-backend/agenta_backend/services/cache_manager.py
@@ -1,10 +1,11 @@
 import json
 import redis
-from typing import List
+from typing import List, Dict, Any
+
 from agenta_backend.config import settings
 from agenta_backend.services.container_manager import (
     retrieve_templates_from_dockerhub,
-    get_templates_info,
+    get_templates_info_from_s3,
 )
 
 
@@ -46,29 +47,31 @@ async def retrieve_templates_from_dockerhub_cached(cache: bool) -> List[dict]:
     return response_data
 
 
-async def retrieve_templates_info_from_dockerhub_cached(cache: bool) -> List[dict]:
-    """Retrieves templates information from Docker Hub and caches the data in Redis for future use.
+async def retrieve_templates_info_from_s3(
+    cache: bool,
+) -> Dict[str, Dict[str, Any]]:
+    """Retrieves templates information from s3 and caches the data in Redis for future use.
+
     Args:
         cache: A boolean value that indicates whether to use the cached data or not.
+
     Returns:
-        Information about organization in DockerHub (cached or network-call)
+        Information about organization in s3 (cached or network-call)
     """
+
     r = redis_connection()
     if cache:
-        cached_data = r.get("org_data")
+        cached_data = r.get("temp_data")
         if cached_data is not None:
             print("Using cache...")
             return json.loads(cached_data.decode("utf-8"))
 
     # If not cached, fetch data from Docker Hub and cache it in Redis
-    response = await get_templates_info(
-        settings.docker_hub_url,
-        settings.docker_hub_repo_owner,
-        settings.docker_hub_repo_name,
+    response = await get_templates_info_from_s3(
+        "https://llm-app-json.s3.eu-central-1.amazonaws.com/llm_info.json"
     )
-    response_data = response["full_description"]
 
     # Cache the data in Redis for 60 minutes
-    r.set("org_data", json.dumps(response_data), ex=900)
+    r.set("temp_data", response, ex=900)
     print("Using network call...")
-    return response_data
+    return response
