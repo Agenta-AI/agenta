411f363dd1c7ac46e2421f15b375ca574492619e
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2023-10-19 18:00:36 +0200

    refactor business logic

--- agenta-backend/agenta_backend/routers/app_router.py
+++ agenta-backend/agenta_backend/routers/app_router.py
@@ -245,7 +245,7 @@ async def add_variant_from_image(
             tags=payload.tags,
             base_name=payload.base_name,
             config_name=payload.config_name,
-            deletable=True,
+            is_template_image=False,
             **user_org_data,
         )
         return await converters.app_variant_db_to_output(app_variant_db)
@@ -352,7 +352,7 @@ async def create_app_and_variant_from_template(
             tags=f"{image_name}",
             base_name="app",
             config_name="default",
-            deletable=False,
+            is_template_image=True,
             **user_org_data,
         )
 
