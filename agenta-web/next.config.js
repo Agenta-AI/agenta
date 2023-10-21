25730c5a54675b573f2abcf51c80d8188f0f4699
dhaneshwar <dhaneshwar6206@gmail.com>
2023-10-21 09:29:15 +0530

    moved webpack to next config object

--- agenta-web/next.config.js
+++ agenta-web/next.config.js
@@ -22,26 +22,26 @@ const nextConfig = {
             },
         ]
     },
+
+    webpack: (config, {webpack, isServer}) => {
+        const envs = {}
+
+        Object.keys(process.env).forEach((env) => {
+            if (env.startsWith("NEXT_PUBLIC_")) {
+                envs[env] = process.env[env]
+            }
+        })
+
+        if (!isServer) {
+            config.plugins.push(
+                new webpack.DefinePlugin({
+                    "process.env": JSON.stringify(envs),
+                }),
+            )
+        }
+
+        return config
+    },
 }
 
 module.exports = withMDX(nextConfig)
-
-module.exports = {
-    webpack: (config, { webpack, isServer }) => {
-      const envs = {};
-      
-      Object.keys(process.env).forEach(env => {
-        if (env.startsWith('NEXT_PUBLIC_')) {
-          envs[env] = process.env[env];
-        }
-      })
-      
-      if (!isServer) {
-        config.plugins.push(new webpack.DefinePlugin({
-          'process.env': JSON.stringify(envs),
-        }))
-      }
-  
-      return config
-    },
-  }
