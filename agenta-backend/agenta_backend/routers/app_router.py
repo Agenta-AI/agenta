1942bf7679aa48214d3aee637a4285388f87c6d7
Abram <israelvictory87@gmail.com>
2023-10-25 14:15:15 +0100

    Update - except document parsing error to explictly remove document using async driver

--- agenta-backend/agenta_backend/routers/app_router.py
+++ agenta-backend/agenta_backend/routers/app_router.py
@@ -194,6 +194,8 @@ async def list_apps(
         user_org_data: dict = await get_user_and_org_id(request.state.user_id)
         apps = await db_manager.list_apps(app_name, org_id, **user_org_data)
         return apps
+    except db_manager.DocumentParsingError as exc:
+        await db_manager.remove_document_using_driver(str(exc.primary_value), "app_db")
     except Exception as e:
         logger.error(f"list_apps exception ===> {e}")
         raise HTTPException(status_code=500, detail=str(e))
@@ -277,7 +279,10 @@ async def remove_app(app_id: str, request: Request):
                 status_code=400,
             )
         else:
-            await app_manager.remove_app(app_id=app_id, **user_org_data)
+            try:
+                await app_manager.remove_app(app_id=app_id, **user_org_data)
+            except db_manager.DocumentParsingError as exc:
+                await db_manager.remove_document_using_driver(str(exc.primary_value), "app_db")
     except DockerException as e:
         detail = f"Docker error while trying to remove the app: {str(e)}"
         raise HTTPException(status_code=500, detail=detail)
