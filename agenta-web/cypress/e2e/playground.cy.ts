57202516ef6fdec5b8bf04033ff350e8ac733afe
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-10-29 17:45:18 +0100

    removes context hook

--- agenta-web/cypress/e2e/playground.cy.ts
+++ agenta-web/cypress/e2e/playground.cy.ts
@@ -7,20 +7,15 @@ describe("Playground Prompt Test", function () {
         })
     })
 
-    context("When testing prompt functionality", () => {
-        it("Should test prompt functionality in the Playground", () => {
-            cy.get('[data-cy="testview-input-parameters-0"]').type("Germany")
-            cy.get('[data-cy="testview-input-parameters-run-button"]').click()
-            cy.get('[data-cy="testview-input-parameters-result"]').should(
-                "contain.text",
-                "Loading...",
-            )
-            cy.get('[data-cy="testview-input-parameters-result"]', {timeout: 15000}).should(
-                "contain.text",
-                "The capital of Germany is Berlin.",
-            )
-            cy.get(".ant-message-notice-content").should("not.exist")
-        })
+    it("Should test prompt functionality in the Playground", () => {
+        cy.get('[data-cy="testview-input-parameters-0"]').type("Germany")
+        cy.get('[data-cy="testview-input-parameters-run-button"]').click()
+        cy.get('[data-cy="testview-input-parameters-result"]').should("contain.text", "Loading...")
+        cy.get('[data-cy="testview-input-parameters-result"]', {timeout: 15000}).should(
+            "contain.text",
+            "The capital of Germany is Berlin.",
+        )
+        cy.get(".ant-message-notice-content").should("not.exist")
     })
 
     after(() => {
