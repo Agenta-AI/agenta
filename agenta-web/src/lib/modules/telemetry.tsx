06881e72f60801c682a4839210aa2e723423c9ce
Abram <israelvictory87@gmail.com>
2023-10-29 11:50:08 +0100

    Update - added next_public_ prefix to env vars and also modified telemetry module

--- agenta-web/src/lib/modules/telemetry.tsx
+++ agenta-web/src/lib/modules/telemetry.tsx
@@ -1,11 +1,11 @@
 import posthog from "posthog-js"
 
 // Check if tracking is enabled
-const trackingEnabled = process.env.TELEMETRY_TRACKING_ENABLED === "true"
+const trackingEnabled = process.env.NEXT_PUBLIC_TELEMETRY_TRACKING_ENABLED === "true"
 
 // Initialize the Posthog client
 if (typeof window !== "undefined" && trackingEnabled) {
-    posthog.init(process.env.POSTHOG_SECRET_KEY as string, {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_SECRET_KEY as string, {
         api_host: "https://app.posthog.com",
         // Enable debug mode in development
         loaded: (posthog) => {
