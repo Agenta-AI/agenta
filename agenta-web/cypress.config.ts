bf26b697c7e45d4423c3e1e7119112e64161462e
Abram <israelvictory87@gmail.com>
2023-10-30 18:50:32 +0100

    Cleanup - revert changes in cypress.config.ts and update frontend-test workflow to make use of incoming pr (#811) fix

--- agenta-web/cypress.config.ts
+++ agenta-web/cypress.config.ts
@@ -1,5 +1,4 @@
 import {defineConfig} from "cypress"
-import process from "process"
 
 export default defineConfig({
     video: false,
@@ -10,7 +9,7 @@ export default defineConfig({
     },
     env: {
         baseApiURL: "http://localhost/api",
-        OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+        OPENAI_API_KEY: "your_api_key_here",
         localBaseUrl: "http://localhost",
         NEXT_PUBLIC_FF: false,
     },
