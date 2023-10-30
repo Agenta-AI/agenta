2cb186edc29cc696ecdfe798e7b2d1f1cca1ff5c
Abram <israelvictory87@gmail.com>
2023-10-30 18:19:20 +0100

    Update - access openai api key from process.env

--- agenta-web/cypress.config.ts
+++ agenta-web/cypress.config.ts
@@ -1,4 +1,5 @@
 import {defineConfig} from "cypress"
+import process from "process"
 
 export default defineConfig({
     video: false,
@@ -9,7 +10,7 @@ export default defineConfig({
     },
     env: {
         baseApiURL: "http://localhost/api",
-        OPENAI_API_KEY: "your_api_key_here",
+        OPENAI_API_KEY: process.env.OPENAI_API_KEY,
         localBaseUrl: "http://localhost",
         NEXT_PUBLIC_FF: false,
     },
