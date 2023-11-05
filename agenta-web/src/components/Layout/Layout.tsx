158d95f96d6da6127de3cda55295d32b07bb457e
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-11-05 11:50:39 +0100

    fix build issues

--- agenta-web/src/components/Layout/Layout.tsx
+++ agenta-web/src/components/Layout/Layout.tsx
@@ -20,6 +20,16 @@ import {useProfileData} from "@/contexts/profile.context"
 
 const {Content, Footer} = Layout
 
+interface WindowWithIntercom extends Window {
+    attachEvent: any
+    Intercom: {
+        reattach_activator: () => void
+        update: (settings: Record<string, any>) => void
+        shutdown: () => void
+    }
+    intercomSettings?: Record<string, any>
+}
+
 type StyleProps = {
     themeMode: "dark" | "light"
     footerHeight: number
@@ -127,49 +137,49 @@ const App: React.FC<LayoutProps> = ({children}) => {
 
     useEffect(() => {
         if (user && isDemo()) {
-            window.intercomSettings = {
+            const windowWithIntercom = window as unknown as WindowWithIntercom
+
+            windowWithIntercom.intercomSettings = {
                 api_base: "https://api-iam.intercom.io",
                 app_id: "t228v7ci",
                 name: user.username,
                 email: user.email,
             }
-            ;(function () {
-                var w = window
-                var ic = w.Intercom
-                if (typeof ic === "function") {
-                    ic("reattach_activator")
-                    ic("update", window.intercomSettings)
-                } else {
-                    var d = document
-                    var i = function () {
-                        i.c(arguments)
-                    }
-                    i.q = []
-                    i.c = function (args) {
-                        i.q.push(args)
-                    }
-                    w.Intercom = i
-                    var l = function () {
-                        var s = d.createElement("script")
-                        s.type = "text/javascript"
-                        s.async = true
-                        s.src = "https://widget.intercom.io/widget/cqi6rnwr"
-                        var x = d.getElementsByTagName("script")[0]
-                        x.parentNode.insertBefore(s, x)
-                    }
-                    if (document.readyState === "complete") {
-                        l()
-                    } else if (w.attachEvent) {
-                        w.attachEvent("onload", l)
-                    } else {
-                        w.addEventListener("load", l, false)
-                    }
+
+            if (windowWithIntercom.Intercom) {
+                windowWithIntercom.Intercom.reattach_activator()
+                windowWithIntercom.Intercom.update(windowWithIntercom.intercomSettings)
+            } else {
+                const d = document
+                const i: WindowWithIntercom["Intercom"] = {
+                    reattach_activator: () => {}, // Empty implementations for fallback
+                    update: (settings: Record<string, any>) => {},
+                    shutdown: () => {},
                 }
-            })()
+                windowWithIntercom.Intercom = i
+
+                const l = () => {
+                    const s = d.createElement("script")
+                    s.type = "text/javascript"
+                    s.async = true
+                    s.src = "https://widget.intercom.io/widget/cqi6rnwr"
+                    const x = d.getElementsByTagName("script")[0]
+                    x.parentNode?.insertBefore(s, x)
+                }
+
+                if (document.readyState === "complete") {
+                    l()
+                } else if (windowWithIntercom.attachEvent) {
+                    windowWithIntercom.attachEvent("onload", l)
+                } else {
+                    windowWithIntercom.addEventListener("load", l, false)
+                }
+            }
         } else {
-            if (window.Intercom) {
-                window.Intercom("shutdown")
-                delete window.intercomSettings
+            const windowWithIntercom = window as unknown as WindowWithIntercom
+            if (windowWithIntercom.Intercom) {
+                windowWithIntercom.Intercom.shutdown()
+                delete windowWithIntercom.intercomSettings
             }
         }
     }, [user])
