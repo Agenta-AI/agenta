2714e79dc688c0803d8c4b19d662efadbd082e5f
Abram <israelvictory87@gmail.com>
2023-11-08 22:54:23 +0100

    Update - modified experimental example application for chat sdk feature

--- examples/experimental/startup_feature_ideas/app.py
+++ examples/experimental/startup_feature_ideas/app.py
@@ -3,39 +3,26 @@ import openai
 from agenta import FloatParam, MessagesInput
 
 default_prompt = (
-    "Give me 10 feature ideas to implement for a food delivery company in {country}!"
+    "Give me 10 feature ideas to implement for a food delivery company in Nigeria!"
 )
 
-ag.init(app_name="baby_name_generator", base_name="app")
+ag.init(app_name="feature_ideas", base_name="app")
 ag.config.default(
     temperature=FloatParam(0.2),
 )
 
 
 @ag.entrypoint
-def generate(
-    country: str,
-    inputs: MessagesInput,
-) -> str:
-    """
-    Generate a baby name based on the given country and gender.
-
-    Args:
-        country (str): The country to generate the name from.
-        gender (str): The gender of the baby.
-
-    Returns:
-        str: The generated baby name.
-    """
-
+def chat(
+    inputs: MessagesInput = MessagesInput([{"role": "string", "content": "string"}])
+) -> str:    
     messages = [
-        {
-            "role": message.role,
-            "content": message.content.format(country=country),
+        {git s
+            "role": message["role"],
+            "content": message["content"],
         }
-        for message in inputs.messages
+        for message in inputs
     ]
-
     chat_completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages,
