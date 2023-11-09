5bed65842eb589b8622aaac0b1ac36d946c70347
Akrem Abayed <aakrem@users.noreply.github.com>
2023-11-09 09:16:47 +0100

    fix

--- agenta-backend/agenta_backend/routers/health_router.py
+++ agenta-backend/agenta_backend/routers/health_router.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter, status
 
 router = APIRouter()
 
-@router.get("", status_code=status.HTTP_200_OK)
+@router.get("/", status_code=status.HTTP_200_OK)
 def health_check():
     return {"status": "ok"}
