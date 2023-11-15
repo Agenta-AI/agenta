c746a93fd088b3791270732cfbca1c303a112e6b
MohammedMaaz <maazkhawar25@gmail.com>
2023-11-15 05:38:15 +0500

    temp

---
+++ agenta-web/src/lib/helpers/testset.ts
@@ -0,0 +1,5 @@
+import {ChatMessage} from "../Types"
+
+export function saveChatTestset(chatMessages: ChatMessage[], nextMessage: ChatMessage) {
+    // return axios.post("/api/testset", testset)
+}
