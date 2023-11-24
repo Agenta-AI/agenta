fa66ece25db2f0ff92e6d85367a4cd252417414c
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-11-24 08:19:39 +0100

    chat: modifies single eval

--- agenta-web/src/components/EvaluationTable/AICritiqueEvaluationTable.tsx
+++ agenta-web/src/components/EvaluationTable/AICritiqueEvaluationTable.tsx
@@ -31,6 +31,7 @@ import {createUseStyles} from "react-jss"
 import {exportAICritiqueEvaluationData} from "@/lib/helpers/evaluate"
 import SecondaryButton from "../SecondaryButton/SecondaryButton"
 import {useAppTheme} from "../Layout/ThemeContextProvider"
+import {testsetRowToChatMessages} from "@/lib/helpers/testset"
 
 const {Title} = Typography
 
@@ -215,7 +216,7 @@ Answer ONLY with one of the given grading or evaluation options.
 
     const handleInputChange = (
         e: React.ChangeEvent<HTMLInputElement>,
-        rowIndex: number,
+        rowIndex: any,
         inputFieldKey: number,
     ) => {
         const newRows = [...rows]
@@ -252,6 +253,9 @@ Answer ONLY with one of the given grading or evaluation options.
                 variantData[idx].optParams!,
                 appId || "",
                 variants[idx].baseId || "",
+                variantData[idx].isChatVariant
+                    ? testsetRowToChatMessages(evaluation.testset.csvdata[rowIndex], false)
+                    : [],
             )
 
             setRowValue(rowIndex, columnName as any, result)
@@ -369,18 +373,22 @@ Answer ONLY with one of the given grading or evaluation options.
             dataIndex: "inputs",
             render: (text: any, record: AICritiqueEvaluationTableRow, rowIndex: number) => (
                 <div>
-                    {record &&
-                        record.inputs &&
-                        record.inputs.length && // initial value of inputs is array with 1 element and variantInputs could contain more than 1 element
-                        record.inputs.map((input: any, index: number) => (
-                            <div className={classes.recordInput} key={index}>
-                                <Input
-                                    placeholder={input.input_name}
-                                    value={input.input_value}
-                                    onChange={(e) => handleInputChange(e, rowIndex, index)}
-                                />
-                            </div>
-                        ))}
+                    {evaluation.testset.testsetChatColumn
+                        ? evaluation.testset.csvdata[rowIndex][
+                              evaluation.testset.testsetChatColumn
+                          ] || " - "
+                        : record &&
+                          record.inputs &&
+                          record.inputs.length && // initial value of inputs is array with 1 element and variantInputs could contain more than 1 element
+                          record.inputs.map((input: any, index: number) => (
+                              <div className={classes.recordInput} key={index}>
+                                  <Input
+                                      placeholder={input.input_name}
+                                      value={input.input_value}
+                                      onChange={(e) => handleInputChange(e, record.id, index)}
+                                  />
+                              </div>
+                          ))}
                 </div>
             ),
         },
