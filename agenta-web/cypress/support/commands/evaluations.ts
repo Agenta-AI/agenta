710ad20f2cbc4b20b563aa7cc7bdbe13b92c53a8
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-11-27 14:47:51 +0100

    fixes cypress error

--- agenta-web/cypress/support/commands/evaluations.ts
+++ agenta-web/cypress/support/commands/evaluations.ts
@@ -103,8 +103,8 @@ Cypress.Commands.add("cleanupVariantAndTestset", () => {
 
 Cypress.Commands.add("addingOpenaiKey", () => {
     cy.visit("/settings")
-    cy.get('[data-cy="openai-api-input"]').type(apiKey)
-    cy.get('[data-cy="openai-api-save"]').click()
+    cy.get('[data-cy="openai-api-input"]').eq(0).type(apiKey)
+    cy.get('[data-cy="openai-api-save"]').eq(0).click()
 })
 
 Cypress.Commands.add("removeOpenAiKey", () => {
