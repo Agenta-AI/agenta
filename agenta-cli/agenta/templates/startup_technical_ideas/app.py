b12fcd0b1da6c3efca026338986bdb4aad2e2baf
Abram <israelvictory87@gmail.com>
2023-11-28 17:16:31 +0100

    Feat - added startup_technical_ideas llm app as a template to use

---
+++ agenta-cli/agenta/templates/startup_technical_ideas/app.py
@@ -0,0 +1,40 @@
+import agenta as ag
+from agenta import FloatParam, MessagesInput, MultipleChoiceParam
+from openai import OpenAI
+
+client = OpenAI()
+
+SYSTEM_PROMPT = "You have expertise in offering technical ideas to startups."
+CHAT_LLM_GPT = [
+    "gpt-3.5-turbo",
+    "gpt-3.5-turbo-16k",
+    "gpt-3.5-turbo-0301",
+    "gpt-3.5-turbo-0613",
+    "gpt-3.5-turbo-16k-0613",
+    "gpt-4",
+]
+
+ag.init(app_name="technical_ideas", base_name="app")
+ag.config.default(
+    temperature=FloatParam(0.2),
+)
+
+
+@ag.entrypoint
+def chat(
+    model: MultipleChoiceParam = MultipleChoiceParam(CHAT_LLM_GPT),
+    inputs: MessagesInput = MessagesInput(
+        [{"role": "system", "content": SYSTEM_PROMPT}]
+    ),
+) -> str:
+    messages = [
+        {
+            "role": message["role"],
+            "content": message["content"],
+        }
+        for message in inputs
+    ]
+    chat_completion = client.chat.completions.create(
+        model=model, messages=messages, temperature=ag.config.temperature
+    )
+    return chat_completion.choices[0].message.content
