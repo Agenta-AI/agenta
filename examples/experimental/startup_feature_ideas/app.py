7590f68f97c2e696721d4671b7246d0b3e400d5c
Abram <israelvictory87@gmail.com>
2023-11-28 17:06:57 +0100

    Update - add new parameters to application

--- examples/experimental/startup_feature_ideas/app.py
+++ examples/experimental/startup_feature_ideas/app.py
@@ -1,12 +1,18 @@
-from agenta import FloatParam, MessagesInput
 import agenta as ag
+from agenta import FloatParam, MessagesInput, MultipleChoiceParam
 from openai import OpenAI
 
 client = OpenAI()
 
-default_prompt = (
-    "Give me 10 feature ideas to implement for a food delivery company in Nigeria!"
-)
+SYSTEM_PROMPT = "You have expertise in offering technical ideas to startups."
+CHAT_LLM_GPT = [
+    "gpt-3.5-turbo",
+    "gpt-3.5-turbo-16k",
+    "gpt-3.5-turbo-0301",
+    "gpt-3.5-turbo-0613",
+    "gpt-3.5-turbo-16k-0613",
+    "gpt-4",
+]
 
 ag.init(app_name="feature_ideas", base_name="app")
 ag.config.default(
@@ -16,7 +22,10 @@ ag.config.default(
 
 @ag.entrypoint
 def chat(
-    inputs: MessagesInput = MessagesInput([{"role": "string", "content": "string"}])
+    model: MultipleChoiceParam = MultipleChoiceParam(CHAT_LLM_GPT),
+    inputs: MessagesInput = MessagesInput(
+        [{"role": "system", "content": SYSTEM_PROMPT}]
+    ),
 ) -> str:
     messages = [
         {
@@ -26,6 +35,6 @@ def chat(
         for message in inputs
     ]
     chat_completion = client.chat.completions.create(
-        model="gpt-3.5-turbo", messages=messages
+        model=model, messages=messages, temperature=ag.config.temperature
     )
     return chat_completion.choices[0].message.content
