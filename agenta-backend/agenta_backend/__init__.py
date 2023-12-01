36e587357cd82b948755b7db94fb1961d78c0f35
Akrem Abayed <aakrem@users.noreply.github.com>
2023-12-01 07:18:42 +0100

    use an environment-specific __init__

--- agenta-backend/agenta_backend/__init__.py
+++ agenta-backend/agenta_backend/__init__.py
@@ -1,4 +1,6 @@
 import os
 
-if os.environ["FEATURE_FLAG"] in ["cloud", "ee"]:
-    import agenta_backend.commons.__init__
+if os.environ["FEATURE_FLAG"] in ["cloud"]:
+    import agenta_backend.cloud.__init__
+if os.environ["FEATURE_FLAG"] in ["ee"]:
+    import agenta_backend.ee.__init__
