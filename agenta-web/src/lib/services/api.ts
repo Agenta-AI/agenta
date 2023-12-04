06c90c87f61d75ad5a8068dc5d8f4ba47342216e
Abram <israelvictory87@gmail.com>
2023-12-04 06:54:03 +0100

    Update - refactor conver testsets to dummy if in use api (axios) logic

--- agenta-web/src/lib/services/api.ts
+++ agenta-web/src/lib/services/api.ts
@@ -315,11 +315,10 @@ export const deleteTestsets = async (ids: string[]) => {
 }
 
 export const convertTestsetsToDummyIfInUse = async (ids: string[]) => {
-    const response = await axios({
-        method: "delete",
-        url: `${process.env.NEXT_PUBLIC_AGENTA_API_URL}/api/testsets/to-dummy/`,
-        data: {testset_ids: ids},
-    })
+    const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_AGENTA_API_URL}/api/testsets/to-dummy/`,
+        {testset_ids: ids},
+    )
     return response.data
 }
 
