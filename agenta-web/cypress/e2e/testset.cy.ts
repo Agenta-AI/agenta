4a5300edf4107ec30e8b1f7245ac40db6ba262dc
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-12-07 15:59:27 +0100

    revert test changes

--- agenta-web/cypress/e2e/testset.cy.ts
+++ agenta-web/cypress/e2e/testset.cy.ts
@@ -33,31 +33,26 @@ describe("Testsets crud and UI functionality", () => {
         it("successfully creates the testset and navigates to the list", () => {
             const testsetName = randString(8)
             cy.get('[data-cy="testset-name-input"]').type(testsetName)
-            cy.get(".ag-cell").should("exist")
-            cy.get(".ag-cell").should("be.visible")
-            cy.get(".ag-row").should("exist")
-            cy.get(".ag-row").should("be.visible")
             cy.get(".ag-row").should("have.length", 3)
             countries.forEach((country, index) => {
-                cy.get(`.ag-center-cols-container .ag-row[row-index="${index}"]`)
-                    .within(() => {
-                        cy.get('.ag-cell').eq(1).type(country.country)
-                        cy.get('.ag-cell').eq(2).type(
-                            `The capital of ${country.country} is ${country.capital}.`,
-                        )
-                    })
+                cy.get(`.ag-center-cols-container .ag-row[row-index="${index}"]`).within(() => {
+                    cy.get(".ag-cell").eq(1).type(country.country)
+                    cy.get(".ag-cell")
+                        .eq(2)
+                        .type(`The capital of ${country.country} is ${country.capital}.`)
+                })
             })
-            // cy.intercept("/api/testsets/*").as("saveTestsetRequest")
-            // cy.get('[data-cy="testset-save-button"]').click()
-            // //wait for the save api to complete
-            // cy.wait("@saveTestsetRequest")
-            // cy.clickLinkAndWait('[data-cy="app-testsets-link"]')
-            // cy.url().should("include", "/testsets")
+            cy.intercept("/api/testsets/*").as("saveTestsetRequest")
+            cy.get('[data-cy="testset-save-button"]').click()
+            //wait for the save api to complete
+            cy.wait("@saveTestsetRequest")
+            cy.clickLinkAndWait('[data-cy="app-testsets-link"]')
+            cy.url().should("include", "/testsets")
 
-            // // validate that the new testset is in the list
-            // cy.get('[data-cy="app-testset-list"]').as("table")
-            // cy.get("@table").get(".ant-table-pagination li a").last().click()
-            // cy.get("@table").contains(testsetName).as("tempTestSet").should("be.visible")
+            // validate that the new testset is in the list
+            cy.get('[data-cy="app-testset-list"]').as("table")
+            cy.get("@table").get(".ant-table-pagination li a").last().click()
+            cy.get("@table").contains(testsetName).as("tempTestSet").should("be.visible")
         })
     })
 
