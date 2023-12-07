dd1518c5d734e6271686dde4266613518b4b75fb
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-12-07 15:41:46 +0100

    assertion test

--- agenta-web/cypress/e2e/testset.cy.ts
+++ agenta-web/cypress/e2e/testset.cy.ts
@@ -38,6 +38,15 @@ describe("Testsets crud and UI functionality", () => {
             cy.get(".ag-row").should("exist")
             cy.get(".ag-row").should("be.visible")
             cy.get(".ag-row").should("have.length", 3)
+            cy.get(`.ag-center-cols-container .ag-row[row-index="0"]`).should("be.visible")
+
+            cy.get(`.ag-center-cols-container .ag-row[row-index="0"]`).should("have.length", 3)
+            cy.get('.ag-cell[col-id="country"]').should("be.visible")
+
+            cy.get('.ag-cell[col-id="country"]').should("have.length", 3)
+            cy.get('.ag-cell[col-id="correct_answer"]').should("be.visible")
+
+            cy.get('.ag-cell[col-id="correct_answer"]').should("have.length", 3)
             // countries.forEach((country, index) => {
             //     cy.get(`.ag-center-cols-container .ag-row[row-index="${index}"]`)
             //         .within(() => {
