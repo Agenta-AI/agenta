19e48626cbc368cc116371284baa806fb5577f42
Abram <israelvictory87@gmail.com>
2023-12-08 18:36:50 +0100

    Update - modified retrieve_cloudwatch_logs function

--- agenta-backend/agenta_backend/services/logs_manager.py
+++ agenta-backend/agenta_backend/services/logs_manager.py
@@ -1,3 +1,7 @@
+# Stdlib Imports
+from typing import List, Union
+
+# Third Party Imports
 import boto3
 
 
@@ -5,34 +9,36 @@ import boto3
 client = boto3.client("logs")
 
 
-def retrieve_cloudwatch_logs(function_app_id: str):
-    log_group_name = f"/aws/lambda/app-{function_app_id}"
+def retrieve_cloudwatch_logs(function_deployment_name: str) -> Union[List[str], str]:
+    """Retrieves the log events from the newest log stream of a
+    specified CloudWatch log group associated with a Lambda function deployment.
 
-    # Describe log streams to get the newest log stream
+    Args:
+        - function_deployment_name: represents the name of the AWS Lambda function name
+
+    Returns:
+     - either a list of log event messages or a string indicating that no \
+        log events or log streams were found.
+    """
+
+    log_group_name = f"/aws/lambda/{function_deployment_name}"
     response = client.describe_log_streams(
         logGroupName=log_group_name,
-        orderBy="lastEventTimestamp",
+        orderBy="LastEventTime",
         descending=True,
         limit=1,
     )
-
     if "logStreams" in response and len(response["logStreams"]) > 0:
         newest_log_stream = response["logStreams"][0]["logStreamName"]
-
-        # Get log events of the newest log stream
         log_events_response = client.get_log_events(
             logGroupName=log_group_name, logStreamName=newest_log_stream
         )
         if "events" in log_events_response:
-            response_data = {}
             list_of_events_messages = []
             for event in log_events_response["events"]:
                 list_of_events_messages.append(event["message"])
-
-            response_data["message"] = "Log events found in the newest log stream"
-            response_data["data"] = list_of_events_messages
-            return response_data
+            return list_of_events_messages
         else:
             return "No log events found in the newest log stream"
     else:
-        return "No log streams found in the log group."
+        return "No log streams found in the log group"
