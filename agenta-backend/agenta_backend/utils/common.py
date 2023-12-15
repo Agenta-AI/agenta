2fd095b574e8462a49039ebbf6a255f6c4346a33
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2023-12-15 21:54:17 +0100

    Add docstring for APIRouter

--- agenta-backend/agenta_backend/utils/common.py
+++ agenta-backend/agenta_backend/utils/common.py
@@ -19,9 +19,27 @@ logger.setLevel(logging.DEBUG)
 
 
 class APIRouter(FastAPIRouter):
+    """
+    Extends the FastAPIRouter class to provide support for alternate paths ending with a forward slash.
+
+    Methods:
+    - api_route: Adds a route to the router with both the original path and an alternate path ending with a forward slash.
+    """
+
     def api_route(
         self, path: str, *, include_in_schema: bool = True, **kwargs: Any
     ) -> Callable[[DecoratedCallable], DecoratedCallable]:
+        """
+        Decorator method that adds a route to the router with both the original path and an alternate path ending with a forward slash.
+
+        Parameters:
+        - path (str): The original path for the route.
+        - include_in_schema (bool): Whether to include the route in the generated OpenAPI schema. Default is True.
+        - **kwargs (Any): Additional keyword arguments to pass to the underlying api_route method.
+
+        Returns:
+        - decorator (Callable[[DecoratedCallable], DecoratedCallable]): A decorator function that can be used to decorate a route function.
+        """
         if path.endswith("/"):
             path = path[:-1]
 
