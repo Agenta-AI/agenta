b2bbe2d95e96143f136ab51da2b52397e57f71b7
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-12-16 15:56:26 +0100

    feat: sync resultList

--- agenta-web/src/components/Playground/TestContextProvider.tsx
+++ agenta-web/src/components/Playground/TestContextProvider.tsx
@@ -4,13 +4,16 @@ import React, {PropsWithChildren, createContext, useState} from "react"
 export const TestContext = createContext<{
     testList: GenericObject[]
     setTestList: React.Dispatch<React.SetStateAction<GenericObject[]>>
-}>({testList: [{}], setTestList: () => {}})
+    setResultsList: React.Dispatch<React.SetStateAction<string[]>>
+    resultsList: string[]
+}>({testList: [{}], setTestList: () => {}, resultsList: [], setResultsList: () => {}})
 
 const TestContextProvider: React.FC<PropsWithChildren> = (props) => {
     const [testList, setTestList] = useState<GenericObject[]>([{}])
+    const [resultsList, setResultsList] = useState<string[]>(testList.map(() => ""))
 
     return (
-        <TestContext.Provider value={{testList, setTestList}}>
+        <TestContext.Provider value={{testList, setTestList, resultsList, setResultsList}}>
             {props.children}
         </TestContext.Provider>
     )
