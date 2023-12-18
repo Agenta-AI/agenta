f59ae50a4328a04b3064e00ae3eb2e2f39538431
MohammedMaaz <maazkhawar25@gmail.com>
2023-12-19 00:40:09 +0500

    added batch execution logic in auto evaluations

--- agenta-web/src/components/Playground/Views/TestView.tsx
+++ agenta-web/src/components/Playground/Views/TestView.tsx
@@ -283,7 +283,7 @@ const App: React.FC<TestViewProps> = ({inputParams, optParams, variant, isChatVa
             ?.querySelectorAll("[data-cy=testview-input-parameters-run-button]")
             .forEach((btn) => funcs.push(() => (btn as HTMLButtonElement).click()))
 
-        batchExecute(funcs, {allowRetry: true, batchSize: 10})
+        batchExecute(funcs)
     }
 
     const handleAddRow = () => {
