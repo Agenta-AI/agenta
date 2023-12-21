62422f921f00e6a796fd54ef89bb189bdf933718
Akrem Abayed <aakrem@users.noreply.github.com>
2023-12-21 16:29:48 +0100

    return the whole evaluation object

--- agenta-cli/agenta/client/client.py
+++ agenta-cli/agenta/client/client.py
@@ -565,4 +565,5 @@ def run_evaluation(app_name: str, host: str, api_key: str = None) -> str:
         raise APIRequestError(
             f"Request to run evaluations failed with status code {response.status_code} and error message: {error_message}."
         )
-    return response.json()["app_id"]
+    print(response.json())
+    return response.json()
