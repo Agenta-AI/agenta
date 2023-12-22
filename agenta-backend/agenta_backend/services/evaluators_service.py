7bacb393bad916ef14b80861aeb9e38876d0e5cc
Abram <israelvictory87@gmail.com>
2023-12-22 20:44:52 +0100

    Refactor - type evaluators_service functions

--- agenta-backend/agenta_backend/services/evaluators_service.py
+++ agenta-backend/agenta_backend/services/evaluators_service.py
@@ -1,14 +1,15 @@
 import re
+from typing import Any
 
 
-def auto_exact_match(variant_output, correct_answer):
+def auto_exact_match(variant_output: str, correct_answer: str):
     if variant_output == correct_answer:
         return 1
     else:
         return 0
 
 
-def auto_similarity_match(variant_output, correct_answer):
+def auto_similarity_match(variant_output: str, correct_answer: str):
     set1 = set(variant_output.split())
     set2 = set(correct_answer.split())
     intersect = set1.intersection(set2)
@@ -18,22 +19,21 @@ def auto_similarity_match(variant_output, correct_answer):
     return similarity
 
 
-def auto_regex_test(test_string, regex, should_match):
+def auto_regex_test(test_string: str, regex: Any, should_match: bool):
     re_pattern = re.compile(regex, re.IGNORECASE)
     result = bool(re_pattern.search(test_string))
     return result == should_match
 
 
 def evaluate(
-    evaluator_name,
-    correct_answer,
-    variant_output,
-    *additional_args,
-    **additional_kwargs,
+    evaluator_name: str,
+    correct_answer: str,
+    variant_output :str,
+    *additional_args: dict,
+    **additional_kwargs: dict,
 ):
     try:
         evaluation_function = globals()[evaluator_name]
-
         return evaluation_function(
             correct_answer, variant_output, *additional_args, **additional_kwargs
         )
