723daf513aaf61777cdc886fc90677374c8cdd10
Abram <israelvictory87@gmail.com>
2023-12-22 13:29:52 +0100

    Update - modified aggregate evaluator results function

--- agenta-backend/agenta_backend/tasks/evaluations.py
+++ agenta-backend/agenta_backend/tasks/evaluations.py
@@ -13,16 +13,17 @@ from agenta_backend.services.db_manager import (
     fetch_evaluator_config,
     get_deployment_by_objectid,
     fetch_testset_by_id,
+    create_evaluator_config,
+    create_aggregated_results,
     create_new_evaluation_scenario,
     update_evaluation_with_aggregated_results,
 )
-from agenta_backend.models.api.evaluation_model import EvaluatorConfig, NewEvaluation
+from agenta_backend.models.api.evaluation_model import NewEvaluation
 
 from agenta_backend.models.db_models import (
     AppDB,
     EvaluationScenarioOutputDB,
     EvaluationScenarioResult,
-    EvaluatorConfigDB,
     AggregatedResultDB,
     Result,
 )
@@ -37,12 +38,6 @@ def evaluate(
     loop = asyncio.get_event_loop()
     new_evaluation = NewEvaluation(**new_evaluation_data)
     app = AppDB(**app_data)
-
-    # NOTE: This will generate a name in case it's run from cli
-    # (
-    #     evaluators_configs,
-    #     evaluator_key_name_mapping,
-    # ) = process_evaluators_configs(app, new_evaluation.evaluators_configs)
 
     testset = loop.run_until_complete(fetch_testset_by_id(testset_id))
     new_evaluation_db = loop.run_until_complete(fetch_evaluation_by_id(evaluation_id))
@@ -97,10 +92,11 @@ def evaluate(
                 )
             )
 
-    aggregated_results = aggregate_evaluator_results(
-        app, evaluators_aggregated_data, evaluator_key_name_mapping
+    aggregated_results = loop.run_until_complete(
+        aggregate_evaluator_results(
+            app, evaluators_aggregated_data, evaluator_key_name_mapping
+        )
     )
-
     updated_evaluation = loop.run_until_complete(
         update_evaluation_with_aggregated_results(
             new_evaluation_db.id, aggregated_results
@@ -108,53 +104,18 @@ def evaluate(
     )
 
 
-# def process_evaluators_configs(
-#     app: AppDB,
-#     evaluators_configs: List[EvaluatorConfig],
-# ) -> Tuple[List[EvaluatorConfigDB], Dict[str, str]]:
-#     """Process evaluators_configs to include names if missing and return a mapping of evaluator keys to names."""
-
-#     processed_configs = []
-#     evaluator_key_name_mapping = {}
-
-#     for config in evaluators_configs:
-#         # Handle the 'name' field with a default value if it's None
-#         name = getattr(config, "name", f"Evaluator_{uuid.uuid4()}")
-
-#         processed_config = EvaluatorConfigDB(
-#             user=app.user,
-#             organization=app.organization,
-#             name=name,
-#             app=app,
-#             evaluator_key=config.evaluator_key,
-#         )
-
-#         processed_configs.append(processed_config)
-#         evaluator_key_name_mapping[
-#             processed_config.evaluator_key
-#         ] = processed_config.name
-
-#     return processed_configs, evaluator_key_name_mapping
-
-
-def aggregate_evaluator_results(
-    app, evaluators_aggregated_data, evaluator_key_name_mapping
-):
+async def aggregate_evaluator_results(
+    app: AppDB, evaluators_aggregated_data: dict, evaluator_key_name_mapping: dict
+) -> List[AggregatedResultDB]:
     aggregated_results = []
     for evaluator_key, values in evaluators_aggregated_data.items():
         average_value = sum(values) / len(values) if values else 0
         evaluator_name = evaluator_key_name_mapping.get(
             evaluator_key, "Unknown Evaluator"
         )
-        aggregated_result_db = AggregatedResultDB(
-            evaluator_config=EvaluatorConfigDB(
-                app=app,
-                user=app.user,
-                organization=app.organization,
-                name=evaluator_name,
-                evaluator_key=evaluator_key,
-            ),
-            result=Result(type="number", value=str(average_value)),
+        evaluator_config = await create_evaluator_config(
+            app, app.user, app.organization, evaluator_name, evaluator_key
         )
+        aggregated_result_db = await create_aggregated_results(evaluator_config, str(average_value))
         aggregated_results.append(aggregated_result_db)
     return aggregated_results
