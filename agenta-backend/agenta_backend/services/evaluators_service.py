a983aeb346265c9486ba41149ffe3b2e39806849
Abram <israelvictory87@gmail.com>
2023-12-22 20:44:52 +0100

    Refactor - type evaluators_service functions

--- agenta-backend/agenta_backend/services/evaluators_service.py
+++ agenta-backend/agenta_backend/services/evaluators_service.py
@@ -1,16 +1,18 @@
 import re
+from typing import Any
 from langchain.chains import LLMChain
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 
-def auto_exact_match(variant_output, correct_answer, settings_values):
+
+def auto_exact_match(variant_output: str, correct_answer: str, settings_values: dict):
     if variant_output == correct_answer:
         return 1
     else:
         return 0
 
 
-def auto_similarity_match(variant_output, correct_answer, settings_values):
+def auto_similarity_match(variant_output: str, correct_answer: str, settings_values: dict):
     set1 = set(variant_output.split())
     set2 = set(correct_answer.split())
     intersect = set1.intersection(set2)
@@ -22,23 +24,22 @@ def auto_similarity_match(variant_output, correct_answer, settings_values):
     return is_similar
 
 
-def auto_regex_test(test_string, regex, should_match):
+def auto_regex_test(test_string: str, regex: Any, should_match: bool):
     re_pattern = re.compile(regex, re.IGNORECASE)
     result = bool(re_pattern.search(test_string))
     return result == should_match
 
 
 def evaluate(
-    evaluator_name,
-    correct_answer,
-    variant_output,
-    settings_values,
-    *additional_args,
-    **additional_kwargs,
+    evaluator_name: str,
+    correct_answer: str,
+    variant_output :str,
+    settings_values: dict,
+    *additional_args: dict,
+    **additional_kwargs: dict,
 ):
     try:
         evaluation_function = globals()[evaluator_name]
-
         return evaluation_function(
             correct_answer, variant_output, settings_values, *additional_args, **additional_kwargs
         )
