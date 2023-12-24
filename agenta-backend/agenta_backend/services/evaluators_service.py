2bd828609e33d3828f272291404d586bd3473c1b
Abram <israelvictory87@gmail.com>
2023-12-24 09:21:00 +0100

    Update - modified module to hint-type args

--- agenta-backend/agenta_backend/services/evaluators_service.py
+++ agenta-backend/agenta_backend/services/evaluators_service.py
@@ -35,7 +35,7 @@ def evaluate(
     correct_answer: str,
     variant_output :str,
     settings_values: dict,
-    *additional_args: dict,
+    *additional_args: tuple,
     **additional_kwargs: dict,
 ):
     try:
