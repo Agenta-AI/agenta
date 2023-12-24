9e7d9652133cd8f3e399eeb31396be73d195dfd4
Abram <israelvictory87@gmail.com>
2023-12-24 09:20:31 +0100

    Update - modified evaluate task to accomodate auto_regex_test

--- agenta-backend/agenta_backend/tasks/evaluations.py
+++ agenta-backend/agenta_backend/tasks/evaluations.py
@@ -56,11 +56,18 @@ def evaluate(
                 evaluator_config = loop.run_until_complete(
                     fetch_evaluator_config(evaluator_config_id)
                 )
-                result = evaluators_service.evaluate(
-                    evaluator_config.evaluator_key,
-                    data_point["correct_answer"],
-                    variant_output,
-                )
+                if evaluator_config.evaluator_key in ["auto_regex_test"]:
+                    result = evaluators_service.auto_regex_test(
+                        variant_output,
+                        evaluator_config.settings_values.get("regex_pattern"),
+                        evaluator_config.settings_values.get("regex_should_match"),
+                    ) # result will come out as a bool (True or False)
+                else:
+                    result = evaluators_service.evaluate(
+                        evaluator_config.evaluator_key,
+                        data_point["correct_answer"],
+                        variant_output,
+                    )
 
                 result_object = EvaluationScenarioResult(
                     evaluator_key=evaluator_config.evaluator_key,
