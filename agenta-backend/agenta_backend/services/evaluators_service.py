901af8e1b0d48fbdcd7ae47b114cc270aede2965
Akrem Abayed <aakrem@users.noreply.github.com>
2023-12-24 08:48:06 +0100

    fix auto similarity match

--- agenta-backend/agenta_backend/services/evaluators_service.py
+++ agenta-backend/agenta_backend/services/evaluators_service.py
@@ -1,21 +1,25 @@
 import re
+from langchain.chains import LLMChain
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
 
-
-def auto_exact_match(variant_output, correct_answer):
+def auto_exact_match(variant_output, correct_answer, settings_values):
     if variant_output == correct_answer:
         return 1
     else:
         return 0
 
 
-def auto_similarity_match(variant_output, correct_answer):
+def auto_similarity_match(variant_output, correct_answer, settings_values):
     set1 = set(variant_output.split())
     set2 = set(correct_answer.split())
     intersect = set1.intersection(set2)
     union = set1.union(set2)
 
     similarity = len(intersect) / len(union)
-    return similarity
+
+    is_similar = True if similarity > settings_values["similarity_threshold"] else False
+    return is_similar
 
 
 def auto_regex_test(test_string, regex, should_match):
@@ -28,6 +32,7 @@ def evaluate(
     evaluator_name,
     correct_answer,
     variant_output,
+    settings_values,
     *additional_args,
     **additional_kwargs,
 ):
@@ -35,7 +40,7 @@ def evaluate(
         evaluation_function = globals()[evaluator_name]
 
         return evaluation_function(
-            correct_answer, variant_output, *additional_args, **additional_kwargs
+            correct_answer, variant_output, settings_values, *additional_args, **additional_kwargs
         )
     except KeyError:
         raise ValueError(f"Evaluation method '{evaluator_name}' not found.")
