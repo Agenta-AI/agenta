0a1ac285515bdb76c60f988ec7aa0a42407f824a
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2023-12-27 16:06:23 +0100

    compare view route

---
+++ agenta-web/src/pages/apps/[app_id]/evaluations-new/compare/index.tsx
@@ -0,0 +1,8 @@
+import EvaluationCompare from "@/components/pages/evaluations/evaluationCompare/evaluationCompare"
+import React from "react"
+
+const EvaluationCompareDetails = () => {
+    return <EvaluationCompare />
+}
+
+export default EvaluationCompareDetails
