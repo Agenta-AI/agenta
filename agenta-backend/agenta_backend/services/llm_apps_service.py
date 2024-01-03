6f35a30364321189bef74c48cf9ace96efffb06c
Abram <israelvictory87@gmail.com>
2024-01-03 15:18:43 +0100

    Update - modified batch_invoke function

--- agenta-backend/agenta_backend/services/llm_apps_service.py
+++ agenta-backend/agenta_backend/services/llm_apps_service.py
@@ -37,34 +37,49 @@ async def get_llm_app_output(uri: str, input: Any) -> AppOutput:
 async def run_with_retry(
     uri: str, input_data: Any, max_retry_count: int, retry_delay: int
 ) -> AppOutput:
+
     retries = 0
+    last_exception = None
     while retries < max_retry_count:
         try:
             result = await get_llm_app_output(uri, input_data)
             return result
         except (httpx.TimeoutException, httpx.ConnectTimeout, httpx.ConnectError) as e:
+            last_exception = e
             print(f"Error in evaluation. Retrying in {retry_delay} seconds:", e)
             await asyncio.sleep(retry_delay)
             retries += 1
 
-    # If max retries reached, raise the last exception
-    raise e
+    # If max retries reached, return the last exception
+    return AppOutput(output=None, status=str(last_exception))
 
 
-async def batch_invoke(uri: str, testset_data: List[dict]) -> List[AppOutput]:
-    batch_size = 10  # Number of evaluations to make in each batch
-    max_retries = 3  # Maximum number of times to retry a failed evaluation
-    retry_delay = 3  # Delay before retrying a failed evaluation (in seconds)
-    delay_between_batches = 5  # Delay between batches (in seconds)
-    list_of_app_outputs: List[AppOutput] = [] # Outputs after running all batches
+async def batch_invoke(
+    uri: str, testset_data: List[dict], rate_limit_config: dict
+) -> List[AppOutput]:
+    batch_size = rate_limit_config[
+        "batch_size"
+    ]  # Number of testset to make in each batch
+    max_retries = rate_limit_config[
+        "max_retries"
+    ]  # Maximum number of times to retry the failed llm call
+    retry_delay = rate_limit_config[
+        "retry_delay"
+    ]  # Delay before retrying the failed llm call (in seconds)
+    delay_between_batches = rate_limit_config[
+        "delay_between_batches"
+    ]  # Delay between batches (in seconds)
+
+    list_of_app_outputs: List[AppOutput] = []  # Outputs after running all batches
 
     async def run_batch(start_idx: int):
         print(f"Preparing {start_idx} batch...")
         end_idx = min(start_idx + batch_size, len(testset_data))
         for index in range(start_idx, end_idx):
-            print(f"Running datapoint(s) in {start_idx} batch...")
             try:
-                batch_output: AppOutput = await run_with_retry(uri, testset_data[index], max_retries, retry_delay)
+                batch_output: AppOutput = await run_with_retry(
+                    uri, testset_data[index], max_retries, retry_delay
+                )
                 list_of_app_outputs.append(batch_output)
                 print(f"Adding outputs to batch {start_idx}")
             except Exception as exc:
