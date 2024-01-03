c823104ecfdba82f7dffebbb26e6976e256b7e4c
Abram <israelvictory87@gmail.com>
2024-01-03 08:55:48 +0100

    Feat - implemented batch invoke rpm rate-limiting logic

--- agenta-backend/agenta_backend/services/llm_apps_service.py
+++ agenta-backend/agenta_backend/services/llm_apps_service.py
@@ -1,15 +1,18 @@
-from typing import Any
+import asyncio
+import logging
+from typing import Any, List
+
+from agenta_backend.models.api.evaluation_model import AppOutput
 
 import httpx
-import backoff
 
 
-@backoff.on_exception(
-    backoff.expo,
-    (httpx.TimeoutException, httpx.ConnectTimeout, httpx.ConnectError),
-    max_tries=2,
-)
-def get_llm_app_output(uri: str, input: Any) -> Any:
+# Set logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+async def get_llm_app_output(uri: str, input: Any) -> AppOutput:
     url = f"{uri}/generate"
 
     # TODO: adjust these hardcoded values in this payload
@@ -23,9 +26,58 @@ def get_llm_app_output(uri: str, input: Any) -> Any:
         "inputs": {"country": input},
     }
 
-    with httpx.Client() as client:
-        response = client.post(
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
             url, json=payload, timeout=httpx.Timeout(timeout=5, read=None, write=5)
         )
         response.raise_for_status()
-        return response.json()
+        return AppOutput(output=response.json(), status="success")
+
+
+async def run_with_retry(
+    uri: str, input_data: Any, max_retry_count: int, retry_delay: int
+) -> AppOutput:
+    retries = 0
+    while retries < max_retry_count:
+        try:
+            result = await get_llm_app_output(uri, input_data)
+            return result
+        except (httpx.TimeoutException, httpx.ConnectTimeout, httpx.ConnectError) as e:
+            print(f"Error in evaluation. Retrying in {retry_delay} seconds:", e)
+            await asyncio.sleep(retry_delay)
+            retries += 1
+
+    # If max retries reached, raise the last exception
+    raise e
+
+
+async def batch_invoke(uri: str, testset_data: List[dict]) -> List[AppOutput]:
+    batch_size = 10  # Number of evaluations to make in each batch
+    max_retries = 3  # Maximum number of times to retry a failed evaluation
+    retry_delay = 3  # Delay before retrying a failed evaluation (in seconds)
+    delay_between_batches = 5  # Delay between batches (in seconds)
+    list_of_app_outputs: List[AppOutput] = [] # Outputs after running all batches
+
+    async def run_batch(start_idx: int):
+        print(f"Preparing {start_idx} batch...")
+        end_idx = min(start_idx + batch_size, len(testset_data))
+        for index in range(start_idx, end_idx):
+            print(f"Running datapoint(s) in {start_idx} batch...")
+            try:
+                batch_output: AppOutput = await run_with_retry(uri, testset_data[index], max_retries, retry_delay)
+                list_of_app_outputs.append(batch_output)
+                print(f"Adding outputs to batch {start_idx}")
+            except Exception as exc:
+                logger.info(
+                    f"Error processing batch[{start_idx}]:[{end_idx}] ==> {str(exc)}"
+                )
+
+        # Schedule the next batch with a delay
+        next_batch_start_idx = end_idx
+        if next_batch_start_idx < len(testset_data):
+            await asyncio.sleep(delay_between_batches)
+            await run_batch(next_batch_start_idx)
+
+    # Start the first batch
+    await run_batch(0)
+    return list_of_app_outputs
