3bd8c7165db7776674d18faa7de82179ee6077bd
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2024-01-12 13:10:11 +0100

    define function to get workspace and organization id

--- agenta-backend/agenta_backend/services/db_manager.py
+++ agenta-backend/agenta_backend/services/db_manager.py
@@ -1922,3 +1922,76 @@ async def update_evaluation(
             setattr(evaluation, key, value)
     await evaluation.save()
     return evaluation
+
+
+async def get_object_workspace_org_id(object_id: str, type: str) -> str:
+    """
+    Get the organization and workspace id of the object.
+
+    Args:
+        object_id (str): The ID of the object.
+        type (str): The type of the object.
+
+    Returns:
+        dict: The organization and workspace id of the object.
+    """
+    try:
+        if type == "app":
+            app = await fetch_app_by_id(object_id)
+            organization_id = str(app.organization.id)
+            workspace_id = str(app.workspace.id)
+            
+        elif type == "app_variant":
+            app_variant = await fetch_app_variant_by_id(object_id)
+            organization_id = str(app_variant.organization.id)
+            workspace_id = str(app_variant.workspace.id)
+            
+        elif type == "base":
+            base = await fetch_base_by_id(object_id)
+            organization_id = str(base.organization.id)
+            workspace_id = str(base.workspace.id)
+            
+        elif type == "deployment":
+            deployment = await get_deployment_by_objectid(object_id)
+            organization_id = str(deployment.organization.id)
+            workspace_id = str(deployment.workspace.id)
+            
+        elif type == "testset":
+            testset = await fetch_testset_by_id(object_id)
+            organization_id = str(testset.organization.id)
+            workspace_id = str(testset.workspace.id)
+            
+        elif type == "evaluation":
+            evaluation = await fetch_evaluation_by_id(object_id)
+            organization_id = str(evaluation.organization.id)
+            workspace_id = str(evaluation.workspace.id)
+            
+        elif type == "evaluation_scenario":
+            evaluation_scenario = await fetch_evaluation_scenario_by_id(object_id)
+            organization_id = str(evaluation_scenario.organization.id)
+            workspace_id = str(evaluation_scenario.workspace.id)
+            
+        elif type == "evaluator_config":
+            evaluator_config = await fetch_evaluator_config(object_id)
+            organization_id = str(evaluator_config.organization.id)
+            workspace_id = str(evaluator_config.workspace.id)
+        
+        elif type == "human_evaluation":
+            human_evaluation = await fetch_human_evaluation_by_id(object_id)
+            organization_id = str(human_evaluation.human_evaluation.id)
+            workspace_id = str(human_evaluation.human_evaluation.id)
+        
+        elif type == "human_evaluation_scenario":
+            human_evaluation_scenario = await fetch_human_evaluation_scenario_by_id(
+                object_id
+            )
+            organization_id = str(human_evaluation_scenario.organization.id)
+            workspace_id = str(human_evaluation_scenario.workspace.id)
+            
+        else:
+            raise ValueError(f"Unknown object type: {type}")
+        
+        return {"organization_id": organization_id, "workspace_id": workspace_id}
+    except Exception as e:
+        raise e
+
