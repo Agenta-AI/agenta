3ef2fb7c05cfd9f562a6c66bed8c6aceadb28cae
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2024-01-14 14:16:29 +0100

    Define Workspace Pydantic Models

--- agenta-backend/agenta_backend/models/api/workspace_models.py
+++ agenta-backend/agenta_backend/models/api/workspace_models.py
@@ -1,7 +1,23 @@
 from datetime import datetime
-from bson import ObjectId
-from typing import Optional, List
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
+from typing import Optional, List, Dict
+from agenta_backend.models.api.user_models import TimestampModel
+from agenta_backend.models.db_models import WorkspaceRole, Permission
+
+
+class WorkspacePermission(BaseModel):
+    role_name: WorkspaceRole
+    permissions: List[Permission]
+
+
+class WorkspaceMember(BaseModel):
+    user_id: str
+    roles: List[WorkspacePermission]
+    
+    
+class WorkspaceMemberOutput(BaseModel):
+    user: Dict
+    roles: List[WorkspacePermission]
 
 
 class Workspace(BaseModel):
@@ -9,16 +25,25 @@ class Workspace(BaseModel):
     name: str
     description: Optional[str]
     type: Optional[str]
-    
+    members: Optional[List[WorkspaceMember]]
+
+
+class WorkspaceOutput(TimestampModel):
+    id: str
+    name: str
+    description: Optional[str]
+    type: Optional[str]
+    organization: str
+    members: Optional[List[WorkspaceMemberOutput]]
+
     
 class CreateWorkspace(BaseModel):
     name: str
     description: Optional[str]
     type: Optional[str]
-    organization_id: str
 
 
-class WorkspaceUpdate(BaseModel):
+class UpdateWorkspace(BaseModel):
     name: Optional[str]
     description: Optional[str]
     updated_at: Optional[datetime]
