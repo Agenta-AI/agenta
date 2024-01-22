a2473ee246c9bf5b17ebaddc76cae9bde3d3c850
Abram <israelvictory87@gmail.com>
2024-01-22 10:30:16 +0100

    Update - improve query lookup for evaluation scenarios

--- agenta-backend/agenta_backend/services/evaluation_service.py
+++ agenta-backend/agenta_backend/services/evaluation_service.py
@@ -290,10 +290,10 @@ async def fetch_evaluation_scenarios_for_evaluation(
         **user_org_data,
     )
     scenarios = await EvaluationScenarioDB.find(
-        EvaluationScenarioDB.evaluation.id == ObjectId(evaluation.id), fetch_links=True
+        EvaluationScenarioDB.evaluation.id == ObjectId(evaluation.id)
     ).to_list()
     eval_scenarios = [
-        converters.evaluation_scenario_db_to_pydantic(scenario)
+        converters.evaluation_scenario_db_to_pydantic(scenario, str(evaluation.id))
         for scenario in scenarios
     ]
     return eval_scenarios
@@ -321,10 +321,11 @@ async def fetch_human_evaluation_scenarios_for_evaluation(
     )
     scenarios = await HumanEvaluationScenarioDB.find(
         HumanEvaluationScenarioDB.evaluation.id == ObjectId(evaluation.id),
-        fetch_links=True,
     ).to_list()
     eval_scenarios = [
-        converters.human_evaluation_scenario_db_to_pydantic(scenario)
+        converters.human_evaluation_scenario_db_to_pydantic(
+            scenario, str(evaluation.id)
+        )
         for scenario in scenarios
     ]
     return eval_scenarios
