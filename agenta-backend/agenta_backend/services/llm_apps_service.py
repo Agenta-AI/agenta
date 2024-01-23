9de50b90e82dbf6cdb98ba45bf38db33bea84fba
Abram <israelvictory87@gmail.com>
2024-01-23 22:11:07 +0100

    Update - added timeout to httpx client

--- agenta-backend/agenta_backend/services/llm_apps_service.py
+++ agenta-backend/agenta_backend/services/llm_apps_service.py
@@ -1,8 +1,8 @@
-import asyncio
 import json
+import asyncio
 import logging
-from typing import Any, Dict, List
 import traceback
+from typing import Any, Dict, List
 
 import httpx
 
@@ -244,6 +244,7 @@ async def get_parameters_from_openapi(uri: str) -> List[Dict]:
 
 async def _get_openai_json_from_uri(uri):
     async with httpx.AsyncClient() as client:
-        resp = await client.get(uri)
+        timeout = httpx.Timeout(timeout=5, read=None, write=5)
+        resp = await client.get(uri, timeout=timeout)
         json_data = json.loads(resp.text)
         return json_data
