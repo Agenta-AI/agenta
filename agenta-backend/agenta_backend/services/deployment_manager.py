0be7c93c9801c64ec47162d7783c0f6beea3a3b5
Abram <israelvictory87@gmail.com>
2024-01-23 22:36:03 +0100

    Update - refactor _get_deployment_uri function and remove redundant imports

--- agenta-backend/agenta_backend/services/deployment_manager.py
+++ agenta-backend/agenta_backend/services/deployment_manager.py
@@ -139,3 +139,7 @@ async def validate_image(image: Image) -> bool:
             f"Image {image.docker_id} with tags {image.tags} not found"
         )
     return True
+
+
+def get_deployment_uri(deployment: DeploymentDB) -> str:
+    return deployment.uri.replace("http://localhost", "http://host.docker.internal")
