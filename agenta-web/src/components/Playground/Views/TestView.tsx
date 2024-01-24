e211ff4e65f1bded0d15fde5d530ad2ca9046b48
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2024-01-24 11:29:59 +0100

    changed playground input background on error

--- agenta-web/src/components/Playground/Views/TestView.tsx
+++ agenta-web/src/components/Playground/Views/TestView.tsx
@@ -244,12 +244,12 @@ const BoxComponent: React.FC<BoxComponentProps> = ({
                         placeholder="Results will be shown here"
                         disabled={!result || result === LOADING_TEXT}
                         style={{
-                            background: result?.startsWith("❌ Error code")
+                            background: result?.startsWith("❌")
                                 ? appTheme === "dark"
                                     ? "#490b0b"
                                     : "#fff1f0"
                                 : "",
-                            color: result?.startsWith("❌ Error code")
+                            color: result?.startsWith("❌")
                                 ? appTheme === "dark"
                                     ? "#ffffffd9"
                                     : "#000000e0"
