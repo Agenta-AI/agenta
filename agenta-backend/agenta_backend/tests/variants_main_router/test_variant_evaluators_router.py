1cf3cdd1537f3fe1d66dd77c01ba0642046a705f
Abram <israelvictory87@gmail.com>
2024-01-30 17:38:26 +0100

    Update - resolve failing testcase to create new evaluation

--- agenta-backend/agenta_backend/tests/variants_main_router/test_variant_evaluators_router.py
+++ agenta-backend/agenta_backend/tests/variants_main_router/test_variant_evaluators_router.py
@@ -193,7 +193,7 @@ async def test_create_evaluation():
 
     assert response.status_code == 200
     assert response_data["app_id"] == payload["app_id"]
-    assert response_data["status"] == EvaluationStatusEnum.EVALUATION_STARTED
+    assert response_data["status"]["value"] == EvaluationStatusEnum.EVALUATION_STARTED
     assert response_data is not None
 
 
