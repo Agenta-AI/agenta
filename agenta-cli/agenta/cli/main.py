cce17baca9730ee2865a4b539f25ef05635f0fdd
Abram <israelvictory87@gmail.com>
2024-02-22 02:17:38 +0100

    Refactor - update use of client

--- agenta-cli/agenta/cli/main.py
+++ agenta-cli/agenta/cli/main.py
@@ -162,6 +162,7 @@ def init(app_name: str, backend_host: str):
                     click.echo(click.style(f"Error: {ex}", fg="red"))
                     sys.exit(1)
 
+        filtered_org = None
         if where_question == "On agenta cloud":
             which_organization = questionary.select(
                 "Which organization do you want to create the app for?",
@@ -180,7 +181,7 @@ def init(app_name: str, backend_host: str):
 
         # Get app_id after creating new app in the backend server
         try:
-            app_id = client.create_app(
+            app_id = client.apps.create_app(
                 app_name=app_name,
                 organization_id=filtered_org.id if filtered_org else None,
             ).app_id
