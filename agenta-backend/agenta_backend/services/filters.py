dde04aa0525e93786b475ac05769cca77ac8879f
Abram <israelvictory87@gmail.com>
2024-03-02 21:50:45 +0100

    Refactor - remove filtering and aggregation logic from redis cache service

--- agenta-backend/agenta_backend/services/filters.py
+++ agenta-backend/agenta_backend/services/filters.py
@@ -5,6 +5,7 @@ from agenta_backend.models.api.observability_models import (
     Trace,
     ObservabilityData,
     GenerationFilterParams,
+    ObservabilityDashboardData,
     ObservabilityDashboardDataRequestParams,
 )
 
@@ -13,6 +14,16 @@ def filter_observability_dashboard_data_by_params(
     params: ObservabilityDashboardDataRequestParams,
     observability_data: List[Union[ObservabilityData, Dict[str, Any]]],
 ):
+    """Filter observability dashboard data by the provided params.
+
+    Args:
+        params (ObservabilityDashboardDataRequestParams): the params to filter data with
+        observability_data (List[Union[ObservabilityData, Dict[str, Any]]]): data to filter
+
+    Returns:
+        filtered list of data
+    """
+
     filtered_data = []
     if observability_data != [] and isinstance(observability_data[0], dict):
         filtered_data = [
@@ -44,9 +55,70 @@ def filter_observability_dashboard_data_by_params(
 def filter_document_by_filter_params(
     filters: GenerationFilterParams, document: Union[Span, Trace]
 ) -> bool:
+    """Filter document by the provided params.
+
+    Args:
+        filters (GenerationFilterParams): the params to filter with
+        document (Union[Span, Trace]): expects either a span or trace document
+
+    Returns:
+        bool: True or False if param matches condition
+    """
+
     if filters:
         if filters.variant and document.variant.variant_name != filters.variant:
             return False
         if filters.environment and document.environment != filters.environment:
             return False
     return True
+
+
+def filter_and_aggregate_cache_observability_data(
+    params: ObservabilityDashboardDataRequestParams,
+    observability_data: List[Dict[str, Any]],
+) -> ObservabilityDashboardData:
+    """Filter and aggregate cache data for the dashboard.
+
+    Args:
+        params (ObservabilityDashboardDataRequestParams): the params to filter data with
+        observability_data (List[Dict[str, Any]]): cached data to filter
+
+    Returns:
+        aggregated dashboard data
+    """
+
+    len_of_spans_db = len(observability_data)
+    filtered_data = filter_observability_dashboard_data_by_params(
+        params, observability_data
+    )
+    if filtered_data == []:
+        return ObservabilityDashboardData(
+            **{
+                "data": [],
+                "total_count": 0,
+                "failure_rate": 0,
+                "total_cost": 0,
+                "avg_cost": 0,
+                "avg_latency": 0,
+                "total_tokens": 0,
+                "avg_tokens": 0,
+            }
+        )
+    return ObservabilityDashboardData(
+        **{
+            "data": filtered_data,
+            "total_count": len_of_spans_db,
+            "failure_rate": 0,
+            "total_cost": round(sum([span["cost"] for span in observability_data]), 5),
+            "avg_cost": round(
+                sum([span["cost"] for span in observability_data]) / len_of_spans_db, 5
+            ),
+            "avg_latency": round(
+                sum([span["latency"] for span in observability_data]) / len_of_spans_db,
+                5,
+            ),
+            "total_tokens": sum([span["total_tokens"] for span in observability_data]),
+            "avg_tokens": sum([span["total_tokens"] for span in observability_data])
+            / len_of_spans_db,
+        }
+    )
