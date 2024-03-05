7740e6359990fd2df0fbe0f2b93f5a67c4f8a2b9
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2024-03-05 12:56:42 +0100

    refactored and improved code

--- agenta-web/src/components/SaveTestsetModal/SaveTestsetModal.tsx
+++ agenta-web/src/components/SaveTestsetModal/SaveTestsetModal.tsx
@@ -1,32 +1,36 @@
+import React, {useEffect, useState} from "react"
+import {useAppId} from "@/hooks/useAppId"
 import {Evaluation, EvaluationScenario} from "@/lib/Types"
 import {EvaluationFlow} from "@/lib/enums"
 import {createNewTestset} from "@/lib/services/api"
-import {Form, Input, Modal, message} from "antd"
-import React, {Dispatch, SetStateAction} from "react"
+import {Form, Input, Modal} from "antd"
 
 type EvaluationRow = EvaluationScenario & {
     evaluationFlow: EvaluationFlow
 } & {[variantId: string]: string}
 
-interface SaveTestsetModalProps {
-    appId: string
-    isTestsetModalOpen: boolean
-    setIsTestsetModalOpen: Dispatch<SetStateAction<boolean>>
+type SaveTestsetModalProps = {
     evaluation: Evaluation
     rows: EvaluationRow[]
-}
+    onSuccess: (testsetName: string) => void
+} & React.ComponentProps<typeof Modal>
 
 const SaveTestsetModal: React.FC<SaveTestsetModalProps> = ({
-    setIsTestsetModalOpen,
-    appId,
-    isTestsetModalOpen,
     evaluation,
     rows,
+    onSuccess,
+    ...props
 }) => {
+    const appId = useAppId()
     const [form] = Form.useForm()
+    const [submitLoading, setSubmitLoading] = useState(false)
 
-    const handleSave = async () => {
-        const testsetName = form.getFieldValue("name")
+    useEffect(() => {
+        form.resetFields()
+    }, [props.open])
+
+    const handleSave = (values: {testset_name: string}) => {
+        setSubmitLoading(true)
         const newRows = rows.map((row, index) => {
             if (evaluation.testset.testsetChatColumn) {
                 return {
@@ -42,26 +46,27 @@ const SaveTestsetModal: React.FC<SaveTestsetModalProps> = ({
             }
         })
 
-        await createNewTestset(appId, testsetName, newRows)
-            .then(() => {
-                message.success(`Row added to the "${testsetName}" test set!`)
+        createNewTestset(appId, values.testset_name, newRows)
+            .then(() => onSuccess(values.testset_name))
+            .catch(console.error)
+            .finally(() => {
+                setSubmitLoading(false)
             })
-            .finally(() => setIsTestsetModalOpen(false))
     }
 
     return (
         <Modal
             title="Add new test set"
             okText="Submit"
-            open={isTestsetModalOpen}
             destroyOnClose
-            onCancel={() => setIsTestsetModalOpen(false)}
-            onOk={() => handleSave()}
+            onOk={form.submit}
+            okButtonProps={{loading: submitLoading}}
+            {...props}
         >
-            <Form form={form}>
+            <Form form={form} onFinish={handleSave}>
                 <Form.Item
                     rules={[{required: true, message: "Please enter test set name!"}]}
-                    name="name"
+                    name="testset_name"
                 >
                     <Input placeholder="Test set name" />
                 </Form.Item>
