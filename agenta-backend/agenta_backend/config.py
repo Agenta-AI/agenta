a555c435f15237ae4c8dbaf1a5719e8d4cb2e7ca
Akrem Abayed <aakrem@users.noreply.github.com>
2024-04-04 18:38:54 +0200

    remove docker_hub_repo_owner and docker_hub_repo_name to use AGENTA_TEMPLATE_REPO

--- agenta-backend/agenta_backend/config.py
+++ agenta-backend/agenta_backend/config.py
@@ -12,8 +12,6 @@ os.environ["DOCKER_REGISTRY_URL"] = toml_config["docker_registry_url"]
 os.environ["REGISTRY"] = toml_config["registry"]
 os.environ["DATABASE_URL"] = toml_config["database_url"]
 os.environ["DOCKER_HUB_URL"] = toml_config["docker_hub_url"]
-os.environ["DOCKER_HUB_REPO_OWNER"] = toml_config["docker_hub_repo_owner"]
-os.environ["DOCKER_HUB_REPO_NAME"] = toml_config["docker_hub_repo_name"]
 os.environ["DATABASE_MODE"] = os.environ.get("DATABASE_MODE", "v2")
 
 
@@ -22,8 +20,7 @@ class Settings(BaseSettings):
     registry: str
     database_url: str
     docker_hub_url: str
-    docker_hub_repo_owner: str
-    docker_hub_repo_name: str
+
 
 
 settings = Settings()
