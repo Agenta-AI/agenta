98aed7946b90c68312cef74be4204d1672bcaf56
Abram <israelvictory87@gmail.com>
2024-04-07 03:47:28 +0100

    Minor refactor - set DEBUG to False and remove redundant imports

--- agenta-cli/agenta/docker/docker_utils.py
+++ agenta-cli/agenta/docker/docker_utils.py
@@ -3,19 +3,15 @@ import shutil
 import tarfile
 import tempfile
 from pathlib import Path
-from tempfile import TemporaryDirectory
 
-import docker
-from agenta.config import settings
-from docker.models.images import Image
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-DEBUG = True
+DEBUG = False
 
 
-def create_dockerfile(out_folder: Path):
+def create_dockerfile(out_folder: Path) -> Path:
     """Creates a dockerfile based on the template in the out_folder.
 
     Arguments:
