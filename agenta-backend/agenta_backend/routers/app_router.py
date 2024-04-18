d91a244f5cc5ddce92414b775f42b1a3114e2ea1
Akrem Abayed <aakrem@users.noreply.github.com>
2024-04-18 17:47:12 +0200

    remove unsupported models

--- agenta-backend/agenta_backend/routers/app_router.py
+++ agenta-backend/agenta_backend/routers/app_router.py
@@ -570,14 +570,11 @@ async def create_app_and_variant_from_template(
                 "MISTRAL_API_KEY",
                 "COHERE_API_KEY",
                 "ANTHROPIC_API_KEY",
-                "PERPLEXITYAI_API_KEY",
                 "ANYSCALE_API_KEY",
-                "FIREWORKS_AI_API_KEY",
-                "GROQ_API_KEY",
+                "PERPLEXITYAI_API_KEY",
                 "DEEPINFRA_API_KEY",
                 "TOGETHERAI_API_KEY",
                 "ALEPHALPHA_API_KEY",
-                "BASETEN_API_KEY",
                 "OPENROUTER_API_KEY",
             ]
             missing_keys = [
