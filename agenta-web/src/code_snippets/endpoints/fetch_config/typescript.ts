4fa8f25736af5591ed93326158b11e3f95ed826a
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2024-05-01 19:16:19 +0100

    fixed curl code spacing, improved env active button styles and reviewed ts code

--- agenta-web/src/code_snippets/endpoints/fetch_config/typescript.ts
+++ agenta-web/src/code_snippets/endpoints/fetch_config/typescript.ts
@@ -4,38 +4,38 @@ export default function tsCode(baseId: string, env_name: string): string {
     const codeString = `
     import axios from 'axios';
 
-    const getConfig = async (baseId: string, environmentName = null) => {
-       try {
-           const baseUrl = 'https://cloud.agenta.ai/api';
-           const params = {
-               base_id: baseId,
-               environment_name: environmentName
-           };
+    const getConfig = async (baseId: string, environmentName: string) => {
+        try {
+            const baseUrl = 'https://cloud.agenta.ai/api';
+            const params = {
+                base_id: baseId,
+                environment_name: environmentName
+            };
 
-           const response = await axios.get(baseUrl + "/configs", {
-               params: params,
-               headers: {
-                   'Authorization': "Bearer YOUR_API_KEY",
-                   'Content-Type': 'application/json'
-               },
-               timeout: 60000
-           });
+            const response = await axios.get(baseUrl + "/configs", {
+                params: params,
+                headers: {
+                    'Authorization': "Bearer YOUR_API_KEY",
+                    'Content-Type': 'application/json'
+                },
+                timeout: 60000
+            });
 
-           if (response.status >= 200 && response.status < 300) {
-               return response.data;
-           } else if (response.status === 422) {
-               throw new Error(JSON.stringify(response.data));
-           }
-       } catch (error) {
-           if (error.response) {
-               console.error("API Error: " + error.response.status, error.response.data);
-           } else if (error.request) {
-               console.error('API Error: No response received', error.request);
-           } else {
-               console.error('Error', error.message);
-           }
-           throw error;
-       }
+            if (response.status >= 200 && response.status < 300) {
+                return response.data;
+            } else if (response.status === 422) {
+                throw new Error(JSON.stringify(response.data));
+            }
+        } catch (error: any) {
+            if (error.response) {
+                console.error("API Error: " + error.response.status, error.response.data);
+            } else if (error.request) {
+                console.error('API Error: No response received', error.request);
+            } else {
+                console.error('Error', error.message);
+            }
+            throw error;
+        }
     };
 
     getConfig('${baseId}', '${env_name}').then(console.log).catch(console.error);
