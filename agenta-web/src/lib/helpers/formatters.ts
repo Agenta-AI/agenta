b61ace1cfa307df57dd360ecdb2889cfee469409
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2024-05-03 12:29:31 +0100

    added fallback value for null cost and usage values

--- agenta-web/src/lib/helpers/formatters.ts
+++ agenta-web/src/lib/helpers/formatters.ts
@@ -12,10 +12,22 @@ export const formatNumber = (value = 0) => {
     return intlNumber.format(value)
 }
 
-export const formatCurrency = (value = 0) => {
-    return intlCurrency.format(value)
+export const formatCurrency = (value: number) => {
+    if (value === 0) {
+        return "-"
+    } else {
+        return intlCurrency.format(value)
+    }
 }
 
 export const formatLatency = (value = 0) => {
     return `${Math.round(value * 1000)}ms`
+}
+
+export const formatTokenUsage = (value: number) => {
+    if (value === 0) {
+        return "-"
+    } else {
+        return value
+    }
 }
