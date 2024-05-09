803b35e003bba4672291d090c80c3ba7a1e6977c
Abram <israelvictory87@gmail.com>
2024-05-09 21:06:24 +0100

    Feat: include workflows based on observability integration without agenta hosted apps

---
+++ examples/app_with_observability/workflows/tracing_from_hosted.py
@@ -0,0 +1,49 @@
+import os
+import requests
+import agenta as ag
+
+
+os.environ["AGENTA_LLM_RUN_PLAYGROUND"] = "cloud"
+API_URL = "https://xxxxxxx.xxx"
+
+
+tracing = ag.Tracing(
+    app_id="xxxxxxxx",
+    host="https://cloud.agenta.ai",
+    api_key="xxxxxx.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+)
+
+
+def hosted_platform_call(content: str):
+    span = tracing.start_span(
+        name="gpt3.5-llm-call",
+        spankind="llm",
+        input={"content": content},
+    )
+    response = requests.post(
+        url=API_URL,
+        json={
+            "inputs": [{"role": "user", "content": content}],
+            "environment": "production",
+        },
+    )
+    tracing.end_span(outputs=response.json(), span=span)
+    return response.json()
+
+
+def query(content: str):
+    tracing.start_parent_span(
+        name="query",
+        inputs={"content": content},
+        environment=os.environ.get("AGENTA_LLM_RUN_PLAYGROUND"),
+    )
+    response = hosted_platform_call(content=content)
+    tracing.end_recording(outputs=response, span=tracing.active_trace)
+    return response
+
+
+if __name__ == "__main__":
+    result = query(
+        content="How is a vector database used when building LLM applications?"
+    )
+    print("\n\n============== Result ============== \n\n", result["message"])
