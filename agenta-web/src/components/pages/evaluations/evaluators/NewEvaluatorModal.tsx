ffb3c2d90322b374c70d9e1965835018f59f8cd1
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2024-05-20 06:47:47 +0100

    filtered out evaluators when direct_use is true or settings_template without correct_answer_keys from evaluators list

--- agenta-web/src/components/pages/evaluations/evaluators/NewEvaluatorModal.tsx
+++ agenta-web/src/components/pages/evaluations/evaluators/NewEvaluatorModal.tsx
@@ -240,7 +240,9 @@ const NewEvaluatorModal: React.FC<Props> = ({
     ...props
 }) => {
     const classes = useStyles()
-    const evaluators = useAtom(evaluatorsAtom)[0].filter((item) => !item.direct_use)
+    const evaluators = useAtom(evaluatorsAtom)[0].filter(
+        (item) => !item.direct_use || item.settings_template.correct_answer_keys,
+    )
     const [selectedEval, setSelectedEval] = useState<Evaluator | null>(null)
     const [submitLoading, setSubmitLoading] = useState(false)
     const [searchTerm, setSearchTerm] = useState<string>("")
