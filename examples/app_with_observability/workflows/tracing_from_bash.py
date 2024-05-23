3233e4f9457b764134a44fcb17ad333207f1acf7
Abram <israelvictory87@gmail.com>
2024-05-23 12:10:00 +0100

    Refactor (workflows): update usecases of using observability with agenta

--- examples/app_with_observability/workflows/tracing_from_bash.py
+++ examples/app_with_observability/workflows/tracing_from_bash.py
@@ -6,6 +6,10 @@ from openai import AsyncOpenAI
 
 os.environ["AGENTA_LLM_RUN_ENVIRONMENT"] = "cloud"
 os.environ["OPENAI_API_KEY"] = "sk-xxxxxxxxxxxxxxxxxxxxxxxxxx"
+os.environ["AGENTA_BASE_URL"] = "https://cloud.agenta.ai"
+os.environ["AGENTA_APP_ID"] = "xxxxxxxxxxxxxxxxxxx"
+os.environ["AGENTA_API_KEY"] = "xxxxxxxxx.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
 
 client = AsyncOpenAI()
 
@@ -13,25 +17,20 @@ default_prompt = (
     "Give me 10 names for a baby from this country {country} with gender {gender}!!!!"
 )
 
-ag.init(
-    app_id="xxxxxxxx",
-    host="https://cloud.agenta.ai",
-    api_key="xxxxxx.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-)
-tracing = ag.llm_tracing()
+ag.init()
 ag.config.default(
     temperature=ag.FloatParam(0.2), prompt_template=ag.TextParam(default_prompt)
 )
 
 
-@ag.span(type="LLM")
+@ag.instrument(spankind="llm")
 async def llm_call(prompt):
     chat_completion = await client.chat.completions.create(
         model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}]
     )
-    tracing.set_span_attribute(
+    ag.tracing.set_span_attribute(
         "model_config", {"model": "gpt-3.5-turbo", "temperature": ag.config.temperature}
-    )  # translate to {"model_config": {"model": "gpt-3.5-turbo", "temperature": 0.2}}
+    )  # translates to {"model_config": {"model": "gpt-3.5-turbo", "temperature": 0.2}}
     tokens_usage = chat_completion.usage.dict()
     return {
         "cost": ag.calculate_token_usage("gpt-3.5-turbo", tokens_usage),
@@ -40,7 +39,7 @@ async def llm_call(prompt):
     }
 
 
-@ag.trace()
+@ag.instrument(config=ag.config.all())
 async def generate(country: str, gender: str):
     """
     Generate a baby name based on the given country and gender.
@@ -57,4 +56,4 @@ async def generate(country: str, gender: str):
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(generate(country="South Africa", gender="Male"))
+    loop.run_until_complete(generate(country="Germany", gender="Male"))
