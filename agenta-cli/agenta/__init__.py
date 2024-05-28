48e92ba8ba8e683ab972f201f6b0396e15c9ec4d
Abram <israelvictory87@gmail.com>
2024-05-28 09:19:22 +0100

    Minor refactor: allow initialization of tracing sdk in agenta_init

--- agenta-cli/agenta/__init__.py
+++ agenta-cli/agenta/__init__.py
@@ -14,11 +14,12 @@ from .sdk.types import (
     BinaryParam,
 )
 from .sdk.tracing.llm_tracing import Tracing
-from .sdk.decorators import tracing
 from .sdk.decorators.tracing import instrument
 from .sdk.decorators.llm_entrypoint import entrypoint, app
-from .sdk.agenta_init import Config, init
+from .sdk.agenta_init import Config, AgentaSingleton, init
 from .sdk.utils.helper.openai_cost import calculate_token_usage
 from .sdk.client import Agenta
 
 config = PreInitObject("agenta.config", Config)
+DEFAULT_AGENTA_SINGLETON_INSTANCE = AgentaSingleton()
+tracing = DEFAULT_AGENTA_SINGLETON_INSTANCE.tracing # type: ignore
