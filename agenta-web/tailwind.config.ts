6aef916cc864a9e54cb6bab43001a52be5f336ce
ashrafchowdury <ashrafdev365@gmail.com>
2024-05-29 14:40:44 +0600

    refactore: removed unused packages and moved type-dependencies into devDependencies

--- agenta-web/tailwind.config.ts
+++ agenta-web/tailwind.config.ts
@@ -130,7 +130,7 @@ const config: Config = {
                 /^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
         },
     ],
-    plugins: [require("@headlessui/tailwindcss"), require("@tailwindcss/forms")],
+    plugins: [require("@tailwindcss/forms")],
 }
 
 export default config
