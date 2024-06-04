861a469f63d8f09e1fb9b388a178ab498db04c84
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2024-06-04 13:57:11 +0100

    revert previous changes

--- agenta-web/src/services/deployment/index.ts
+++ agenta-web/src/services/deployment/index.ts
@@ -10,8 +10,13 @@ import {getAgentaApiUrl} from "@/lib/helpers/utils"
 //  - delete: DELETE data from server
 
 export const fetchEnvironments = async (appId: string): Promise<Environment[]> => {
-    const {data} = await axios(`${getAgentaApiUrl()}/api/apps/${appId}/environments/`)
+    const response = await fetch(`${getAgentaApiUrl()}/api/apps/${appId}/environments/`)
 
+    if (response.status !== 200) {
+        throw new Error("Failed to fetch environments")
+    }
+
+    const data: Environment[] = await response.json()
     return data
 }
 
