5f28addc8bf644ccc5983ed6f5a3e13447222507
Abram <israelvictory87@gmail.com>
2024-06-05 12:34:58 +0100

    refactor (sdk): refactor the litellm callback handler based on #pullrequestreview-2090993436

--- agenta-cli/agenta/sdk/tracing/callbacks.py
+++ agenta-cli/agenta/sdk/tracing/callbacks.py
@@ -28,20 +28,14 @@ def litellm_handler():
             return ag.tracing
 
         def log_pre_api_call(self, model, messages, kwargs):
+            call_type = kwargs.get("call_type")
+            span_kind = (
+                "llm" if call_type in ["completion", "acompletion"] else "embedding"
+            )
             self._trace.start_span(
-                name=kwargs["litellm_params"][
-                    "litellm_call_id"
-                ],  # make use of the litellm_call_id as the span name
-                input=kwargs["input"],
-                spankind=(
-                    "llm"
-                    if kwargs.get("call_type") in ["completion", "acompletion"]
-                    else (
-                        "embedding"
-                        if kwargs.get("call_type") in ["embedding", "aembedding"]
-                        else "unset"
-                    )
-                ),
+                name=f"{span_kind}_call",
+                input={"messages": kwargs["messages"]},
+                spankind=span_kind,
             )
             self._trace.set_span_attribute(
                 {
@@ -61,7 +55,7 @@ def litellm_handler():
                     "message": kwargs.get(
                         "complete_streaming_response"
                     ),  # the complete streamed response (only set if `completion(..stream=True)`)
-                    "usage": kwargs.get("usage"),  # litellm calculates usage
+                    "usage": response_obj.usage.dict(),  # litellm calculates usage
                     "cost": kwargs.get(
                         "response_cost"
                     ),  # litellm calculates response cost
@@ -75,7 +69,7 @@ def litellm_handler():
             self._trace.end_span(
                 outputs={
                     "message": response_obj.choices[0].message.content,
-                    "usage": kwargs.get("usage"),  # litellm calculates usage
+                    "usage": response_obj.usage.dict(),  # litellm calculates usage
                     "cost": kwargs.get(
                         "response_cost"
                     ),  # litellm calculates response cost
@@ -87,7 +81,7 @@ def litellm_handler():
         ):
             self._trace.update_span_status(span=self._trace.active_span, value="ERROR")
             self._trace.set_span_attribute(
-                attributes={
+                {
                     "traceback_exception": kwargs[
                         "traceback_exception"
                     ],  # the traceback generated via `traceback.format_exc()`
@@ -99,7 +93,7 @@ def litellm_handler():
             self._trace.end_span(
                 outputs={
                     "message": kwargs["exception"],  # the Exception raised
-                    "usage": kwargs.get("usage"),  # litellm calculates usage
+                    "usage": response_obj.usage.dict(),  # litellm calculates usage
                     "cost": kwargs.get(
                         "response_cost"
                     ),  # litellm calculates response cost
@@ -115,7 +109,7 @@ def litellm_handler():
                     "message": kwargs.get(
                         "complete_streaming_response"
                     ),  # the complete streamed response (only set if `completion(..stream=True)`)
-                    "usage": kwargs.get("usage"),  # litellm calculates usage
+                    "usage": response_obj.usage.dict(),  # litellm calculates usage
                     "cost": kwargs.get(
                         "response_cost"
                     ),  # litellm calculates response cost
@@ -129,7 +123,7 @@ def litellm_handler():
             self._trace.end_span(
                 outputs={
                     "message": response_obj.choices[0].message.content,
-                    "usage": kwargs.get("usage"),  # litellm calculates usage
+                    "usage": response_obj.usage.dict(),  # litellm calculates usage
                     "cost": kwargs.get(
                         "response_cost"
                     ),  # litellm calculates response cost
@@ -141,7 +135,7 @@ def litellm_handler():
         ):
             self._trace.update_span_status(span=self._trace.active_span, value="ERROR")
             self._trace.set_span_attribute(
-                attributes={
+                {
                     "traceback_exception": kwargs[
                         "traceback_exception"
                     ],  # the traceback generated via `traceback.format_exc()`
@@ -153,7 +147,7 @@ def litellm_handler():
             self._trace.end_span(
                 outputs={
                     "message": kwargs["exception"],  # the Exception raised
-                    "usage": kwargs.get("usage"),  # litellm calculates usage
+                    "usage": response_obj.usage.dict(),  # litellm calculates usage
                     "cost": kwargs.get(
                         "response_cost"
                     ),  # litellm calculates response cost
