be88912294460c3254440ed0ef17182e7af9493c
Abram <israelvictory87@gmail.com>
2024-06-07 12:36:27 +0100

    refactor (tools): provide default db to use

--- agenta-backend/agenta_backend/models/db_engine.py
+++ agenta-backend/agenta_backend/models/db_engine.py
@@ -83,8 +83,7 @@ class DBEngine:
     """
 
     def __init__(self) -> None:
-        self.mode = os.environ.get("DATABASE_MODE", "v2")
-        self.db_url = f"{os.environ.get('POSTGRES_URI')}/agenta_{self.mode}"
+        self.db_url = os.environ.get("POSTGRES_URI")
         self.engine = create_async_engine(self.db_url, echo=True)
         self.async_session = sessionmaker(
             self.engine, expire_on_commit=False, class_=AsyncSession
