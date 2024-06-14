f4759b523faa100fd939b1354650535836670f33
Abram <israelvictory87@gmail.com>
2024-06-14 16:46:40 +0100

    refactor (backend): improve converters and results_service for human evaluation and scenarios

--- agenta-backend/agenta_backend/services/results_service.py
+++ agenta-backend/agenta_backend/services/results_service.py
@@ -1,4 +1,5 @@
-from beanie import PydanticObjectId as ObjectId
+import uuid
+from typing import Sequence
 
 from agenta_backend.services import db_manager
 from agenta_backend.utils.common import isCloudEE
@@ -19,23 +20,35 @@ else:
 
 
 async def fetch_results_for_evaluation(evaluation: HumanEvaluationDB):
-    evaluation_scenarios = await HumanEvaluationScenarioDB.find(
-        HumanEvaluationScenarioDB.evaluation.id == evaluation.id,
-    ).to_list()
+    evaluation_scenarios = await db_manager.fetch_human_evaluation_scenarios(
+        evaluation_id=str(evaluation.id)
+    )
 
     results = {}
     if len(evaluation_scenarios) == 0:
         return results
 
-    results["variants"] = [str(variant) for variant in evaluation.variants]
-    variant_names = []
-    for variant_id in evaluation.variants:
-        variant = await db_manager.get_app_variant_instance_by_id(str(variant_id))
-        variant_name = variant.variant_name if variant else str(variant_id)
+    evaluation_variants = await db_manager.fetch_human_evaluation_variants(
+        human_evaluation_id=str(evaluation.id)
+    )
+    results["variants"] = [
+        str(evaluation_variant.variant_id)
+        for evaluation_variant in evaluation_variants
+    ]
+
+    variant_names: list[str] = []
+    for evaluation_variant in evaluation_variants:
+        variant_name = (
+            evaluation_variant.variant.variant_name
+            if isinstance(evaluation_variant.variant_id, uuid.UUID)
+            else str(evaluation_variant.variant_id)
+        )
         variant_names.append(str(variant_name))
+
     results["variant_names"] = variant_names
     results["nb_of_rows"] = len(evaluation_scenarios)
-    if evaluation.evaluation_type == EvaluationType.human_a_b_testing:
+
+    if evaluation.evaluation_type == EvaluationType.human_a_b_testing: # type: ignore
         results.update(
             await _compute_stats_for_human_a_b_testing_evaluation(evaluation_scenarios)
         )
@@ -52,7 +65,9 @@ async def _compute_stats_for_evaluation(evaluation_scenarios: list, classes: lis
     return results
 
 
-async def _compute_stats_for_human_a_b_testing_evaluation(evaluation_scenarios: list):
+async def _compute_stats_for_human_a_b_testing_evaluation(
+    evaluation_scenarios: Sequence[EvaluationScenarioDB]
+):
     results = {}
     results["variants_votes_data"] = {}
     results["flag_votes"] = {}
@@ -79,12 +94,14 @@ async def _compute_stats_for_human_a_b_testing_evaluation(evaluation_scenarios: 
         if len(evaluation_scenarios)
         else 0
     )
+
     for scenario in evaluation_scenarios:
         if scenario.vote not in results["variants_votes_data"]:
             results["variants_votes_data"][scenario.vote] = {}
             results["variants_votes_data"][scenario.vote]["number_of_votes"] = 1
         else:
             results["variants_votes_data"][scenario.vote]["number_of_votes"] += 1
+
     for key, value in results["variants_votes_data"].items():
         value["percentage"] = round(
             value["number_of_votes"] / len(evaluation_scenarios) * 100, 2
@@ -93,27 +110,17 @@ async def _compute_stats_for_human_a_b_testing_evaluation(evaluation_scenarios: 
 
 
 async def fetch_results_for_single_model_test(evaluation_id: str):
-    results = await HumanEvaluationScenarioDB.find(
-        HumanEvaluationScenarioDB.evaluation.id == ObjectId(evaluation_id)
-    ).to_list()
+    evaluation_scenarios = await db_manager.fetch_human_evaluation_scenarios(
+        evaluation_id=str(evaluation_id)
+    )
     scores_and_counts = {}
-    for result in results:
-        score = result.score
+    for evaluation_scenario in evaluation_scenarios:
+        score = evaluation_scenario.score
+        if isinstance(score, str):
+            if score.isdigit():  # Check if the string is a valid integer
+                score = int(score)
+            else:
+                continue  # Skip if the string is not a valid integer
+
         scores_and_counts[score] = scores_and_counts.get(score, 0) + 1
     return scores_and_counts
-
-
-async def fetch_average_score_for_custom_code_run(evaluation_id: str) -> float:
-    eval_scenarios = await EvaluationScenarioDB.find(
-        EvaluationScenarioDB.evaluation.id == ObjectId(evaluation_id)
-    ).to_list()
-
-    list_of_scores = []
-    for scenario in eval_scenarios:
-        score = scenario.score
-        if not scenario.score:
-            score = 0
-        list_of_scores.append(round(float(score), 2))
-
-    average_score = sum(list_of_scores) / len(list_of_scores)
-    return average_score
