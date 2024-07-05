7b2f6fda1453c1df1ba9d897514c8a50438691b8
aakrem <akrem@agenta.ai>
2024-07-05 13:30:17 +0200

    enhancements to mongo db engine

--- agenta-backend/agenta_backend/migrations/mongo_to_postgres/mongo_db_engine.py
+++ agenta-backend/agenta_backend/migrations/mongo_to_postgres/mongo_db_engine.py
@@ -2,21 +2,19 @@ import os
 from pymongo import MongoClient
 from agenta_backend.utils.common import isCloudEE
 
-MONGO_URI_SRC = os.environ.get("MONGODB_URI")
+MONGO_URI = os.environ.get("MONGODB_URI")
 MONGO_DATABASE_MODE = os.environ.get("DATABASE_MODE")
 MONGO_DB_NAME_SRC = f"agenta_{MONGO_DATABASE_MODE}"
 
 
 if isCloudEE():
-    MONGO_URI_DEST = os.environ.get("MONGO_URI_DEST", None)
-    MONGO_DEST_DATABASE_MODE = os.environ.get("MONGO_DEST_DATABASE_MODE", None)
-    MONGO_DB_NAME_DEST = (
-        f"agenta_{MONGO_DEST_DATABASE_MODE}" if MONGO_DEST_DATABASE_MODE else None
-    )
-    if not MONGO_URI_DEST and not MONGO_DB_NAME_DEST:
-        mongo_client_dest = MongoClient(MONGO_URI_DEST)
+    MONGO_DB_NAME_DEST = os.environ.get("MONGO_DB_NAME_DEST", None)
+    MONGO_DB_NAME_DEST = f"agenta_{MONGO_DB_NAME_DEST}" if MONGO_DB_NAME_DEST else None
 
-mongo_client_src = MongoClient(MONGO_URI_SRC)
+    if MONGO_DB_NAME_DEST:
+        mongo_client_dest = MongoClient(MONGO_URI)
+
+mongo_client_src = MongoClient(MONGO_URI)
 
 
 def get_mongo_db(mode):
