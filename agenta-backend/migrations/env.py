b616888974bfd076b42a0ae46a7dd141cfabab14
Abram <israelvictory87@gmail.com>
2024-07-08 10:35:58 +0100

    minor refactor (backend): replace use of 'db_engine.db_url' to 'db_engine.postgres_uri'

--- agenta-backend/migrations/env.py
+++ agenta-backend/migrations/env.py
@@ -7,9 +7,14 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+from agenta_backend.models.db_engine import db_engine
+
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", db_engine.postgres_uri)  # type: ignore
+
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,9 +23,11 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
+from agenta_backend.models.db_models import Base  # noqa: F403
+
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -30,15 +37,12 @@ target_metadata = None
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
-
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
     here as well.  By skipping the Engine creation
     we don't even need a DBAPI to be available.
-
     Calls to context.execute() here emit the given string to the
     script output.
-
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
@@ -62,7 +66,6 @@ def do_run_migrations(connection: Connection) -> None:
 async def run_async_migrations() -> None:
     """In this scenario we need to create an Engine
     and associate a connection with the context.
-
     """
 
     connectable = async_engine_from_config(
