487bc38e8aafb097e3ff0e054da0c4e4a6f2731a
Abram <israelvictory87@gmail.com>
2024-07-23 15:43:04 +0100

    feat (backend): created rag faithfulness and context relevancy evaluators function

--- agenta-backend/agenta_backend/tasks/evaluations.py
+++ agenta-backend/agenta_backend/tasks/evaluations.py
@@ -433,6 +433,8 @@ async def aggregate_evaluator_results(
             "auto_contains_all",
             "auto_contains_json",
             "auto_levenshtein_distance",
+            "rag_faithfulness",
+            "rag_context_relevancy",
         ]:
             result = aggregation_service.aggregate_float(results)
 
