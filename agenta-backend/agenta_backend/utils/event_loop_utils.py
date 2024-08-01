5c5ad15caec1bebd09e43477e8e1d1ef4aabcb28
Abram <israelvictory87@gmail.com>
2024-08-01 21:26:52 +0100

    feat (backend): add utility function to ensure event loop is retrieved or created

---
+++ agenta-backend/agenta_backend/utils/event_loop_utils.py
@@ -0,0 +1,19 @@
+import asyncio
+
+
+def ensure_event_loop() -> asyncio.AbstractEventLoop:
+    """
+    Ensure that there is an event loop available in the current thread.
+    If there isn't one, create a new event loop and set it.
+
+    Returns:
+        asyncio.AbstractEventLoop: The event loop for the current thread.
+    """
+
+    try:
+        loop = asyncio.get_event_loop()
+    except RuntimeError as e:
+        if "There is no current event loop in thread" in str(e):
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+    return loop
