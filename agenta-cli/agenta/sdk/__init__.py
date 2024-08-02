0fdbdc6b2d4feb40e9fb113845103327ae95ea60
Mahmoud Mabrouk <mahmoud@agenta.ai>
2024-08-02 19:40:45 +0200

    feat(sdk): AGE-486 from_route implemented

--- agenta-cli/agenta/sdk/__init__.py
+++ agenta-cli/agenta/sdk/__init__.py
@@ -20,6 +20,7 @@ from .decorators.tracing import instrument
 from .decorators.llm_entrypoint import entrypoint, app, route
 from .agenta_init import Config, AgentaSingleton, init
 from .utils.helper.openai_cost import calculate_token_usage
+from .config_manager import ConfigManager
 
 config = PreInitObject("agenta.config", Config)
 DEFAULT_AGENTA_SINGLETON_INSTANCE = AgentaSingleton()
