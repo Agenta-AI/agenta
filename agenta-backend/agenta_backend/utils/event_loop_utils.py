9ba496f66bbad26e9156573d3c3ee08f9f7e12ce
Abram <israelvictory87@gmail.com>
2024-08-02 14:49:18 +0100

    docs (backend): improve docstring in ensure_event_loop function

--- agenta-backend/agenta_backend/utils/event_loop_utils.py
+++ agenta-backend/agenta_backend/utils/event_loop_utils.py
@@ -6,6 +6,9 @@ def ensure_event_loop() -> asyncio.AbstractEventLoop:
     Ensure that there is an event loop available in the current thread.
     If there isn't one, create a new event loop and set it.
 
+    Raises:
+        RuntimeError: There is no current event loop in thread 'AnyIO worker thread'.
+
     Returns:
         asyncio.AbstractEventLoop: The event loop for the current thread.
     """
