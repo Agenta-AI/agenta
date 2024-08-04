e2b069a7afe56e51f57c43388dff9cc57f72cfbe
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2024-08-04 11:47:07 +0100

    design(frontend): added query param key value to open evaluation modals on navigate

--- agenta-web/src/pages/apps/[app_id]/annotations/human_a_b_testing.tsx
+++ agenta-web/src/pages/apps/[app_id]/annotations/human_a_b_testing.tsx
@@ -1,17 +1,21 @@
 import HumanEvaluationResult from "@/components/Evaluations/HumanEvaluationResult"
 import HumanEvaluationModal from "@/components/HumanEvaluationModal/HumanEvaluationModal"
+import {useQueryParam} from "@/hooks/useQuery"
 import React, {useState} from "react"
 
 const HumanABTestingEvaluation = () => {
     const [isEvalModalOpen, setIsEvalModalOpen] = useState(false)
+    const [isQueryHumanEvalOpen, setIsQueryHumanEvalOpen] = useQueryParam("openHumanEvalModal")
+
     return (
         <>
             <HumanEvaluationResult setIsEvalModalOpen={setIsEvalModalOpen} />
 
             <HumanEvaluationModal
                 evaluationType={"human_a_b_testing"}
-                isEvalModalOpen={isEvalModalOpen}
+                isEvalModalOpen={isQueryHumanEvalOpen === "open" || isEvalModalOpen}
                 setIsEvalModalOpen={setIsEvalModalOpen}
+                setIsQueryHumanEvalOpen={setIsQueryHumanEvalOpen}
             />
         </>
     )
