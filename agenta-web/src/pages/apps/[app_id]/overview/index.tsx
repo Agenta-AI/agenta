95fe4bcfbe3d83b77bf7c20c53c5468863bb938a
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2024-08-04 11:54:57 +0100

    fix(frontend): dynamically imported Observability overview

--- agenta-web/src/pages/apps/[app_id]/overview/index.tsx
+++ agenta-web/src/pages/apps/[app_id]/overview/index.tsx
@@ -1,12 +1,14 @@
 import AbTestingEvalOverview from "@/components/pages/overview/abTestingEvaluation/AbTestingEvalOverview"
 import AutomaticEvalOverview from "@/components/pages/overview/automaticEvaluation/AutomaticEvalOverview"
 import DeploymentOverview from "@/components/pages/overview/deployments/DeploymentOverview"
-import ObservabilityOverview from "@/components/pages/overview/observability/ObservabilityOverview"
 import SingleModelEvalOverview from "@/components/pages/overview/singleModelEvaluation/SingleModelEvalOverview"
 import VariantsOverview from "@/components/pages/overview/variants/VariantsOverview"
+import {dynamicComponent} from "@/lib/helpers/dynamic"
 import {JSSTheme} from "@/lib/Types"
 import {Typography} from "antd"
 import {createUseStyles} from "react-jss"
+
+const ObservabilityOverview = dynamicComponent("pages/overview/observability/ObservabilityOverview")
 
 const {Title} = Typography
 
