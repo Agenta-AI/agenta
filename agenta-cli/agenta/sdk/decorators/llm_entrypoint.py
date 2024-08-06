8ba6aa1ab1b934d68b4c83981166581d90397737
Juan Pablo Vega <jp@agenta.ai>
2024-08-06 16:31:38 +0200

    fix(sdk) only keep trace_id in deployed endpoints

--- agenta-cli/agenta/sdk/decorators/llm_entrypoint.py
+++ agenta-cli/agenta/sdk/decorators/llm_entrypoint.py
@@ -141,7 +141,11 @@ class entrypoint(BaseDecorator):
             )
 
             entrypoint_result = await self.execute_function(
-                func, *args, params=func_params, config_params=config_params
+                func,
+                True,  # inline trace: True
+                *args,
+                params=func_params,
+                config_params=config_params,
             )
 
             return entrypoint_result
@@ -194,7 +198,11 @@ class entrypoint(BaseDecorator):
             )
 
             entrypoint_result = await self.execute_function(
-                func, *args, params=func_params, config_params=config_params
+                func,
+                False,  # inline trace: False
+                *args,
+                params=func_params,
+                config_params=config_params,
             )
 
             return entrypoint_result
@@ -274,7 +282,9 @@ class entrypoint(BaseDecorator):
             if name in func_params and func_params[name] is not None:
                 func_params[name] = self.ingest_file(func_params[name])
 
-    async def execute_function(self, func: Callable[..., Any], *args, **func_params):
+    async def execute_function(
+        self, func: Callable[..., Any], inline_trace, *args, **func_params
+    ):
         """Execute the function and handle any exceptions."""
 
         try:
@@ -311,6 +321,10 @@ class entrypoint(BaseDecorator):
                         remaining_steps -= 1
 
                 trace = ag.tracing.dump_trace()
+
+                if not inline_trace:
+                    trace = {"trace_id": trace["trace_id"]}
+
                 ag.tracing.flush_spans()
                 tracing_context.reset(token)
 
@@ -541,6 +555,7 @@ class entrypoint(BaseDecorator):
         result = loop.run_until_complete(
             self.execute_function(
                 func,
+                True,  # inline trace: True
                 **{"params": args_func_params, "config_params": args_config_params},
             )
         )
