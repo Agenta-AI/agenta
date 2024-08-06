d57ffffda737cfd9987f4ca536e0cc9a12e73ef9
aakrem <akrem@agenta.ai>
2024-08-06 13:01:04 +0200

    fix(tool): AGE-512 fix issues in postgres container

--- docker-assets/postgres/init-db.sql
+++ docker-assets/postgres/init-db.sql
@@ -1,1 +1,11 @@
-CREATE DATABASE agenta_oss;
+-- Create the username role with a password
+CREATE ROLE username WITH LOGIN PASSWORD 'password';
+
+-- Grant necessary permissions to username
+GRANT ALL PRIVILEGES ON DATABASE agenta_oss TO username;
+
+-- Connect to the agenta_oss database
+\c agenta_oss
+
+-- Grant schema permissions to username
+GRANT ALL ON SCHEMA public TO username;
