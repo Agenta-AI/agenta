69175281a6a02fe5819a1fe548872adb8e47142d
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2024-08-09 14:20:00 +0100

    fix(frontend): moved fetch variants request to parent level to avoid multiple request calls

--- agenta-web/src/pages/apps/[app_id]/overview/index.tsx
+++ agenta-web/src/pages/apps/[app_id]/overview/index.tsx
@@ -4,15 +4,20 @@ import DeploymentOverview from "@/components/pages/overview/deployments/Deployme
 import SingleModelEvalOverview from "@/components/pages/overview/singleModelEvaluation/SingleModelEvalOverview"
 import VariantsOverview from "@/components/pages/overview/variants/VariantsOverview"
 import {useAppsData} from "@/contexts/app.context"
+import {useAppId} from "@/hooks/useAppId"
 import {dynamicComponent} from "@/lib/helpers/dynamic"
 import {renameVariablesCapitalizeAll} from "@/lib/helpers/utils"
-import {JSSTheme} from "@/lib/Types"
+import {JSSTheme, Variant} from "@/lib/Types"
+import {fetchVariants} from "@/services/api"
 import {MoreOutlined} from "@ant-design/icons"
 import {PencilLine, Trash} from "@phosphor-icons/react"
 import {Button, Dropdown, Space, Typography} from "antd"
+import {useEffect, useState} from "react"
 import {createUseStyles} from "react-jss"
 
-const ObservabilityOverview = dynamicComponent("pages/overview/observability/ObservabilityOverview")
+const ObservabilityOverview: any = dynamicComponent(
+    "pages/overview/observability/ObservabilityOverview",
+)
 
 const {Title} = Typography
 
@@ -30,9 +35,28 @@ const useStyles = createUseStyles((theme: JSSTheme) => ({
 }))
 
 export default function Overview() {
+    const appId = useAppId()
     const classes = useStyles()
     const {currentApp} = useAppsData()
     const capitalizedAppName = renameVariablesCapitalizeAll(currentApp?.app_name || "")
+    const [variants, setVariants] = useState<Variant[]>([])
+    const [isVariantLoading, setIsVariantLoading] = useState(false)
+
+    useEffect(() => {
+        const fetchAllVariants = async () => {
+            try {
+                setIsVariantLoading(true)
+                const data = await fetchVariants(appId)
+                setVariants(data)
+            } catch (error) {
+                console.error(error)
+            } finally {
+                setIsVariantLoading(false)
+            }
+        }
+
+        fetchAllVariants()
+    }, [appId])
 
     return (
         <div className={classes.container}>
@@ -62,11 +86,11 @@ export default function Overview() {
                 </Dropdown>
             </Space>
 
-            <ObservabilityOverview />
+            <ObservabilityOverview variants={variants} />
 
-            <DeploymentOverview />
+            <DeploymentOverview variants={variants} />
 
-            <VariantsOverview />
+            <VariantsOverview variantList={variants} isVariantLoading={isVariantLoading} />
 
             <AutomaticEvalOverview />
 
