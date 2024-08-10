b0114355b37ba55d31cd1c4a0d96f5673fb2920c
Juan Pablo Vega <jp@agenta.ai>
2024-08-10 16:04:54 +0200

    feat(sdk): Add ignore_inputs/outputs to @instrument

--- agenta-cli/agenta/sdk/decorators/tracing.py
+++ agenta-cli/agenta/sdk/decorators/tracing.py
@@ -2,7 +2,7 @@
 import inspect
 import traceback
 from functools import wraps
-from typing import Any, Callable, Optional
+from typing import Any, Callable, Optional, List, Union
 
 # Own Imports
 import agenta as ag
@@ -40,9 +40,13 @@ class instrument(BaseDecorator):
         self,
         config: Optional[dict] = None,
         spankind: str = "workflow",
+        ignore_inputs: Union[List[str], bool] = False,
+        ignore_outputs: Union[List[str], bool] = False,
     ) -> None:
         self.config = config
         self.spankind = spankind
+        self.ignore_inputs = ignore_inputs
+        self.ignore_outputs = ignore_outputs
 
     def __call__(self, func: Callable[..., Any]):
         is_coroutine_function = inspect.iscoroutinefunction(func)
@@ -54,38 +58,52 @@ class instrument(BaseDecorator):
 
             return input_dict
 
+        def redact(io, blacklist):
+            return {
+                key: io[key]
+                for key in io.keys()
+                if key
+                not in (
+                    blacklist
+                    if isinstance(blacklist, list)
+                    else [] if blacklist is False else io.keys()
+                )
+            }
+
+        def patch(result):
+            TRACE_DEFAULT_KEY = "__default__"
+
+            outputs = result
+
+            # PATCH : if result is not a dict, make it a dict
+            if not isinstance(result, dict):
+                outputs = {TRACE_DEFAULT_KEY: result}
+            else:
+                # PATCH : if result is a legacy dict, clean it up
+                if (
+                    "message" in result.keys()
+                    and "cost" in result.keys()
+                    and "usage" in result.keys()
+                ):
+                    outputs = {TRACE_DEFAULT_KEY: result["message"]}
+
+                    ag.tracing.store_cost(result["cost"])
+                    ag.tracing.store_usage(result["usage"])
+
+            return outputs
+
         @wraps(func)
         async def async_wrapper(*args, **kwargs):
             async def wrapped_func(*args, **kwargs):
                 with ag.tracing.Context(
                     name=func.__name__,
-                    input=get_inputs(*args, **kwargs),
+                    input=redact(get_inputs(*args, **kwargs), self.ignore_inputs),
                     spankind=self.spankind,
                     config=self.config,
                 ):
                     result = await func(*args, **kwargs)
 
-                    TRACE_DEFAULT_KEY = "__default__"
-
-                    outputs = result
-
-                    # PATCH : if result is not a dict, make it a dict
-                    if not isinstance(result, dict):
-                        outputs = {TRACE_DEFAULT_KEY: result}
-                    else:
-                        # PATCH : if result is a legacy dict, clean it up
-                        if (
-                            "message" in result.keys()
-                            and "cost" in result.keys()
-                            and "usage" in result.keys()
-                        ):
-                            outputs = {TRACE_DEFAULT_KEY: result["message"]}
-
-                            ag.tracing.store_cost(result["cost"])
-                            ag.tracing.store_usage(result["usage"])
-                    # END OF PATH
-
-                    ag.tracing.store_outputs(outputs)
+                    ag.tracing.store_outputs(redact(patch(result), self.ignore_outputs))
 
                     return result
 
@@ -96,33 +114,13 @@ class instrument(BaseDecorator):
             def wrapped_func(*args, **kwargs):
                 with ag.tracing.Context(
                     name=func.__name__,
-                    input=get_inputs(*args, **kwargs),
+                    input=redact(get_inputs(*args, **kwargs), self.ignore_inputs),
                     spankind=self.spankind,
                     config=self.config,
                 ):
                     result = func(*args, **kwargs)
 
-                    TRACE_DEFAULT_KEY = "__default__"
-
-                    outputs = result
-
-                    # PATCH : if result is not a dict, make it a dict
-                    if not isinstance(result, dict):
-                        outputs = {TRACE_DEFAULT_KEY: result}
-                    else:
-                        # PATCH : if result is a legacy dict, clean it up
-                        if (
-                            "message" in result.keys()
-                            and "cost" in result.keys()
-                            and "usage" in result.keys()
-                        ):
-                            outputs = {"message": result["message"]}
-
-                            ag.tracing.store_cost(result["cost"])
-                            ag.tracing.store_usage(result["usage"])
-                    # END OF PATH
-
-                    ag.tracing.store_outputs(outputs)
+                    ag.tracing.store_outputs(redact(patch(result), self.ignore_outputs))
 
                     return result
 
