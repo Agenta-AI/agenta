5e6c47faab1115106f001961d41a99fc60ae8b5d
ashrafchowdury <ashrafdev365@gmail.com>
2024-08-11 20:53:57 +0600

    test: saved single model testset

--- agenta-web/cypress/e2e/single-model-test-evaluation.cy.ts
+++ agenta-web/cypress/e2e/single-model-test-evaluation.cy.ts
@@ -1,6 +1,9 @@
+import {randString} from "../../src/lib/helpers/utils"
+
 describe("Single Model Test workflow", () => {
     let app_id
     let testset_name
+    const saved_testset_name = randString(8)
     before(() => {
         cy.createVariantsAndTestsets()
         cy.get("@app_id").then((appId) => {
@@ -43,6 +46,19 @@ describe("Single Model Test workflow", () => {
 
             cy.wait("@generateRequest")
             cy.get('[data-cy="evaluation-vote-panel-numeric-vote-input"]').type("100")
+            cy.get('[data-cy="single-model-save-testset-button"]').click()
+            cy.get(".ant-modal-content").contains("Add new test set").should("be.visible")
+            cy.get('[data-cy="single-model-save-testset-modal-input"]').type(saved_testset_name)
+            cy.get(".ant-modal-footer > .ant-btn-primary").contains("Submit").click()
+            cy.wait(500)
+        })
+
+        it("Shloud check the evaluation testset is successfully saved", () => {
+            cy.visit(`/apps/${app_id}/testsets`)
+            cy.url().should("include", "/testsets")
+            cy.get('[data-cy="app-testset-list"]').as("table")
+            cy.get("@table").get(".ant-table-pagination li a").last().click()
+            cy.get("@table").contains(saved_testset_name).as("tempTestSet").should("be.visible")
         })
     })
 
