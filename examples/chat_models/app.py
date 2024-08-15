e11e59c4a61f65bab14b457eec59dbd1fe35e0fb
Abram <israelvictory87@gmail.com>
2024-08-15 20:17:02 +0100

    chore (app): replace deprecated models with gpt 4o and 4o-mini

--- examples/chat_models/app.py
+++ examples/chat_models/app.py
@@ -21,12 +21,11 @@ replicate_dict = {
 
 # ChatGpt 3.5 models
 CHAT_LLM_GPT = [
-    "gpt-3.5-turbo-16k-0613",
     "gpt-3.5-turbo-16k",
-    "gpt-3.5-turbo-0613",
-    "gpt-3.5-turbo-0301",
     "gpt-3.5-turbo",
     "gpt-4",
+    "gpt-4o",
+    "gpt-4o-mini",
 ]
 
 ag.config.default(
