179d78e547e2eb92737cdd0ba7fd3eeb1f4bc5ce
ashrafchowdury <ashrafdev365@gmail.com>
2024-08-19 16:52:00 +0600

    enhance: search bar functionalities

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -180,26 +180,9 @@ const config: Config = {
       ],
     } satisfies Preset.ThemeConfig,
     algolia: {
-      // The application ID provided by Algolia
-      appId: "OUSV9KGN15",
-      // Public API key: it is safe to commit it
-      apiKey: "87468b00a479e707410f7b2b05ef6f9b",
-      indexName: "YOUR_INDEX_NAME",
-      // Optional: see doc section below
-      contextualSearch: true,
-      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-      externalUrlRegex: "external\\.com|domain\\.com",
-      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-      replaceSearchResultPathname: {
-        from: "/docs/", // or as RegExp: /\/docs\//
-        to: "/",
-      },
-      // Optional: Algolia search parameters
-      searchParameters: {},
-      // Optional: path for search page that enabled by default (`false` to disable it)
-      searchPagePath: "search",
-      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
-      insights: false,
+      appId: "BEKE35D93N",
+      apiKey: "d760ee239785f6acd72ea26185681706",
+      indexName: "agenta-ai",
     },
   },
 
