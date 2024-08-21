916d2433c64ba05812c467e13ecf65a339a4c80a
Juan Pablo Vega <jp@agenta.ai>
2024-08-21 11:11:01 +0200

    clean up JSON checks

--- agenta-backend/agenta_backend/services/evaluators_service.py
+++ agenta-backend/agenta_backend/services/evaluators_service.py
@@ -620,18 +620,25 @@ def auto_json_diff(
     settings_values: Dict[str, Any],  # pylint: disable=unused-argument
     lm_providers_keys: Dict[str, Any],  # pylint: disable=unused-argument
 ) -> Result:
-    if not isinstance(output, str):
-        output = output.get("data", "")
-        if not isinstance(output, str):
-            return Result(
-                type="error",
-                value=None,
-                error=Error(
-                    message="JSON Diff expects a string output but was given something else."
-                ),
+    try:
+        output = output.get("data", "") if isinstance(output, dict) else output
+
+        if isinstance(output, dict):
+            output = json.dumps(output)
+        elif isinstance(output, str):
+            try:
+                json.loads(output)
+            except:
+                raise Exception(
+                    f"Evaluator 'auto_json_diff' requires string outputs to be JSON strings."
+                )
+        else:
+            raise Exception(
+                f"Evaluator 'auto_json_diff' requires the output to be either a JSON string or a JSON object, but received {type(output).__name__} instead."
             )
 
-    try:
+        print(output)
+
         correct_answer = get_correct_answer(data_point, settings_values)
         average_score = compare_jsons(
             ground_truth=json.loads(correct_answer),
