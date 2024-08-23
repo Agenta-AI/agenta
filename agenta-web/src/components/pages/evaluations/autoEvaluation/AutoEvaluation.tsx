bd747a4116a7403a4357ccc46e8c356a6f697a0d
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2024-08-23 13:31:14 +0100

    initial commit: setup configure evaluator modal

---
+++ agenta-web/src/components/pages/evaluations/autoEvaluation/AutoEvaluation.tsx
@@ -0,0 +1,241 @@
+import {_Evaluation, JSSTheme} from "@/lib/Types"
+import {
+    ArrowsLeftRight,
+    Columns,
+    Database,
+    Gauge,
+    GearSix,
+    Note,
+    Plus,
+    Rocket,
+    Trash,
+} from "@phosphor-icons/react"
+import {Button, Dropdown, Space, Spin, Table} from "antd"
+import React, {useState} from "react"
+import {createUseStyles} from "react-jss"
+import ConfigureEvaluatorModal from "./evaluators/ConfigureEvaluatorModal"
+import {ColumnsType} from "antd/es/table"
+import {MoreOutlined} from "@ant-design/icons"
+
+const useStyles = createUseStyles((theme: JSSTheme) => ({
+    button: {
+        display: "flex",
+        alignItems: "center",
+    },
+}))
+
+const AutoEvaluation = () => {
+    const classes = useStyles()
+    const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([])
+    const [isConfigEvaluatorModalOpen, setIsConfigEvaluatorModalOpen] = useState(false)
+
+    const columns: ColumnsType<_Evaluation> = [
+        {
+            title: "Variant",
+            dataIndex: "variants",
+            key: "variants",
+            fixed: "left",
+            onHeaderCell: () => ({
+                style: {minWidth: 160},
+            }),
+        },
+        {
+            title: "Test set",
+            dataIndex: "testsetName",
+            key: "testsetName",
+            onHeaderCell: () => ({
+                style: {minWidth: 160},
+            }),
+        },
+        {
+            title: "Status",
+            dataIndex: "status",
+            key: "status",
+            onHeaderCell: () => ({
+                style: {minWidth: 240},
+            }),
+        },
+        {
+            title: "Results",
+            children: [
+                {
+                    title: "Evaluator 1",
+                    dataIndex: "aggregated_results",
+                    key: "results",
+                    onHeaderCell: () => ({
+                        style: {minWidth: 240},
+                    }),
+                },
+                {
+                    title: "Evaluator 2",
+                    dataIndex: "aggregated_results",
+                    key: "results",
+                    onHeaderCell: () => ({
+                        style: {minWidth: 240},
+                    }),
+                },
+                {
+                    title: "Evaluator 3",
+                    dataIndex: "aggregated_results",
+                    key: "results",
+                    onHeaderCell: () => ({
+                        style: {minWidth: 240},
+                    }),
+                },
+            ],
+        },
+        {
+            title: "Created on",
+            dataIndex: "created_at",
+            key: "createdAt",
+            onHeaderCell: () => ({
+                style: {minWidth: 160},
+            }),
+        },
+        {
+            title: "Avg. Latency",
+            dataIndex: "average_latency",
+            key: "average_latency",
+            onHeaderCell: () => ({
+                style: {minWidth: 160},
+            }),
+        },
+        {
+            title: "Total Cost",
+            dataIndex: "average_cost",
+            key: "average_cost",
+            onHeaderCell: () => ({
+                style: {minWidth: 160},
+            }),
+        },
+        {
+            title: <GearSix size={16} />,
+            key: "key",
+            width: 56,
+            fixed: "right",
+            align: "center",
+            render: (_, record) => {
+                return (
+                    <Dropdown
+                        trigger={["click"]}
+                        overlayStyle={{width: 180}}
+                        menu={{
+                            items: [
+                                {
+                                    key: "details",
+                                    label: "Open details",
+                                    icon: <Note size={16} />,
+                                    onClick: (e) => {
+                                        e.domEvent.stopPropagation()
+                                    },
+                                },
+                                {
+                                    key: "variant",
+                                    label: "View variant",
+                                    icon: <Rocket size={16} />,
+                                    onClick: (e) => {
+                                        e.domEvent.stopPropagation()
+                                    },
+                                },
+                                {
+                                    key: "view_testset",
+                                    label: "View test set",
+                                    icon: <Database size={16} />,
+                                    onClick: (e) => {
+                                        e.domEvent.stopPropagation()
+                                    },
+                                },
+                                {type: "divider"},
+                                {
+                                    key: "delete_eval",
+                                    label: "Delete",
+                                    icon: <Trash size={16} />,
+                                    danger: true,
+                                    onClick: (e) => {
+                                        e.domEvent.stopPropagation()
+                                    },
+                                },
+                            ],
+                        }}
+                    >
+                        <Button
+                            onClick={(e) => e.stopPropagation()}
+                            type="text"
+                            icon={<MoreOutlined />}
+                            size="small"
+                        />
+                    </Dropdown>
+                )
+            },
+        },
+    ]
+
+    return (
+        <div className="flex flex-col gap-2">
+            <div className="flex items-center justify-between">
+                <Space>
+                    <Button type="primary" icon={<Plus size={14} />} className={classes.button}>
+                        Start new evaluation
+                    </Button>
+                    <Button
+                        icon={<Gauge size={14} />}
+                        className={classes.button}
+                        onClick={() => setIsConfigEvaluatorModalOpen(true)}
+                    >
+                        Configure evaluators
+                    </Button>
+                </Space>
+                <Space>
+                    <Button
+                        danger
+                        type="text"
+                        icon={<Trash size={14} />}
+                        className={classes.button}
+                    >
+                        Delete
+                    </Button>
+                    <Button
+                        type="text"
+                        icon={<ArrowsLeftRight size={14} />}
+                        className={classes.button}
+                    >
+                        Compare
+                    </Button>
+                    <Button icon={<Columns size={14} />} className={classes.button}>
+                        Edit columns
+                    </Button>
+                </Space>
+            </div>
+
+            <Spin spinning={false}>
+                <Table
+                    rowSelection={{
+                        type: "checkbox",
+                        columnWidth: 48,
+                        onChange: (selectedRowKeys: React.Key[]) => {
+                            setSelectedRowKeys(selectedRowKeys)
+                        },
+                    }}
+                    className="ph-no-capture"
+                    columns={columns}
+                    rowKey={"id"}
+                    dataSource={[]}
+                    scroll={{x: true}}
+                    bordered
+                    pagination={false}
+                    onRow={(record) => ({
+                        style: {cursor: "pointer"},
+                        onClick: () => {},
+                    })}
+                />
+            </Spin>
+
+            <ConfigureEvaluatorModal
+                open={isConfigEvaluatorModalOpen}
+                onCancel={() => setIsConfigEvaluatorModalOpen(false)}
+            />
+        </div>
+    )
+}
+
+export default AutoEvaluation
---
+++ agenta-web/src/components/pages/evaluations/autoEvaluation/evaluators/EvaluatorCard.tsx
@@ -0,0 +1,148 @@
+import {EvaluatorConfig, JSSTheme} from "@/lib/Types"
+import {MoreOutlined} from "@ant-design/icons"
+import {Copy, Note, Trash} from "@phosphor-icons/react"
+import {Button, Card, Dropdown, Tag, Typography} from "antd"
+import React from "react"
+import {createUseStyles} from "react-jss"
+
+interface EvaluatorCardProps {
+    evaluatorConfigs: EvaluatorConfig[]
+}
+
+const useStyles = createUseStyles((theme: JSSTheme) => ({
+    container: {
+        display: "flex",
+        flexDirection: "column",
+        gap: theme.paddingLG,
+    },
+    cardTitle: {
+        fontSize: theme.fontSizeLG,
+        lineHeight: theme.lineHeightLG,
+        fontWeight: theme.fontWeightMedium,
+    },
+    evaluatorCard: {
+        width: 276,
+        display: "flex",
+        flexDirection: "column",
+        transition: "all 0.025s ease-in",
+        cursor: "pointer",
+        "& > .ant-card-head": {
+            minHeight: 0,
+            padding: theme.paddingSM,
+
+            "& .ant-card-head-title": {
+                fontSize: theme.fontSize,
+                fontWeight: theme.fontWeightMedium,
+                lineHeight: theme.lineHeight,
+            },
+        },
+        "& > .ant-card-body": {
+            padding: theme.paddingSM,
+            display: "flex",
+            flexDirection: "column",
+            gap: theme.marginXS,
+            "& div": {
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+            },
+        },
+        "&:hover": {},
+    },
+}))
+
+const EvaluatorCard = ({evaluatorConfigs}: EvaluatorCardProps) => {
+    const classes = useStyles()
+
+    const formatEvluatorConfigs = Object.entries(
+        evaluatorConfigs.reduce(
+            (acc, curr) => {
+                if (!acc[curr.evaluator_key]) {
+                    acc[curr.evaluator_key] = []
+                }
+                acc[curr.evaluator_key].push(curr)
+                return acc
+            },
+            {} as Record<string, EvaluatorConfig[]>,
+        ),
+    ).map(([title, items]) => ({
+        title,
+        items,
+    }))
+
+    return (
+        <div className={classes.container}>
+            {formatEvluatorConfigs.map(({title, items}) => (
+                <div className="flex flex-col gap-2" key={title}>
+                    <Typography.Text className={classes.cardTitle}>{title}</Typography.Text>
+                    <div className="flex gap-4">
+                        {items.map((item) => (
+                            <Card
+                                key={item.id}
+                                className={classes.evaluatorCard}
+                                title={item.name}
+                                extra={
+                                    <Dropdown
+                                        trigger={["hover"]}
+                                        placement="bottomRight"
+                                        overlayStyle={{width: 180}}
+                                        menu={{
+                                            items: [
+                                                {
+                                                    key: "view_config",
+                                                    label: "View configuration",
+                                                    icon: <Note size={16} />,
+                                                    onClick: (e: any) => {
+                                                        e.domEvent.stopPropagation()
+                                                    },
+                                                },
+                                                {
+                                                    key: "clone",
+                                                    label: "Clone",
+                                                    icon: <Copy size={16} />,
+                                                    onClick: (e: any) => {
+                                                        e.domEvent.stopPropagation()
+                                                    },
+                                                },
+                                                {type: "divider"},
+                                                {
+                                                    key: "delete_app",
+                                                    label: "Delete",
+                                                    icon: <Trash size={16} />,
+                                                    danger: true,
+                                                    onClick: (e: any) => {
+                                                        e.domEvent.stopPropagation()
+                                                    },
+                                                },
+                                            ],
+                                        }}
+                                    >
+                                        <Button
+                                            type="text"
+                                            onClick={(e) => e.stopPropagation()}
+                                            icon={<MoreOutlined />}
+                                            size="small"
+                                        />
+                                    </Dropdown>
+                                }
+                            >
+                                <div>
+                                    <Typography.Text>Type</Typography.Text>
+                                    <Tag color="cyan" className="mr-0">
+                                        cyan
+                                    </Tag>
+                                </div>
+                                <div>
+                                    <Typography.Text>Version</Typography.Text>
+                                    <Typography.Text type="secondary">v1.1</Typography.Text>
+                                </div>
+                            </Card>
+                        ))}
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default EvaluatorCard
