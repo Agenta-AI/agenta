4382e109a418eed33f20412b473654bf457adcc6
Abram <israelvictory87@gmail.com>
2024-09-04 15:00:31 +0100

    refactor (backend): update custom data migration to create and remove default projects and hook it into Alembic migration

--- agenta-backend/agenta_backend/migrations/postgres/data_migrations/projects.py
+++ agenta-backend/agenta_backend/migrations/postgres/data_migrations/projects.py
@@ -1,143 +1,67 @@
 import os
 import traceback
 
-import click
-from slugify import slugify
 
+import click
 from sqlalchemy.future import select
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import MultipleResultsFound
 
-from agenta_backend.models.db_models import AppDB, ProjectDB
+from agenta_backend.models.db_models import ProjectDB
 
 
-def assign_default_project():
-    # Define the project name that was used as default
+def get_default_projects(session):
+    query = session.execute(select(ProjectDB).filter_by(is_default=True))
+    return query.scalars().all()
+
+
+def create_default_project():
     PROJECT_NAME = "Default"
-
-    # Create engine for db connection
-    engine = create_engine(url=os.environ["POSTGRES_URI"])
-
-    # Create a session factory
+    engine = create_engine(os.getenv("POSTGRES_URI"))
     sync_session = sessionmaker(engine, expire_on_commit=False)
 
     with sync_session() as session:
         try:
-            # Retrieve the default project
-            query = session.execute(
-                select(ProjectDB).filter_by(slug_name=slugify(PROJECT_NAME))
-            )
-            default_projects = query.scalars().all()
-
-            # Ensure that multiple projects do not exist, otherwise raise an exception
-            if len(default_projects) > 1:
-                ERROR_MESSAGE = (
-                    f"Error: Multiple default projects found. Only one default project is allowed. "
-                    f"Please ensure there is only one project with the slug name '{slugify(PROJECT_NAME)}' and try again."
+            default_projects = get_default_projects(session)
+            if len(default_projects) >= 1:
+                raise ValueError(
+                    "Multiple default projects found. Please ensure only one exists."
                 )
-                click.echo(click.style(ERROR_MESSAGE, fg="red"), color=True)
-                raise MultipleResultsFound(ERROR_MESSAGE)
 
-            # If no default project exists, create one
-            if not default_projects:
-                new_project = ProjectDB(
-                    project_name=PROJECT_NAME, slug_name=slugify(PROJECT_NAME)
-                )
+            if len(default_projects) == 0:
+                new_project = ProjectDB(project_name=PROJECT_NAME, is_default=True)
                 session.add(new_project)
                 session.commit()
-                default_project = new_project
-            else:
-                default_project = default_projects[0]
-
-            # Fetch all apps with project_id as None
-            apps_query = session.execute(
-                select(AppDB).filter(AppDB.project_id.is_(None))
-            )
-            apps = apps_query.scalars().all()
-
-            # Update each app individually
-            for app in apps:
-                app.project_id = default_project.id
-            session.commit()
 
         except Exception as e:
-            # Handle exceptions and rollback if necessary
             session.rollback()
-            click.echo(
-                click.style(
-                    f"\nAn ERROR occurred while assigning the default project to existing apps: {traceback.format_exc()}",
-                    fg="red",
-                ),
-                color=True,
-            )
+            click.echo(click.style(f"ERROR: {traceback.format_exc()}", fg="red"))
             raise e
 
 
-def revert_default_project():
-    # Define the project name that was used as default
-    PROJECT_NAME = "Default"
-
-    # Create engine for db connection
-    engine = create_engine(url=os.environ["POSTGRES_URI"])
-
-    # Create a session factory
+def remove_default_project():
+    engine = create_engine(os.getenv("POSTGRES_URI"))
     sync_session = sessionmaker(engine, expire_on_commit=False)
 
     with sync_session() as session:
         try:
-            # Retrieve the default project
-            query = session.execute(
-                select(ProjectDB).filter_by(slug_name=slugify(PROJECT_NAME))
-            )
-            default_projects = query.scalars().all()
-
-            # If no default project is found, nothing to revert
-            if not default_projects:
+            default_projects = get_default_projects(session)
+            if len(default_projects) == 0:
                 click.echo(
-                    click.style("No default project found to revert.", fg="yellow"),
-                    color=True,
+                    click.style("No default project found to remove.", fg="yellow")
                 )
                 return
 
-            # Ensure that only one default project exists
             if len(default_projects) > 1:
-                ERROR_MESSAGE = (
-                    f"Error: Multiple default projects found. Only one default project is allowed. "
-                    f"Please ensure there is only one project with the slug name '{slugify(PROJECT_NAME)}' and try again."
+                raise ValueError(
+                    "Multiple default projects found. Please ensure only one exists."
                 )
-                click.echo(click.style(ERROR_MESSAGE, fg="red"), color=True)
-                raise MultipleResultsFound(ERROR_MESSAGE)
 
-            # Get the default project
-            default_project = default_projects[0]
-
-            # Set all apps' project_id to None
-            apps_query = session.execute(
-                select(AppDB).filter(AppDB.project_id.is_(None))
-            )
-            apps = apps_query.scalars().all()
-
-            # Update each app individually
-            for app in apps:
-                app.project_id = None
-
-            # Delete the default project
-            session.delete(default_project)
+            session.delete(default_projects[0])
             session.commit()
-
-            click.echo(
-                click.style("Reverted changes successfully.", fg="green"), color=True
-            )
+            click.echo(click.style("Default project removed successfully.", fg="green"))
 
         except Exception as e:
-            # Handle exceptions and rollback if necessary
             session.rollback()
-            click.echo(
-                click.style(
-                    f"\nAn ERROR occurred while reverting the default project changes: {traceback.format_exc()}",
-                    fg="red",
-                ),
-                color=True,
-            )
+            click.echo(click.style(f"ERROR: {traceback.format_exc()}", fg="red"))
             raise e
