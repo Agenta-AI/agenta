3adcba6e06db60647b3b844ae33e5e5617a81c40
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2024-09-04 15:32:13 +0100

    fix(frontend): transform trace tree and setup mapping

--- agenta-web/src/lib/transformers.ts
+++ agenta-web/src/lib/transformers.ts
@@ -238,3 +238,31 @@ export const transformTraceTreeToJson = (tree: TraceSpan[]) => {
 
     return nodeMap
 }
+
+export const generatePaths = (obj: Record<string, any>, currentPath = "") => {
+    let paths: {value: string}[] = []
+
+    if (typeof obj === "object" && obj !== null && !Array.isArray(obj)) {
+        Object.entries(obj).forEach(([key, value]) => {
+            const newPath = currentPath ? `${currentPath}.${key}` : key
+            if (value && typeof value === "object" && Object.keys(value).length) {
+                paths.push({value: newPath})
+                paths = paths.concat(generatePaths(value, newPath))
+            } else if (value && typeof value !== "object") {
+                paths.push({value: newPath})
+            }
+        })
+    } else if (Array.isArray(obj)) {
+        obj.forEach((value, index) => {
+            const newPath = `${currentPath}[${index}]`
+            if (value && typeof value === "object" && Object.keys(value).length) {
+                paths.push({value: newPath})
+                paths = paths.concat(generatePaths(value, newPath))
+            } else if (value && typeof value !== "object") {
+                paths.push({value: newPath})
+            }
+        })
+    }
+
+    return paths
+}
