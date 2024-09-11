fe0697754de182d8a94d5a184ae0871ff51aeada
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2024-09-11 19:09:11 +0100

    fix(frontend): fixed cypress test

--- agenta-web/cypress/e2e/app-navigation.cy.ts
+++ agenta-web/cypress/e2e/app-navigation.cy.ts
@@ -34,10 +34,10 @@ describe("App Navigation without errors", () => {
         cy.contains(/evaluations/i)
 
         cy.get(".ant-tabs-tab").eq(1).click()
-        cy.url().should("include", "/evaluations?selectedEvaluation=ab_testing_evaluation")
+        cy.url().should("include", "/evaluations?selectedEvaluation=human_annotation")
 
         cy.get(".ant-tabs-tab").eq(2).click()
-        cy.url().should("include", "/evaluations?selectedEvaluation=single_model_evaluation")
+        cy.url().should("include", "/evaluations?selectedEvaluation=human_ab_testing")
 
         cy.get(".ant-tabs-tab").eq(0).click()
         cy.url().should("include", "/evaluations?selectedEvaluation=auto_evaluation")
