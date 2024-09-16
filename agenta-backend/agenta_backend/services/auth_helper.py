d2fb567356d35e01cdb9ed7889579128a18d274b
Abram <israelvictory87@gmail.com>
2024-09-16 17:10:56 +0100

    refactor (backend): improve authentication middleware to: - set project_id from request (if it exists) to request.state - otherwise, set the default project id to request.state - improve try-except-finally to set default user_id

--- agenta-backend/agenta_backend/services/auth_helper.py
+++ agenta-backend/agenta_backend/services/auth_helper.py
@@ -1,6 +1,13 @@
+import logging
+
 from fastapi import Request, HTTPException
 
+from agenta_backend.utils.project_utils import retrieve_project_id_from_request
 from agenta_backend.services.db_manager import fetch_default_project, NoResultFound
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 class SessionContainer(object):
@@ -21,20 +28,29 @@ def verify_session():
 
 async def authentication_middleware(request: Request, call_next):
     try:
-        if not hasattr(request.state, "user_id"):
-            user_uid_id = "0"
-            setattr(request.state, "user_id", user_uid_id)
+        # Retrieve project_id from request
+        project_id_from_request = await retrieve_project_id_from_request(
+            request=request
+        )
 
-        if not hasattr(request.state, "project_id"):
-            project = await fetch_default_project()
+        # Set project_id if found or fetch default
+        if project_id_from_request and not hasattr(request.state, "project_id"):
+            setattr(request.state, "project_id", project_id_from_request)
+        elif not project_id_from_request:
+            project = await fetch_default_project()  # Fetch the default project
             if project is None:
                 raise NoResultFound("Default project not found.")
-
             setattr(request.state, "project_id", str(project.id))
 
         # Call the next middleware or route handler
         response = await call_next(request)
         return response
     except Exception as e:
+        # Handle exceptions, set status code
         status_code = e.status_code if hasattr(e, "status_code") else 500
         raise HTTPException(status_code=status_code, detail=str(e))
+    finally:
+        # Set default user_id if not already set
+        if not hasattr(request.state, "user_id"):
+            logger.warning("user_id not found in request.state, setting to default '0'")
+            setattr(request.state, "user_id", "0")
