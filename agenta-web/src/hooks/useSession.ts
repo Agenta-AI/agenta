edb51f08ddbd84131c215a4543a4f52829413b30
ashrafchowdury <ashrafdev365@gmail.com>
2024-09-22 20:46:11 +0600

    fix(frontend): api key missing message issue

--- agenta-web/src/hooks/useSession.ts
+++ agenta-web/src/hooks/useSession.ts
@@ -3,7 +3,7 @@ import {isDemo} from "@/lib/helpers/utils"
 import {useRouter} from "next/router"
 import posthog from "posthog-js"
 import {useSessionContext} from "supertokens-auth-react/recipe/session"
-import {signOut} from "supertokens-auth-react/recipe/passwordless"
+import {signOut} from "supertokens-auth-react/recipe/session"
 
 export const useSession: () => {loading: boolean; doesSessionExist: boolean; logout: () => void} =
     isDemo()
@@ -20,7 +20,7 @@ export const useSession: () => {loading: boolean; doesSessionExist: boolean; log
                           .then(() => {
                               posthog.reset()
                               reset()
-                              router.push("/auth")
+                              router.push("/auth").then(() => router.reload())
                           })
                           .catch(console.error)
                   },
