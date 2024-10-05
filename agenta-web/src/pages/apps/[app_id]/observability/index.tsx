24a4f5421f6df4b26beb950bfb47066dc23c20d4
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2024-10-05 23:34:33 +0200

    fix(frontend): replaced Observability specific drawer with generic drawer component

--- agenta-web/src/pages/apps/[app_id]/observability/index.tsx
+++ agenta-web/src/pages/apps/[app_id]/observability/index.tsx
@@ -1,7 +1,10 @@
-import TraceDrawer from "@/components/pages/observability/drawer"
+import GenericDrawer from "@/components/GenericDrawer"
+import TraceHeader from "@/components/pages/observability/drawer/TraceHeader"
 import {useQueryParam} from "@/hooks/useQuery"
 import {JSSTheme} from "@/lib/Types"
-import {Button, Typography} from "antd"
+import {DeleteOutlined} from "@ant-design/icons"
+import {CaretDown, CaretUp} from "@phosphor-icons/react"
+import {Button, Space, Tag, Typography} from "antd"
 import React from "react"
 import {createUseStyles} from "react-jss"
 
@@ -28,7 +31,12 @@ const ObservabilityDashboard: React.FC<Props> = () => {
                 Open drawer
             </Button>
 
-            <TraceDrawer open={!!selectedTraceId} onClose={() => setSelectedTraceId("")} />
+            <GenericDrawer
+                open={!!selectedTraceId}
+                onClose={() => setSelectedTraceId("")}
+                expandable
+                headerExtra={<TraceHeader />}
+            />
         </div>
     )
 }
