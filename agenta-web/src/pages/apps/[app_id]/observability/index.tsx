bb737a3888a89d2216f25c8bbaaa45cf54fb5025
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2024-10-09 19:28:44 +0200

    feat(frontend): propagated Trace header with mock data and implement next and prev button func

--- agenta-web/src/pages/apps/[app_id]/observability/index.tsx
+++ agenta-web/src/pages/apps/[app_id]/observability/index.tsx
@@ -3,9 +3,12 @@ import TraceContent from "@/components/pages/observability/drawer/TraceContent"
 import TraceHeader from "@/components/pages/observability/drawer/TraceHeader"
 import TraceTree from "@/components/pages/observability/drawer/TraceTree"
 import {useQueryParam} from "@/hooks/useQuery"
+import {useTraces} from "@/lib/hooks/useTraces"
 import {JSSTheme} from "@/lib/Types"
-import {Button, Typography} from "antd"
-import React from "react"
+import {AgentaRootsDTO} from "@/services/observability/types"
+import {Table, Typography} from "antd"
+import {ColumnsType} from "antd/es/table"
+import React, {useMemo} from "react"
 import {createUseStyles} from "react-jss"
 
 const useStyles = createUseStyles((theme: JSSTheme) => ({
@@ -21,24 +24,60 @@ interface Props {}
 const ObservabilityDashboard: React.FC<Props> = () => {
     const classes = useStyles()
     const [selectedTraceId, setSelectedTraceId] = useQueryParam("trace", "")
+    const {traces} = useTraces()
+
+    const activeTrace = useMemo(() => {
+        return traces?.find((item) => item.root.id === selectedTraceId)
+    }, [selectedTraceId, traces])
+
+    const columns: ColumnsType<AgentaRootsDTO> = [
+        {
+            title: "Trace Id",
+            dataIndex: "key",
+            key: "key",
+            width: 200,
+            render: (_, record) => {
+                return <div>{record.root.id}</div>
+            },
+        },
+    ]
 
     return (
         <div className="flex flex-col gap-6">
             <Typography.Text className={classes.title}>Observability</Typography.Text>
 
             <div>Observability Table</div>
-            <Button onClick={() => setSelectedTraceId("12345")} className="w-fit">
-                Open drawer
-            </Button>
+            {traces && (
+                <Table
+                    columns={columns}
+                    dataSource={traces}
+                    bordered
+                    style={{cursor: "pointer"}}
+                    onRow={(record) => ({
+                        onClick: () => {
+                            setSelectedTraceId(record.root.id)
+                        },
+                    })}
+                />
+            )}
 
-            <GenericDrawer
-                open={!!selectedTraceId}
-                onClose={() => setSelectedTraceId("")}
-                expandable
-                headerExtra={<TraceHeader />}
-                mainContent={<TraceContent />}
-                sideContent={<TraceTree />}
-            />
+            {activeTrace && traces?.length && (
+                <GenericDrawer
+                    open={!!selectedTraceId}
+                    onClose={() => setSelectedTraceId("")}
+                    expandable
+                    headerExtra={
+                        <TraceHeader
+                            activeTrace={activeTrace}
+                            selectedTraceId={selectedTraceId}
+                            traces={traces}
+                            setSelectedTraceId={setSelectedTraceId}
+                        />
+                    }
+                    mainContent={<TraceContent />}
+                    sideContent={<TraceTree />}
+                />
+            )}
         </div>
     )
 }
