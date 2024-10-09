01c2291cf2dcdd608da2c572946d661b869749bb
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2024-10-09 19:28:44 +0200

    feat(frontend): propagated Trace header with mock data and implement next and prev button func

--- agenta-web/src/components/pages/observability/drawer/TraceHeader.tsx
+++ agenta-web/src/components/pages/observability/drawer/TraceHeader.tsx
@@ -1,9 +1,17 @@
 import {JSSTheme} from "@/lib/Types"
+import {AgentaRootsDTO} from "@/services/observability/types"
 import {DeleteOutlined} from "@ant-design/icons"
 import {CaretDown, CaretUp} from "@phosphor-icons/react"
 import {Button, Space, Tag, Typography} from "antd"
-import React from "react"
+import React, {useMemo} from "react"
 import {createUseStyles} from "react-jss"
+
+interface TraceHeaderProps {
+    activeTrace: AgentaRootsDTO
+    selectedTraceId: string
+    traces: AgentaRootsDTO[]
+    setSelectedTraceId: (val: string) => void
+}
 
 const useStyles = createUseStyles((theme: JSSTheme) => ({
     title: {
@@ -13,20 +21,53 @@ const useStyles = createUseStyles((theme: JSSTheme) => ({
     },
 }))
 
-const TraceHeader = () => {
+const TraceHeader = ({
+    activeTrace,
+    selectedTraceId,
+    traces,
+    setSelectedTraceId,
+}: TraceHeaderProps) => {
     const classes = useStyles()
+
+    const activeTraceIndex = useMemo(() => {
+        return traces?.findIndex((item) => item.root.id === selectedTraceId)
+    }, [selectedTraceId, traces])
+
+    const handleNextTrace = () => {
+        if (traces && activeTraceIndex !== undefined && activeTraceIndex < traces.length - 1) {
+            setSelectedTraceId(traces[activeTraceIndex + 1].root.id)
+        }
+    }
+
+    const handlePrevTrace = () => {
+        if (traces && activeTraceIndex !== undefined && activeTraceIndex > 0) {
+            setSelectedTraceId(traces[activeTraceIndex - 1].root.id)
+        }
+    }
 
     return (
         <div className="flex items-center justify-between">
             <Space>
                 <div>
-                    <Button onClick={() => {}} type="text" icon={<CaretUp size={16} />} />
-                    <Button onClick={() => {}} type="text" icon={<CaretDown size={16} />} />
+                    <Button
+                        onClick={() => handlePrevTrace()}
+                        type="text"
+                        disabled={activeTraceIndex !== undefined && activeTraceIndex <= 0}
+                        icon={<CaretUp size={16} />}
+                    />
+                    <Button
+                        onClick={() => handleNextTrace()}
+                        type="text"
+                        disabled={
+                            activeTraceIndex !== undefined && activeTraceIndex >= traces.length - 1
+                        }
+                        icon={<CaretDown size={16} />}
+                    />
                 </div>
 
                 <Typography.Text className={classes.title}>Trace</Typography.Text>
 
-                <Tag className="font-normal"># 66c5b76d6165af12bab569e9</Tag>
+                <Tag className="font-normal"># {activeTrace.root.id}</Tag>
             </Space>
 
             <Button icon={<DeleteOutlined />} />
