7b9722f07eb7978a3659ccb3fbc16bd371866db8
Juan Pablo Vega <jp@agenta.ai>
2024-10-17 18:43:07 +0200

    deploy_prompt_by_env_ref()

--- agenta-backend/agenta_backend/services/prompts_manager.py
+++ agenta-backend/agenta_backend/services/prompts_manager.py
@@ -17,6 +17,7 @@ from agenta_backend.services.db_manager import (
     fetch_app_environment_revision_by_app_variant_revision_id,
     create_new_app_variant,
     update_variant_parameters,
+    deploy_to_environment,
 )
 
 from agenta_backend.utils.common import isEE, isCloudProd, isCloudDev, isCloudEE, isOss
@@ -212,7 +213,6 @@ async def fork_prompt_by_prompt_ref(
     project_id: str,
     user_id: str,
     prompt_ref: ReferenceDTO,
-    config_params: Optional[Dict[str, Any]] = None,
 ) -> Optional[PromptDTO]:
     app_variant_revision = None
     if prompt_ref.commit_id:
@@ -287,7 +287,7 @@ async def fork_prompt_by_prompt_ref(
         base=variant_base,
         config={
             "config_name": app_variant_revision.config_name,
-            "parameters": config_params or app_variant_revision.config_parameters,
+            "parameters": app_variant_revision.config_parameters,
         },
         base_name=app_variant_revision.base_name,
     )
@@ -313,7 +313,6 @@ async def fork_prompt_by_env_ref(
     project_id: str,
     user_id: str,
     env_ref: ReferenceDTO,
-    config_params: Optional[Dict[str, Any]] = None,
 ) -> Optional[PromptDTO]:
     app_environment_revision = None
     if env_ref.commit_id:
@@ -396,7 +395,7 @@ async def fork_prompt_by_env_ref(
         base=variant_base,
         config={
             "config_name": app_variant_revision.config_name,
-            "parameters": config_params or app_variant_revision.config_parameters,
+            "parameters": app_variant_revision.config_parameters,
         },
         base_name=app_variant_revision.base_name,
     )
@@ -422,7 +421,6 @@ async def fork_prompt_by_app_id_and_env_name(
     project_id: str,
     user_id: str,
     app_id: str,
-    config_params: Dict[str, Any] = None,
 ) -> Optional[PromptDTO]:
     raise NotImplementedError()
 
@@ -491,28 +489,96 @@ async def commit_prompt(
 # - DEPLOY
 
 
-async def deploy_prompt_by_prompt_ref(
-    project_id: str,
-    prompt_ref: ReferenceDTO,
-) -> Optional[PromptDTO]:
-    prompt = PromptDTO()
-
-    return prompt
-
-
-async def deploy_prompt_by_prompt(
-    project_id: str,
-    prompt: PromptDTO,
-) -> Optional[PromptDTO]:
-    prompt = PromptDTO()
-
-    return prompt
-
-
 async def deploy_prompt_by_env_ref(
     project_id: str,
+    user_id: str,
+    prompt_ref: ReferenceDTO,
     env_ref: ReferenceDTO,
 ) -> Optional[PromptDTO]:
-    prompt = PromptDTO()
+    app_environment_revision = None
+    if env_ref.commit_id:
+        app_environment_revision = await fetch_app_environment_revision(
+            # project_id=project_id,
+            revision_id=env_ref.commit_id,
+        )
+    elif env_ref.id and env_ref.version:
+        app_environment_revision = await fetch_app_environment_revision_by_environment(
+            # project_id=project_id,
+            environment_id=env_ref.id,
+            revision=env_ref.version,
+        )
+
+    if not app_environment_revision:
+        return None
+
+    app_environment = await fetch_app_environment_by_id(
+        # project_id=project_id,
+        environment_id=app_environment_revision.environment_id,
+    )
+
+    if not app_environment:
+        return None
+
+    await deploy_to_environment(
+        # project_id=project_id,
+        environment_name=app_environment.name,
+        variant_id=prompt_ref.id,
+        user_org_data={"user_uid": user_id},
+    )
+
+    app_variant = await fetch_app_variant_by_id(
+        # project_id=project_id,
+        app_variant_id=prompt_ref.id,
+    )
+
+    if not app_variant:
+        return None
+
+    app_environment_revision = await fetch_app_environment_revision_by_environment(
+        project_id=project_id,
+        environment_id=app_environment_revision.environment_id,
+        revision=app_variant.revision,
+    )
+
+    if not app_environment_revision:
+        return None
+
+    variant_base = await fetch_base_by_id(
+        project_id=project_id,
+        base_id=app_variant.base_id,
+    )
+
+    if not variant_base:
+        return None
+
+    deployment = await get_deployment_by_id(
+        project_id=project_id,
+        deployment_id=variant_base.deployment_id,
+    )
+
+    if not deployment:
+        return None
+
+    prompt = PromptDTO(
+        id=prompt_ref.id,
+        ref=ReferenceDTO(
+            id=prompt_ref.id,
+            version=app_variant.revision,
+            commit_id=app_variant.id,
+        ),
+        url=deployment.uri,
+        params=app_variant.config_parameters,
+        app_id=app_variant.app_id,
+        env_ref=env_ref,
+        env_name=app_environment.name,
+    )
 
     return prompt
+
+
+async def deploy_prompt_by_app_id_and_env_name(
+    project_id: str,
+    app_id: str,
+    env_name: str,
+) -> Optional[PromptDTO]:
+    raise NotImplementedError()
