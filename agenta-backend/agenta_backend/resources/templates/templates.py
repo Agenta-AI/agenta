88b9a85092dde127d1cdcc024485fd4fff77054c
aakrem <akrem@agenta.ai>
2024-10-17 15:25:27 +0200

    refactor

--- agenta-backend/agenta_backend/resources/templates/templates.py
+++ agenta-backend/agenta_backend/resources/templates/templates.py
@@ -1,4 +1,4 @@
-templates_info = {
+templates = {
     "chat_openai": {
         "name": "Single Prompt OpenAI",
         "description": "Sample single prompt application using different OpenAI models",
@@ -8,13 +8,3 @@ templates_info = {
         "description": "Sample Chat application using different OpenAI models",
     },
 }
-
-
-def get_oss_templates():
-    """
-    Returns a list of templates.
-
-    Returns:
-        List[dict]: A list of template dictionaries.
-    """
-    return templates_info
