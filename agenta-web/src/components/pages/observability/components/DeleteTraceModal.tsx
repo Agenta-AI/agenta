ead9be480696d716fb9214e0f6e4cea49c40ce5f
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2024-10-20 22:55:10 +0200

    feat(frontend): added delete trace modal component

---
+++ agenta-web/src/components/pages/observability/components/DeleteTraceModal.tsx
@@ -0,0 +1,23 @@
+import {DeleteOutlined} from "@ant-design/icons"
+import {Modal} from "antd"
+import React from "react"
+
+type DeleteTraceModalProps = {} & React.ComponentProps<typeof Modal>
+
+const DeleteTraceModal = ({...props}: DeleteTraceModalProps) => {
+    return (
+        <Modal
+            centered
+            destroyOnClose
+            width={380}
+            title={"Are you sure you want to delete?"}
+            okButtonProps={{icon: <DeleteOutlined />, danger: true}}
+            okText={"Delete"}
+            {...props}
+        >
+            This action is not reversible.
+        </Modal>
+    )
+}
+
+export default DeleteTraceModal
