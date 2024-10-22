1a83c450589165fadbf61fdb2e1d1fc2626b62b2
Mahmoud Mabrouk <mahmoud@agenta.ai>
2024-10-22 13:17:01 +0200

    test(sdk): added sanity check app

---
+++ agenta-cli/tests/observability_sdk/sanity_check/app_old_sdk.py
@@ -0,0 +1,40 @@
+
+import agenta as ag
+from pydantic import BaseModel, Field
+
+ag.init()
+
+class MyConfig(BaseModel):
+    prompt: str = Field(default="somevalue")
+
+@ag.instrument(
+    spankind="EMBEDDING",
+    ignore_outputs=["ignored", "cost", "usage"],
+)
+def embed(description: str):
+    print("embed")
+    return {
+        "embedding": "somedata",
+        "ignored": "ignored",
+        "cost": 15,
+        "usage": 20,
+    }
+
+
+@ag.instrument(spankind="GENERATOR", ignore_inputs=True)
+async def summarizer(topic: str, genre: str, report: dict) -> dict:
+
+    return {"report": "mya"}
+
+
+@ag.entrypoint
+@ag.instrument(spankind="WORKFLOW")
+async def rag(topic: str, genre: str, count: int = 5):
+    return "simple"
+    # result = await embed("something")
+
+
+    # result = await summarizer("topic", "genre", "report")
+
+
+    # return result["report"]
