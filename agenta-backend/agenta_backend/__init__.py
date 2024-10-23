5054c3385eda68c3aa94035845a1f723793e48e1
Mahmoud Mabrouk <mahmoud@agenta.ai>
2024-10-23 19:47:01 +0200

    Update __init__.py

--- agenta-backend/agenta_backend/__init__.py
+++ agenta-backend/agenta_backend/__init__.py
@@ -4,3 +4,5 @@ if os.environ["FEATURE_FLAG"] in ["cloud", "cloud-dev"]:
     import agenta_backend.cloud.__init__
 if os.environ["FEATURE_FLAG"] in ["ee"]:
     import agenta_backend.ee.__init__
+
+
