8d9638be83a4a60327b6796cc45f109adc1ddf3d
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2024-10-25 14:25:30 +0200

    fix(frontend): cleanup and improved fetch trace logic

--- agenta-web/src/lib/hooks/useTraces.ts
+++ agenta-web/src/lib/hooks/useTraces.ts
@@ -1,6 +1,12 @@
 import {fetchAllTraces} from "@/services/observability/core"
-import {_AgentaRootsResponse} from "@/services/observability/types"
+import {
+    _AgentaRootsResponse,
+    AgentaNodeDTO,
+    AgentaRootsDTO,
+    AgentaTreeDTO,
+} from "@/services/observability/types"
 import {useEffect, useState} from "react"
+import {buildNodeTree, observabilityTransformer} from "../helpers/observability_helpers"
 
 export const useTraces = () => {
     const [traces, setTraces] = useState<_AgentaRootsResponse[]>([])
@@ -10,9 +16,35 @@ export const useTraces = () => {
         try {
             setIsLoadingTraces(true)
             const data = await fetchAllTraces()
-            setTraces(data)
+
+            const transformedTraces: _AgentaRootsResponse[] = []
+
+            if (data?.roots) {
+                transformedTraces.push(
+                    ...data.roots.flatMap((item: AgentaRootsDTO) =>
+                        observabilityTransformer(item.trees[0]),
+                    ),
+                )
+            }
+
+            if (data?.trees) {
+                transformedTraces.push(
+                    ...data.trees.flatMap((item: AgentaTreeDTO) => observabilityTransformer(item)),
+                )
+            }
+
+            if (data?.nodes) {
+                transformedTraces.push(
+                    ...data.nodes
+                        .flatMap((node: AgentaNodeDTO) => buildNodeTree(node))
+                        .flatMap((item: AgentaTreeDTO) => observabilityTransformer(item)),
+                )
+            }
+
+            setTraces(transformedTraces)
         } catch (error) {
             console.error(error)
+            console.error("Failed to fetch traces:", error)
         } finally {
             setIsLoadingTraces(false)
         }
@@ -22,8 +54,5 @@ export const useTraces = () => {
         fetchTraces()
     }, [])
 
-    return {
-        traces: traces,
-        isLoadingTraces,
-    }
+    return {traces, isLoadingTraces}
 }
