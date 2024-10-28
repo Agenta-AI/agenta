f7763886683cf9a3057afc2de931c3f85b70aab5
ashrafchowdury <ashrafdev365@gmail.com>
2024-10-28 18:09:55 +0600

    enhane(frontend): refactor endpoint api url

--- agenta-web/src/services/observability/core/index.ts
+++ agenta-web/src/services/observability/core/index.ts
@@ -9,9 +9,7 @@ import axios from "@/lib/helpers/axiosConfig"
 //  - update: PUT data to server
 //  - delete: DELETE data from server
 
-export const fetchAllTraces = async (projectId: string) => {
-    const response = await axios.get(
-        `${getAgentaApiUrl()}/api/observability/v1/traces/search?project_id=${projectId}`,
-    )
+export const fetchAllTraces = async () => {
+    const response = await axios.get(`${getAgentaApiUrl()}/api/observability/v1/traces/search`)
     return response.data
 }
