c1ba6e9c84b8ac34191b7bda41dd6e9a3b54c9c4
Abraham 'Abram' Israel <israelvictory87@gmail.com>
2024-10-29 10:28:31 +0100

    feat (cli): create decorator for handling exceptions in CLI commands

---
+++ agenta-cli/agenta/sdk/utils/exceptions.py
@@ -0,0 +1,20 @@
+import logging
+from functools import wraps
+
+
+logger = logging.getLogger(__name__)
+
+
+def handle_exceptions():
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            try:
+                return await func(*args, **kwargs)
+            except Exception as e:
+                logger.error("--- HANDLING EXCEPTION ---")
+                logger.error("--------------------------")
+                raise e
+
+        return wrapper
+    return decorator
