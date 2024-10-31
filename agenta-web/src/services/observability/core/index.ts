bbd8c957476f54e4625252ad5afacd967485131c
ashrafchowdury <ashrafdev365@gmail.com>
2024-10-31 16:02:48 +0600

    fix(frontend): converted observability page into component

--- agenta-web/src/services/observability/core/index.ts
+++ agenta-web/src/services/observability/core/index.ts
@@ -11,7 +11,7 @@ import axios from "@/lib/helpers/axiosConfig"
 
 export const fetchAllTraces = async ({appId, queries}: {appId: string; queries?: string}) => {
     const filterByAppId = appId
-        ? `filtering={"conditions":[{"key":"refs.application_id","operator":"is","value":"${appId}"}]}&`
+        ? `filtering={"conditions":[{"key":"refs.application.id","operator":"is","value":"${appId}"}]}&`
         : ""
 
     const response = await axios.get(
