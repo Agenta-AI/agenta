2701b0c471cea654797c8048d77460d6e26e008b
ashrafchowdury <ashrafdev365@gmail.com>
2024-11-19 13:16:40 +0600

    feat(frontend): created a global hook to store project data

---
+++ agenta-web/src/contexts/project.context.tsx
@@ -0,0 +1,84 @@
+import {useSession} from "@/hooks/useSession"
+import {PropsWithChildren, createContext, useState, useContext, useEffect, useCallback} from "react"
+import {fetcAllhProjects} from "@/services/project"
+import useStateCallback from "@/hooks/useStateCallback"
+
+type Project = {
+    workspace_id: string | null
+    workspace_name: string | null
+    project_id: string | null
+    project_name: string | null
+    user_role: string | null
+}
+
+type ProjectContextType = {
+    project: Project | null
+    isProjectId: boolean
+    isLoading: boolean
+    reset: () => void
+    refetch: (onSuccess?: () => void) => void
+}
+
+const initialValues: ProjectContextType = {
+    project: null,
+    isProjectId: false,
+    isLoading: false,
+    reset: () => {},
+    refetch: () => {},
+}
+
+export const ProjectContext = createContext<ProjectContextType>(initialValues)
+
+export const useProjectData = () => useContext(ProjectContext)
+
+const projectContextValues = {...initialValues}
+
+export const getProjectValues = () => projectContextValues
+
+const ProjectContextProvider: React.FC<PropsWithChildren> = ({children}) => {
+    const [project, setProject] = useStateCallback<Project | null>(null)
+    const [isLoading, setIsLoading] = useState(true)
+    const {doesSessionExist} = useSession()
+
+    const isProjectId = !isLoading && Boolean(project?.project_id)
+
+    const fetcher = useCallback(async (onSuccess?: () => void) => {
+        try {
+            const data = await fetcAllhProjects()
+            setProject(data[0], onSuccess)
+        } catch (error) {
+            console.error(error)
+        } finally {
+            setIsLoading(false)
+        }
+    }, [])
+
+    useEffect(() => {
+        if (doesSessionExist) {
+            fetcher()
+        }
+    }, [doesSessionExist])
+
+    const reset = () => {
+        setProject(initialValues.project)
+    }
+
+    projectContextValues.project = project
+    projectContextValues.isProjectId = isProjectId
+
+    return (
+        <ProjectContext.Provider
+            value={{
+                project,
+                isProjectId,
+                isLoading,
+                reset,
+                refetch: fetcher,
+            }}
+        >
+            {children}
+        </ProjectContext.Provider>
+    )
+}
+
+export default ProjectContextProvider
---
+++ agenta-web/src/services/project/index.ts
@@ -0,0 +1,14 @@
+import axios from "@/lib/helpers/axiosConfig"
+import {getAgentaApiUrl} from "@/lib/helpers/utils"
+
+//Prefix convention:
+//  - fetch: GET single entity from server
+//  - fetchAll: GET all entities from server
+//  - create: POST data to server
+//  - update: PUT data to server
+//  - delete: DELETE data from server
+
+export const fetcAllhProjects = async () => {
+    const response = await axios.get(`${getAgentaApiUrl()}/api/projects`)
+    return response.data
+}
