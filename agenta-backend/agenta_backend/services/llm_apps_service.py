fabf5ab5bddae51b7cbaac76cea27bda5c6e7b88
Juan Pablo Vega <jp@agenta.ai>
2024-11-21 10:49:20 +0100

    apply black

--- agenta-backend/agenta_backend/services/llm_apps_service.py
+++ agenta-backend/agenta_backend/services/llm_apps_service.py
@@ -293,9 +293,9 @@ async def batch_invoke(
         "delay_between_batches"
     ]  # Delay between batches (in seconds)
 
-    list_of_app_outputs: List[InvokationResult] = (
-        []
-    )  # Outputs after running all batches
+    list_of_app_outputs: List[
+        InvokationResult
+    ] = []  # Outputs after running all batches
 
     headers = None
     if isCloudEE():
