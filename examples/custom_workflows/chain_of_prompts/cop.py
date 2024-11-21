8c86c6bbde7d7c1c7fc55f45f0f7cc8e93cb2ec3
Mahmoud Mabrouk <mahmoud@agenta.ai>
2024-11-21 18:26:58 +0100

    docs(app): AGE-874 update example to include instrumentation

--- examples/custom_workflows/chain_of_prompts/cop.py
+++ examples/custom_workflows/chain_of_prompts/cop.py
@@ -1,17 +1,23 @@
 from openai import OpenAI
 import agenta as ag
 from pydantic import BaseModel, Field
+from opentelemetry.instrumentation.openai import OpenAIInstrumentor
+
 ag.init()
 
 client = OpenAI()
 prompt1 = "Summarize the following blog post: {blog_post}"
 prompt2 = "Write a tweet based on this: {output_1}"
 
+OpenAIInstrumentor().instrument()
+
+
 class CoPConfig(BaseModel):
     prompt1: str = Field(default=prompt1)
     prompt2: str = Field(default=prompt2)
 
 @ag.route("/", config_schema=CoPConfig)
+@ag.instrument()
 def generate(blog_post: str):
     config = ag.ConfigManager.get_from_route(schema=CoPConfig)
     formatted_prompt1 = config.prompt1.format(blog_post=blog_post)
