2e8d80738dff7e0cab1e1b1ab6d2230f5dd86b24
Mahmoud Mabrouk <mahmoud@agenta.ai>
2024-11-22 16:43:03 +0100

    format

--- agenta-web/src/code_snippets/endpoints/fetch_config/typescript.ts
+++ agenta-web/src/code_snippets/endpoints/fetch_config/typescript.ts
@@ -1,4 +1,4 @@
-import { js as beautify } from "js-beautify";
+import {js as beautify} from "js-beautify"
 
 export default function tsCode(appName: string, env_name: string): string {
     const codeString = `import axios from 'axios';
@@ -33,8 +33,8 @@ const getConfig = async (appName: string, environmentSlug: string) => {
 };
 
 getConfig('demo', 'production').then(console.log).catch(console.error);
-    `;
+    `
 
-    const formattedCodeString = beautify(codeString);
-    return formattedCodeString;
+    const formattedCodeString = beautify(codeString)
+    return formattedCodeString
 }
