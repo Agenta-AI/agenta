28bf6c5f2ea64be82666a8720ae9528e26c1a21d
aakrem <akrem@agenta.ai>
2024-11-25 15:02:53 +0100

    trigger prettier check to be sure

--- agenta-web/src/hooks/useAppId.ts
+++ agenta-web/src/hooks/useAppId.ts
@@ -1,6 +1,6 @@
-import {useRouter} from "next/router"
+import { useRouter } from "next/router"
 
 export const useAppId = (): string => {
     const router = useRouter()
-    return (router.query.app_id ?? "") as string
+    return (router.query.app_id ?? '') as string
 }
