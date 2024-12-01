8b0dae311d6eb6250a2b3845022adef54297ea04
Mahmoud Mabrouk <mahmoud@agenta.ai>
2024-12-01 21:04:19 +0100

    moving ee components to ee folder

--- agenta-web/src/lib/helpers/dynamic.ts
+++ agenta-web/src/lib/helpers/dynamic.ts
@@ -1,7 +1,7 @@
 import dynamic from "next/dynamic"
 
 export function dynamicComponent<T>(path: string, fallback: any = () => null) {
-    return dynamic<T>(() => import(`@/components/${path}`), {
+    return dynamic<T>(() => import(`@/ee/components/${path}`), {
         loading: fallback,
         ssr: false,
     })
