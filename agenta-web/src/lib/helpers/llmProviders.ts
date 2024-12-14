3f053c0fe52122b7baf494ab28097ca184843b91
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2024-12-14 23:56:30 +0100

    feat(frontend): added helper to transform secret response

--- agenta-web/src/lib/helpers/llmProviders.ts
+++ agenta-web/src/lib/helpers/llmProviders.ts
@@ -1,5 +1,5 @@
 import cloneDeep from "lodash/cloneDeep"
-import {camelToSnake} from "./utils"
+import {SecretDTOProvider, VaultSecretDTO} from "../types_ee"
 
 const llmAvailableProvidersToken = "llmAvailableProvidersToken"
 
@@ -7,6 +7,7 @@ export type LlmProvider = {
     title: string
     key: string
     name: string
+    id?: string
 }
 
 export const llmAvailableProviders: LlmProvider[] = [
@@ -57,6 +58,37 @@ export const saveLlmProviderKey = (providerName: string, keyValue: string) => {
 
 export const getLlmProviderKey = (providerName: string) =>
     getAllProviderLlmKeys().find((item: LlmProvider) => item.title === providerName)?.key
+
+export const transformSecret = (secrets: VaultSecretDTO[]) => {
+    return secrets.reduce((acc, curr) => {
+        const name = curr.header?.name
+        const {key, provider} = curr.secret.data
+
+        const envNameMap: Record<string, string> = {
+            [SecretDTOProvider.OPENAI]: "OPENAI_API_KEY",
+            [SecretDTOProvider.COHERE]: "COHERE_API_KEY",
+            [SecretDTOProvider.ANYSCALE]: "ANYSCALE_API_KEY",
+            [SecretDTOProvider.DEEPINFRA]: "DEEPINFRA_API_KEY",
+            [SecretDTOProvider.ALEPHALPHA]: "ALEPHALPHA_API_KEY",
+            [SecretDTOProvider.GROQ]: "GROQ_API_KEY",
+            [SecretDTOProvider.MISTRALAI]: "MISTRAL_API_KEY",
+            [SecretDTOProvider.ANTHROPIC]: "ANTHROPIC_API_KEY",
+            [SecretDTOProvider.PERPLEXITYAI]: "PERPLEXITYAI_API_KEY",
+            [SecretDTOProvider.TOGETHERAI]: "TOGETHERAI_API_KEY",
+            [SecretDTOProvider.OPENROUTER]: "OPENROUTER_API_KEY",
+            [SecretDTOProvider.GEMINI]: "GEMINI_API_KEY",
+        }
+
+        acc.push({
+            title: name || "",
+            key: key,
+            name: envNameMap[provider] || "",
+            id: curr.id,
+        })
+
+        return acc
+    }, [] as LlmProvider[])
+}
 
 export const getAllProviderLlmKeys = () => {
     const providers = cloneDeep(llmAvailableProviders)
