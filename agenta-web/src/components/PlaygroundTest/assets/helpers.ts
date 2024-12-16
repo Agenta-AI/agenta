7e6e9dc07a5e3047f43e52d341670cde5aee5289
Arda Erzin <ardaerzin@gmail.com>
2024-12-16 01:23:33 +0100

    refactor(frontend): move config related functions to root

--- agenta-web/src/components/PlaygroundTest/assets/helpers.ts
+++ agenta-web/src/components/PlaygroundTest/assets/helpers.ts
@@ -1,3 +1,6 @@
+import {getBodySchemaName} from "@/lib/helpers/openapi_parser"
+import type {OpenAPISchema, GroupConfigReturn, PromptDefaults, ModelDefaults} from "../state/types"
+
 /**
  * Accesses a nested property in an object using a dot and bracket notation path.
  *
@@ -13,4 +16,89 @@ export const accessKeyInVariant = (path: string, object: Record<string, any>): a
             if (o === undefined || o === null) return undefined
             return o[i]
         }, object)
+}
+
+export const groupConfigOptions = <R extends boolean = false, P extends boolean = false>({
+    configObject,
+    filterByName = (_: string) => true,
+    reduce = false as R,
+    configKeyRoot,
+}: {
+    configObject: Record<string, any>
+    filterByName: (propertyName: string) => boolean
+    reduce?: R
+    configKeyRoot: string
+}): GroupConfigReturn<R, P> => {
+    const filtered = Object.keys(configObject).filter(filterByName)
+
+    return (
+        reduce
+            ? filtered.reduce(
+                  (acc, propertyName) => ({
+                      ...acc,
+                      [propertyName]: configObject[propertyName],
+                      key: propertyName,
+                  }),
+                  {} as P extends true ? PromptDefaults : ModelDefaults,
+              )
+            : filtered.map((propertyName, index) => ({
+                  ...(configObject[propertyName] || {}),
+                  key: propertyName,
+                  configKey: `${configKeyRoot}.[${index}]`,
+              }))
+    ) as GroupConfigReturn<R, P>
+}
+
+/**
+ * Parses the variant schema from the OpenAPI schema.
+ *
+ * @param {OpenAPISchema} originalSchema - The original OpenAPI schema.
+ * @returns {any} - The parsed variant schema.
+ */
+export const parseVariantSchema = (originalSchema: OpenAPISchema) => {
+    const schemaName = originalSchema ? getBodySchemaName(originalSchema) : ""
+
+    // TODO: refactor when we have multiple prompts
+    // Extract our configuration that defines the shape of a
+    // variant from openapi schema
+    const agentaConfig = originalSchema.components.schemas[schemaName]?.properties?.agenta_config
+
+    const configKeyRoot = `schema.promptConfig.[${0}]`
+    const modelProperties = groupConfigOptions<false, false>({
+        configObject: agentaConfig?.properties || {},
+        filterByName: (propertyName) => !propertyName.includes("prompt_"),
+        configKeyRoot: `${configKeyRoot}.modelProperties`,
+    })
+    const modelDefaults = groupConfigOptions<true, false>({
+        configObject: agentaConfig?.default,
+        filterByName: (propertyName) => !propertyName.includes("prompt_"),
+        reduce: true,
+        configKeyRoot: `${configKeyRoot}.configKeyRoot`,
+    })
+
+    const promptProperties = groupConfigOptions<false, false>({
+        configObject: agentaConfig?.properties || {},
+        filterByName: (propertyName) => propertyName.includes("prompt_"),
+        configKeyRoot: `${configKeyRoot}.promptProperties`,
+    })
+
+    const promptDefaults = groupConfigOptions<true, true>({
+        configObject: agentaConfig?.default,
+        filterByName: (propertyName) => propertyName.includes("prompt_"),
+        reduce: true,
+        configKeyRoot: `${configKeyRoot}.promptDefaults`,
+    })
+
+    return {
+        schemaName,
+        promptConfig: [
+            {
+                key: `${schemaName}-prompt-${0}`,
+                modelProperties,
+                modelDefaults,
+                promptProperties,
+                promptDefaults,
+            },
+        ],
+    }
 }
