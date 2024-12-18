f88c8faa7af2c14559bfe49d7d892035b9a9a2bd
ashrafchowdury <ashrafdev365@gmail.com>
2024-12-18 17:04:24 +0600

    refactor(frontend): removed observability index file from oss

--- agenta-web/src/services/observability/api/index.ts
+++
@@ -1,6 +0,0 @@
-//Prefix convention:
-//  - fetch: GET single entity from server
-//  - fetchAll: GET all entities from server
-//  - create: POST data to server
-//  - update: PUT data to server
-//  - delete: DELETE data from server
