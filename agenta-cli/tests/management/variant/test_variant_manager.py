dc5db1b94a6c072ed3c6140c807a15c9e4cea233
Abram <israelvictory87@gmail.com>
2024-12-19 15:16:27 +0100

    feat (sdk:tests): add more test cases to VariantManager test suite.

--- agenta-cli/tests/management/variant/test_variant_manager.py
+++ agenta-cli/tests/management/variant/test_variant_manager.py
@@ -2,6 +2,8 @@ import uuid
 
 import pytest
 
+from tests.management.deployment.fixtures import *
+
 
 @pytest.mark.usefixtures("create_app_from_template")
 class TestVariantManager:
@@ -104,3 +106,288 @@ class TestVariantManager:
 
         # ASSERT: Verify validation error
         assert response.status_code == 422, "Expected 422 validation error"
+
+    @pytest.mark.asyncio
+    @pytest.mark.variant_manager
+    async def test_configs_commit_success(
+        self,
+        http_client,
+        get_variant_revisions,
+        get_completion_app_from_list,
+    ):
+        # ARRANGE: Prepare test data
+        app_id = get_completion_app_from_list.get("app_id", None)
+        variant_revision = get_variant_revisions[0]
+        variant_revision_id = variant_revision.get("id", None)
+        variant_revision_config_name = variant_revision.get("config", {}).get(
+            "config_name", None
+        )
+        variant_revision_version = variant_revision.get("revision")
+
+        # ACT: Add configuration
+        response = await http_client.post(
+            "variants/configs/commit",
+            json={
+                "config": {
+                    "params": {
+                        "model": "gpt-4",
+                        "top_p": 1,
+                        "inputs": [{"name": "country"}],
+                        "force_json": 0,
+                        "max_tokens": 1000,
+                        "prompt_user": "What is the capital of {country}?",
+                        "temperature": 0.65,
+                        "prompt_system": "You are an expert in geography.",
+                        "presence_penalty": 0,
+                        "frequence_penalty": 0,
+                    },
+                    "application_ref": {
+                        "slug": None,
+                        "version": None,
+                        "id": app_id,
+                    },
+                    "service_ref": None,
+                    "variant_ref": {
+                        "slug": variant_revision_config_name,
+                        "version": variant_revision_version,
+                        "id": variant_revision_id,
+                    },
+                    "environment_ref": None,
+                }
+            },
+        )
+
+        # ASSERT: Verify response
+        assert response.status_code == 200
+        assert "params" and "url" in response.json()
+        assert "variant_lifecycle" in response.json()
+
+    @pytest.mark.asyncio
+    @pytest.mark.variant_manager
+    async def test_configs_commit_missing_data(
+        self, http_client, get_completion_app_from_list
+    ):
+        # ARRANGE: Prepare test data
+        app_id = get_completion_app_from_list.get("app_id", None)
+
+        # ACT: Add configuration
+        response = await http_client.post(
+            "variants/configs/commit",
+            json={
+                "params": {},
+                "url": "",
+                "application_ref": {
+                    "slug": "test",
+                    "version": None,
+                    "id": app_id,
+                },
+            },
+        )
+
+        # ASSERT: Verify response
+        assert response.status_code == 422
+
+    @pytest.mark.asyncio
+    @pytest.mark.variant_manager
+    async def test_configs_delete_success(
+        self, http_client, get_completion_app_from_list, list_app_variants
+    ):
+        # ARRANGE: Prepare test data
+        app_name = get_completion_app_from_list.get("app_name", None)
+        app_variant = list_app_variants[0]
+
+        # ACT: Add configuration
+        variant_response = await http_client.post(
+            "variants/from-base",
+            json={
+                "base_id": app_variant.get("base_id"),
+                "new_variant_name": "from_pytest_for_deletion",
+                "new_config_name": "from_base_config",
+                "parameters": {},
+            },
+        )
+        variant_response.raise_for_status()
+
+        response = await http_client.post(
+            "variants/configs/delete",
+            json={
+                "variant_ref": {
+                    "slug": "from_pytest_for_deletion",
+                    "version": None,
+                    "id": None,
+                },
+                "application_ref": {
+                    "slug": app_name,
+                    "version": None,
+                    "id": None,
+                },
+            },
+        )
+
+        # ASSERT: Verify response
+        assert response.status_code == 200
+        assert 204 == response.json()
+
+    @pytest.mark.asyncio
+    @pytest.mark.variant_manager
+    async def test_configs_delete_not_found(
+        self, http_client, get_completion_app_from_list
+    ):
+        # ARRANGE: Prepare test data
+        app_name = get_completion_app_from_list.get("app_name", None)
+
+        # ACT: Add configuration
+        response = await http_client.post(
+            "variants/configs/delete",
+            json={
+                "variant_ref": {
+                    "slug": "non-existent-variant",
+                    "version": None,
+                    "id": None,
+                },
+                "application_ref": {
+                    "slug": app_name,
+                    "version": None,
+                    "id": None,
+                },
+            },
+        )
+
+        # ASSERT: Verify response
+        assert 204 == response.json()
+
+    @pytest.mark.asyncio
+    @pytest.mark.variant_manager
+    async def test_configs_list_success(
+        self, http_client, get_completion_app_from_list
+    ):
+        # ARRANGE: Prepare test data
+        app_name = get_completion_app_from_list.get("app_name", None)
+
+        # ACT: Add configuration
+        response = await http_client.post(
+            "variants/configs/list",
+            json={
+                "application_ref": {
+                    "slug": app_name,
+                    "version": None,
+                    "id": None,
+                }
+            },
+        )
+
+        # ASSERT: Verify response
+        assert response.status_code == 200
+        assert isinstance(response.json(), list)
+        assert len(response.json()) > 0
+
+    @pytest.mark.asyncio
+    @pytest.mark.variant_manager
+    async def test_configs_list_not_found(self, http_client):
+        # ACT: Add configuration
+        response = await http_client.post(
+            "variants/configs/list",
+            json={
+                "application_ref": {
+                    "slug": "non_existent_app",
+                    "version": None,
+                    "id": None,
+                }
+            },
+        )
+
+        # ASSERT: Verify response
+        assert response.status_code == 200
+        assert [] == response.json()
+
+    @pytest.mark.asyncio
+    @pytest.mark.variant_manager
+    async def test_configs_history_by_slug_and_appid_success(
+        self,
+        http_client,
+        get_completion_app_from_list,
+        get_variant_revisions,
+    ):
+        # ARRANGE: Prepare test data
+        app_id = get_completion_app_from_list.get("app_id", None)
+        variant_revision = get_variant_revisions[0]
+        variant_revision_config_name = variant_revision.get("config", {}).get(
+            "config_name", None
+        )
+
+        # ACT: Add configuration
+        response = await http_client.post(
+            "variants/configs/history",
+            json={
+                "application_ref": {
+                    "slug": None,
+                    "version": None,
+                    "id": app_id,
+                },
+                "variant_ref": {
+                    "slug": variant_revision_config_name,
+                    "version": None,
+                    "id": None,
+                },
+            },
+        )
+
+        # ASSERT: Verify response
+        assert response.status_code == 200
+        assert isinstance(response.json(), list)
+        assert len(response.json()) >= 1
+
+    @pytest.mark.asyncio
+    @pytest.mark.variant_manager
+    async def test_configs_history_by_id_success(self, http_client, list_app_variants):
+        # ARRANGE: Prepare test data
+        app_variant = list_app_variants[0]
+
+        # ACT: Add configuration
+        response = await http_client.post(
+            "variants/configs/history",
+            json={
+                "variant_ref": {
+                    "slug": None,
+                    "version": None,
+                    "id": app_variant.get("variant_id", None),
+                },
+            },
+        )
+
+        # ASSERT: Verify response
+        assert response.status_code == 200
+        assert isinstance(response.json(), list)
+        assert len(response.json()) > 0
+
+    @pytest.mark.asyncio
+    @pytest.mark.variant_manager
+    async def test_configs_history_not_found(self, http_client):
+        # ACT: Add configuration
+        app_not_found_response = await http_client.post(
+            "variants/configs/history",
+            json={
+                "variant_ref": {
+                    "slug": "non_existent_app",
+                    "version": None,
+                    "id": None,
+                }
+            },
+        )
+        variant_not_found_response = await http_client.post(
+            "variants/configs/history",
+            json={
+                "variant_ref": {
+                    "slug": None,
+                    "version": None,
+                    "id": str(uuid.uuid4()),
+                }
+            },
+        )
+
+        # ASSERT: Verify response
+        assert app_not_found_response.status_code == 200
+        assert variant_not_found_response.status_code == 200
+        assert [] == (
+            app_not_found_response.json() and variant_not_found_response.json()
+        )
