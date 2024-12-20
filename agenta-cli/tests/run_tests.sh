b0a194887d7446c5c1af477ac07bd15fdb73261f
Abram <israelvictory87@gmail.com>
2024-12-20 13:38:23 +0100

    refactor (sdk:tests): add command to run sdk_routing test suites

--- agenta-cli/tests/run_tests.sh
+++ agenta-cli/tests/run_tests.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-OPENAI_API_KEY=sk-xxxxxxxx AGENTA_HOST=http://localhost AGENTA_API_KEY=xxxxx.xxxxxxxxxxxxx pytest management/ -v -m variant_management # run variant_management tests
-
+OPENAI_API_KEY=sk-xxxxx AGENTA_HOST=http://localhost AGENTA_API_KEY=xxxx.xxxxxxxxxxxxxxx pytest -n 2 -v ./management/*
+BASE_URL=http://127.0.0.1 AGENTA_HOST=http://localhost AGENTA_API_KEY=xxxx.xxxxxxxxxxxxxxx pytest -v ./sdk_routing/*
