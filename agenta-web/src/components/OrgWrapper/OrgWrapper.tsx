23822794477d0468a03f38fa9e51a4d23733efeb
ashrafchowdury <ashrafdev365@gmail.com>
2024-12-24 00:46:41 +0600

    fix(frontend): import cloud components error

--- agenta-web/src/components/OrgWrapper/OrgWrapper.tsx
+++ agenta-web/src/components/OrgWrapper/OrgWrapper.tsx
@@ -1,13 +1,13 @@
 import {useState, useEffect, useCallback} from "react"
 import {isDemo} from "@/lib/helpers/utils"
+import {dynamicContext} from "@/lib/helpers/dynamic"
 
 const OrgWrapper = ({children}: {children: React.ReactNode}) => {
     const [Wrapper, setWrapper] = useState<any>(null)
 
     const initilizeOrgProvider = useCallback(async () => {
-        // @ts-ignore
-        const OrgProvider = (await import("@/contexts/org.context")).default
-        setWrapper(() => OrgProvider)
+        const OrgContextProvider = await dynamicContext("org.context", Wrapper)
+        setWrapper(() => OrgContextProvider.default)
     }, [])
 
     useEffect(() => {
