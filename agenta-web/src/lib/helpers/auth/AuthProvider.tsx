aed3775e44d76b1efd3e7373154ea4d1b860a763
ashrafchowdury <ashrafdev365@gmail.com>
2024-12-23 19:06:39 +0600

    refactor(frontend): moved auth init code from _app.tsx to a component

---
+++ agenta-web/src/lib/helpers/auth/AuthProvider.tsx
@@ -0,0 +1,38 @@
+import {useEffect, useCallback} from "react"
+import SuperTokensReact, {SuperTokensWrapper} from "supertokens-auth-react"
+import {AuthProviderType} from "./types"
+import {isDemo} from "../utils"
+;(async () => {
+    if (typeof window !== "undefined" && isDemo()) {
+        const {frontendConfig} = await import("@/config/frontendConfig")
+        SuperTokensReact.init(frontendConfig())
+    }
+})()
+
+const AuthProvider: AuthProviderType = ({children, pageProps}) => {
+    const doRefresh = useCallback(async () => {
+        if (!isDemo()) return
+
+        if (pageProps.fromSupertokens === "needs-refresh") {
+            const session = await import("supertokens-auth-react/recipe/session")
+
+            if (await session.attemptRefreshingSession()) {
+                location.reload()
+            } else {
+                SuperTokensReact.redirectToAuth()
+            }
+        }
+    }, [pageProps.fromSupertokens])
+
+    useEffect(() => {
+        doRefresh()
+    }, [doRefresh])
+
+    if (isDemo() && pageProps.fromSupertokens === "needs-refresh") {
+        return null
+    }
+
+    return isDemo() ? <SuperTokensWrapper>{children}</SuperTokensWrapper> : <>{children}</>
+}
+
+export default AuthProvider
