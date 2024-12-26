08324af23d606529fe92f2116ca23725a8140e6d
ashrafchowdury <ashrafdev365@gmail.com>
2024-12-26 22:14:10 +0600

    enhance(frontend): implemented select model UI

--- agenta-web/src/components/PlaygroundTest/Components/PlaygroundVariantModelConfig/assets/ModelConfigModal.tsx
+++ agenta-web/src/components/PlaygroundTest/Components/PlaygroundVariantModelConfig/assets/ModelConfigModal.tsx
@@ -1,6 +1,6 @@
-import {memo, useCallback, type MouseEvent} from "react"
+import {memo, useCallback, useState, type MouseEvent} from "react"
 import clsx from "clsx"
-import {Button} from "antd"
+import {Button, InputNumber, Select, Slider, Switch, Typography} from "antd"
 import PlaygroundVariantPropertyControl from "../../PlaygroundVariantPropertyControl"
 
 import type {
@@ -11,6 +11,8 @@ import type {
 import type {Path} from "../../../types/pathHelpers"
 import type {StateVariant} from "../../../state/types"
 
+const {Text} = Typography
+
 /**
  * Renders the modal action buttons for saving and canceling changes
  */
@@ -20,7 +22,7 @@ const ModalActions: React.FC<ModelConfigModalActionsProps> = ({
     className,
     ...props
 }) => (
-    <div className={clsx("flex items-center justify-end gap-2 mt-4", className)} {...props}>
+    <div className={clsx("flex items-center justify-end gap-2 mt-5", className)} {...props}>
         <Button onClick={handleClose}>Cancel</Button>
         <Button onClick={handleSave} variant="solid" color="default">
             Save
@@ -68,14 +70,61 @@ const ModelConfigModal: React.FC<PlaygroundVariantModelConfigModalProps> = ({
     handleSave,
     handleClose,
 }) => {
+    const [model, setModel] = useState("")
+
     const preventClickBubble = useCallback((e: MouseEvent<HTMLElement>) => {
         e.preventDefault()
         e.stopPropagation()
     }, [])
 
     return (
-        <ModalContent onClick={preventClickBubble}>
-            {properties.map((property) => {
+        <ModalContent onClick={preventClickBubble} className="!w-[300px]">
+            <section className="flex flex-col gap-4">
+                <div className="w-full flex flex-col gap-1">
+                    <Text>Model</Text>
+                    <Select
+                        showSearch
+                        placeholder="Selecct a model"
+                        className="w-full"
+                        popupClassName="w-full"
+                        value={model}
+                        onChange={(value) => setModel(value)}
+                        filterOption={(input, option) =>
+                            (option?.label ?? "").toLowerCase().includes(input.toLowerCase())
+                        }
+                        options={[
+                            {value: "1", label: "Jack"},
+                            {value: "2", label: "Lucy"},
+                            {value: "3", label: "Tom"},
+                        ]}
+                    />
+                </div>
+
+                <div className="w-full flex flex-col gap-2">
+                    <div className="flex flex-col">
+                        <div className="w-full flex items-center justify-between">
+                            <Text>Temprature</Text>
+                            <InputNumber min={1} max={10} defaultValue={3} />
+                        </div>
+                        <Slider min={1} max={20} value={2} />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <div className="w-full flex items-center justify-between">
+                            <Text>Max Tokens</Text>
+                            <InputNumber min={1} max={10} defaultValue={3} />
+                        </div>
+                        <Slider min={1} max={20} value={2} />
+                    </div>
+                </div>
+
+                <div className="flex items-center justify-between">
+                    <Text>Force JSON</Text>
+                    <Switch defaultChecked />
+                </div>
+            </section>
+
+            {/* {properties.map((property) => {
                 return (
                     <PlaygroundVariantPropertyControl
                         key={property.key}
@@ -84,7 +133,8 @@ const ModelConfigModal: React.FC<PlaygroundVariantModelConfigModalProps> = ({
                         valueKey={property.valueKey as Path<StateVariant>}
                     />
                 )
-            })}
+            })} */}
+
             <ModalActions handleSave={handleSave} handleClose={handleClose} />
         </ModalContent>
     )
