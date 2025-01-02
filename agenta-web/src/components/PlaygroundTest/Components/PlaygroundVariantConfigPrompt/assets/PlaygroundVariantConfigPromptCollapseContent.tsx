bf9b841745bffb319a6b8c404265252f60a45460
Arda Erzin <ardaerzin@gmail.com>
2025-01-02 18:17:37 +0100

    chore: cleanup

--- agenta-web/src/components/PlaygroundTest/Components/PlaygroundVariantConfigPrompt/assets/PlaygroundVariantConfigPromptCollapseContent.tsx
+++ agenta-web/src/components/PlaygroundTest/Components/PlaygroundVariantConfigPrompt/assets/PlaygroundVariantConfigPromptCollapseContent.tsx
@@ -1,9 +1,12 @@
+import { useCallback } from "react"
 import AddButton from "../../../assets/AddButton"
 import PromptMessageConfig from "../../PromptMessageConfig"
 import usePlayground from "@/components/PlaygroundTest/hooks/usePlayground"
 import type {PromptCollapseContentProps} from "../types"
+import { createObjectFromMetadata } from "@/components/PlaygroundTest/betterTypes/transformers/arrays"
 import clsx from "clsx"
-
+import type { ArrayMetadata, EnhancedArrayMessage } from "../../../betterTypes/types"
+import {Typography} from "antd"
 /**
  * PlaygroundVariantConfigPromptCollapseContent renders the configuration interface
  * for a single prompt's messages.
@@ -21,7 +24,7 @@ const PlaygroundVariantConfigPromptCollapseContent: React.FC<PromptCollapseConte
     className,
     ...props
 }) => {
-    const {messageIds} = usePlayground({
+    const {messageIds, mutateVariant, inputKeys} = usePlayground({
         variantId,
         hookId: "PlaygroundConfigVariantPrompts",
         variantSelector: (variant) => {
@@ -34,10 +37,30 @@ const PlaygroundVariantConfigPromptCollapseContent: React.FC<PromptCollapseConte
             }
 
             return {
-                messageIds: messages.value.map((message) => message.__id)
+                messageIds: messages.value.map((message) => message.__id),
+                inputKeys: prompt.inputKeys.value || []
             }
         },
     })
+
+    const addNewMessage = useCallback(() => {
+        if (!mutateVariant) return
+
+        mutateVariant((draft) => {
+            const variantPrompt = draft.prompts?.find((p) => p.__id === promptId)
+            const messages = variantPrompt?.messages.value
+            const metadata = (variantPrompt?.messages.__metadata as ArrayMetadata).itemMetadata
+
+            if (variantPrompt && messages && metadata) {
+                const newMessage = createObjectFromMetadata(metadata) as EnhancedArrayMessage
+                if (newMessage) {
+                    messages.push(newMessage)
+                }
+            }
+
+            return draft
+        })
+    }, [mutateVariant, promptId])
 
     console.log(
         "usePlayground[%cComponent%c] - PlaygroundVariantConfigPromptCollapseContent - RENDER!",
@@ -45,6 +68,7 @@ const PlaygroundVariantConfigPromptCollapseContent: React.FC<PromptCollapseConte
         "",
         variantId,
         messageIds,
+        inputKeys
     )
 
     return (
@@ -59,7 +83,16 @@ const PlaygroundVariantConfigPromptCollapseContent: React.FC<PromptCollapseConte
                     messageId={messageId}
                 />
             ))}
-            <AddButton label="Message" />
+            <AddButton label="Message" onClick={addNewMessage} />
+
+            <div className="flex flex-col gap-2">
+                <Typography.Text strong>Input keys:</Typography.Text>
+                {(inputKeys || []).map((inputKey) => (
+                    <div key={inputKey}>
+                        {inputKey}
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
