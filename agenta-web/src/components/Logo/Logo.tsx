66810513b18c4bf751de7e955ef0395947f97123
Kaosiso Ezealigo <ezealigokosiso@gmail.com>
2025-01-07 11:50:50 +0100

    enhancement(frontend): reformat Sidebar component's layout to enhance responsiveness and visual appearance

--- agenta-web/src/components/Logo/Logo.tsx
+++ agenta-web/src/components/Logo/Logo.tsx
@@ -15,25 +15,7 @@ const Logo: React.FC<Partial<React.ComponentProps<typeof Image>> & {isOnlyIconLo
 
     const logoSrc = useMemo(() => LOGOS[appTheme], [appTheme])
 
-    return (
-        <div
-            className={`overflow-hidden h-[51px] transition-width duration-300 ease-in-out ${
-                isOnlyIconLogo ? "w-[40px]" : "w-[154.8px]"
-            }`}
-        >
-            <Image
-                {...imageProps}
-                src={logoSrc}
-                alt="Agenta Logo"
-                width={154.8}
-                height={51}
-                style={{
-                    transition: "transform 0.3s ease",
-                    transform: isOnlyIconLogo ? "translateX(-5px)" : "translateX(0)",
-                }}
-            />
-        </div>
-    )
+    return <Image {...imageProps} src={logoSrc} alt="Agenta Logo" width={154.8} height={51} />
 }
 
 export default Logo
