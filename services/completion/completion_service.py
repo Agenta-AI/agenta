5c622a10f692946e22a61f40aedb19525bec8386
Mahmoud Mabrouk <mahmoud@agenta.ai>
2025-01-10 17:10:59 +0100

    fix(tool): AGE-1430 fixed completion to work with vault

--- services/completion/completion_service.py
+++ services/completion/completion_service.py
@@ -37,10 +37,12 @@ async def generate(
                 detail=f"Invalid inputs. Expected: {sorted(required_keys)}, got: {sorted(provided_keys)}",
             )
 
-    api_key = ag.SecretsManager.get_api_key_for_model(ag.config.model)
+    api_key = ag.SecretsManager.get_api_key_for_model(config.prompt.llm_config.model)
 
     if not api_key:
-        raise ValueError(f"API key not found for model {ag.config.model}")
+        raise ValueError(
+            f"API key not found for model {config.prompt.llm_config.model}"
+        )
 
     response = await litellm.acompletion(
         **{
