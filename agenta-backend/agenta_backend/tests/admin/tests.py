27c8d244ac2d1619d2793fb3ba9d017adb5edb76
Abram <israelvictory87@gmail.com>
2025-01-10 14:34:18 +0100

    feat (tests): refactor api tests for /apps and /testsets scope based on rtm

---
+++ agenta-backend/agenta_backend/tests/admin/tests.py
@@ -0,0 +1,59 @@
+import pytest
+
+
+class TestAdminAuthentication:
+    @pytest.mark.asyncio
+    @pytest.mark.typical
+    @pytest.mark.grumpy
+    @pytest.mark.security
+    async def test_api_authentication_missing_token(self, http_client):
+        # ARRANGE: prepare data
+        expected_status = 401
+        description = "Missing token"
+        headers = {}
+
+        # ACT: send request
+        response = await http_client.get("admin/accounts", headers=headers)
+
+        # ASSERT: verify response
+        assert (
+            response.status_code == expected_status
+        ), f"Failed for case: {description}"
+
+    @pytest.mark.asyncio
+    @pytest.mark.typical
+    @pytest.mark.grumpy
+    @pytest.mark.security
+    async def test_api_authentication_unsupported_token(self, http_client):
+        # ARRANGE: prepare data
+        token = "ak-xxxxxxxxxx"
+        expected_status = 401
+        description = "Unsupported token"
+        headers = {"Authorization": f"{token}"}
+
+        # ACT: send request
+        response = await http_client.get("admin/accounts", headers=headers)
+
+        # ASSERT: verify response
+        assert (
+            response.status_code == expected_status
+        ), f"Failed for case: {description}"
+
+    @pytest.mark.asyncio
+    @pytest.mark.typical
+    @pytest.mark.grumpy
+    @pytest.mark.security
+    async def test_api_authentication_invalid_token(self, http_client):
+        # ARRANGE: prepare data
+        token = "agenta.invalid-token"
+        expected_status = 500
+        description = "Invalid token"
+        headers = {"Authorization": f"Access {token}"}
+
+        # ACT: send request
+        response = await http_client.get("admin/accounts", headers=headers)
+
+        # ASSERT: verify response
+        assert (
+            response.status_code == expected_status
+        ), f"Failed for case: {description}"
