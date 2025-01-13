1070124d440c79bb04a965e2bccea4457c40bbba
Arda Erzin <ardaerzin@gmail.com>
2025-01-13 15:41:33 +0100

    chore(frontend): prettify files

--- agenta-web/src/components/Playground/index.tsx
+++ agenta-web/src/components/Playground/index.tsx
@@ -1,14 +1,14 @@
-import { useMemo } from "react"
+import {useMemo} from "react"
 
-import dynamic from 'next/dynamic'
-import { useRouter } from "next/router"
+import dynamic from "next/dynamic"
+import {useRouter} from "next/router"
 import useSWR from "swr"
 
-import { getAgentaApiUrl } from "@/lib/helpers/utils"
-import { ListAppsItem } from "@/lib/Types"
+import {getAgentaApiUrl} from "@/lib/helpers/utils"
+import {ListAppsItem} from "@/lib/Types"
 
-const NewPlayground = dynamic(() => import("../NewPlayground/Playground"), { ssr: false })
-const OldPlayground = dynamic(() => import("../OldPlayground/Playground"), { ssr: false })
+const NewPlayground = dynamic(() => import("../NewPlayground/Playground"), {ssr: false})
+const OldPlayground = dynamic(() => import("../OldPlayground/Playground"), {ssr: false})
 
 const PlaygroundRouter = () => {
     const router = useRouter()
@@ -22,18 +22,18 @@ const PlaygroundRouter = () => {
     if (isLoading) {
         return <div>Loading...</div>
     } else if (!!app) {
-        if (app.app_type?.includes('(old)')) {
+        if (app.app_type?.includes("(old)")) {
             return <OldPlayground />
         } else {
             if (!router.query.playground) {
                 router.replace(`${router.asPath}?playground=new-playground`, undefined, {
-                    shallow: true
+                    shallow: true,
                 })
             }
             return <NewPlayground />
         }
     } else {
-        router.push('/apps')
+        router.push("/apps")
         return null
     }
 }
