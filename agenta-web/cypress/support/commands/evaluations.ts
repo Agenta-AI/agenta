fc3f2469a8290cab5f50f8b3cf9887bc7823f314
Arda Erzin <ardaerzin@gmail.com>
2025-01-14 00:08:27 +0100

    chore(e2e): use cy.log

--- agenta-web/cypress/support/commands/evaluations.ts
+++ agenta-web/cypress/support/commands/evaluations.ts
@@ -86,7 +86,7 @@ Cypress.Commands.add("cleanupVariantAndTestset", () => {
 
 Cypress.Commands.add("addingOpenaiKey", () => {
     cy.visit("/settings")
-    console.log("ADDING API KEY", !!apiKey)
+    cy.task("log", `ADDING API KEY: ${!!apiKey}`)
     cy.get('[data-cy="openai-api-input"]').eq(0).type(apiKey)
     cy.get('[data-cy="openai-api-save"]').eq(0).click()
 })
