57309447cd684530ec15e9d74c0682007ede13d5
Arda Erzin <ardaerzin@gmail.com>
2025-01-15 11:24:19 +0100

    input row generation from testsets

--- agenta-web/src/components/NewPlayground/Components/PlaygroundGenerations/assets/GenerationHeader/index.tsx
+++ agenta-web/src/components/NewPlayground/Components/PlaygroundGenerations/assets/GenerationHeader/index.tsx
@@ -1,13 +1,49 @@
-import {useState} from "react"
+import {useCallback, useState} from "react"
 import {Play} from "@phosphor-icons/react"
 import {Button, Typography} from "antd"
 import LoadTestsetModal from "../../../Modals/LoadTestsetModal"
 import usePlayground from "@/components/NewPlayground/hooks/usePlayground"
+import {SetStateAction} from "jotai"
+import cloneDeep from "lodash/cloneDeep"
+import {createInputRow} from "@/components/NewPlayground/hooks/usePlayground/assets/inputHelpers"
+import {Enhanced} from "@/components/NewPlayground/assets/utilities/genericTransformer/types"
 
 const GenerationHeader = () => {
-    const {runTests} = usePlayground()
+    const {mutate, runTests} = usePlayground()
     const [testsetData, setTestsetData] = useState<Record<string, any> | null>(null)
     const [isTestsetModalOpen, setIsTestsetModalOpen] = useState(false)
+
+    const wrappedSetTestsetData = useCallback((d: SetStateAction<Record<string, any> | null>) => {
+        const data = Array.isArray(d) ? d : [d]
+
+        mutate(
+            (state) => {
+                const clonedState = cloneDeep(state)
+
+                if (!clonedState) return state
+
+                const generationMetadata = clonedState.generationData.__metadata
+                const newGenerationRows = data.map((row) => {
+                    const inputKeys = Object.keys(generationMetadata?.itemMetadata.properties)
+                    const newRow = createInputRow(inputKeys, generationMetadata?.itemMetadata)
+                    for (const key of inputKeys) {
+                        ;(newRow[key] as Enhanced<string>).value = row[key]
+                    }
+
+                    return newRow
+                })
+
+                clonedState.generationData.value = newGenerationRows
+
+                return clonedState
+            },
+            {
+                revalidate: false,
+            },
+        )
+
+        setTestsetData(d)
+    }, [])
 
     return (
         <section className="h-[48px] flex justify-between items-center gap-4 px-4 py-2 border-0 border-b border-solid border-[rgba(5,23,41,0.06)]">
@@ -33,7 +69,7 @@ const GenerationHeader = () => {
                 open={isTestsetModalOpen}
                 onCancel={() => setIsTestsetModalOpen(false)}
                 testsetData={testsetData}
-                setTestsetData={setTestsetData}
+                setTestsetData={wrappedSetTestsetData}
             />
         </section>
     )
