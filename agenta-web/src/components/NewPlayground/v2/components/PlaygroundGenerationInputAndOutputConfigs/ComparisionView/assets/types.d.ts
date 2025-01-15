afa0368dd3d0e31777d7638a4337d33a82289f96
ashrafchowdury <ashraf.chowdury09@gmail.com>
2025-01-15 20:19:05 +0600

    refactor(frontend): removed unused components from v2 directory

--- agenta-web/src/components/NewPlayground/v2/components/PlaygroundGenerationInputAndOutputConfigs/ComparisionView/assets/types.d.ts
+++
@@ -1,7 +0,0 @@
-export type PlaygroundComparisionGenerationOutputHeaderProps = {
-    className?: string
-}
-
-export type PlaygroundComparisionGenerationInputHeaderProps = {
-    className?: string
-}
