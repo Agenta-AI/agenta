7e9b186c29a3e8b0aaf9c6e1531095728d420406
Arda Erzin <ardaerzin@gmail.com>
2025-01-18 21:49:50 +0100

    feat(frontend): cleanup next.config file

--- agenta-web/next.config.js
+++ agenta-web/next.config.js
@@ -1,28 +1,9 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-    enabled: process.env.ANALYZE === "true",
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
     reactStrictMode: true,
     pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
     productionBrowserSourceMaps: true,
-    transpilePackages: [
-        "@lobehub/ui",
-        "@lobehub/icons",
-        "@lobehub/fluent-emoji",
-        "rc-util",
-        "antd",
-        "rc-pagination",
-        "rc-picker",
-        "rc-tree",
-        "rc-input",
-        "rc-table",
-        "@ant-design/icons",
-        "@ant-design/icons-svg",
-    ],
-    swcMinify: true,
     images: {
         remotePatterns: [{hostname: "fps.cdnpk.net"}],
     },
@@ -36,26 +17,6 @@ const nextConfig = {
             },
         ]
     },
-
-    webpack: (config, {webpack, isServer}) => {
-        const envs = {}
-
-        Object.keys(process.env).forEach((env) => {
-            if (env.startsWith("NEXT_PUBLIC_")) {
-                envs[env] = process.env[env]
-            }
-        })
-
-        if (!isServer) {
-            config.plugins.push(
-                new webpack.DefinePlugin({
-                    "process.env": JSON.stringify(envs),
-                }),
-            )
-        }
-
-        return config
-    },
 }
 
-module.exports = withBundleAnalyzer(nextConfig)
+module.exports = nextConfig
