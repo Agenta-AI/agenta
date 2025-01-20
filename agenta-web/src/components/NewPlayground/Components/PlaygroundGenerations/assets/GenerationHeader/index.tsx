8fccc9b0f712f6edf1c9cd34946a914b73ca57eb
Arda Erzin <ardaerzin@gmail.com>
2025-01-20 18:32:49 +0100

    enhance(frontend): improve clear button action behavior

--- agenta-web/src/components/NewPlayground/Components/PlaygroundGenerations/assets/GenerationHeader/index.tsx
+++ agenta-web/src/components/NewPlayground/Components/PlaygroundGenerations/assets/GenerationHeader/index.tsx
@@ -1,23 +1,28 @@
 import {useCallback, useState} from "react"
+
+import clsx from "clsx"
 import dynamic from "next/dynamic"
 import {Button, Typography} from "antd"
-import usePlayground from "@/components/NewPlayground/hooks/usePlayground"
 import {SetStateAction} from "jotai"
-import {createInputRow} from "@/components/NewPlayground/hooks/usePlayground/assets/inputHelpers"
-import {
+
+import TestsetDrawerButton from "../../../Drawers/TestsetDrawer"
+import RunButton from "../../../../assets/RunButton"
+import usePlayground from "../../../../hooks/usePlayground"
+import {useStyles} from "./styles"
+import {clearRuns} from "@/components/NewPlayground/hooks/usePlayground/assets/generationHelpers"
+import {createInputRow} from "../../../../hooks/usePlayground/assets/inputHelpers"
+import {getMetadataLazy} from "../../../../state"
+
+import type {PlaygroundStateData} from "../../../../hooks/usePlayground/types"
+import type {GenerationHeaderProps} from "./types"
+import type {InputType} from "@/components/NewPlayground/assets/utilities/transformer/types"
+import type {
     ArrayMetadata,
     Enhanced,
     EnhancedObjectConfig,
     ObjectMetadata,
-} from "@/components/NewPlayground/assets/utilities/genericTransformer/types"
-import {getMetadataLazy} from "@/components/NewPlayground/state"
-import {InputType} from "@/components/NewPlayground/assets/utilities/transformer/types"
-import {PlaygroundStateData} from "@/components/NewPlayground/hooks/usePlayground/types"
-import {GenerationHeaderProps} from "./types"
-import {useStyles} from "./styles"
-import clsx from "clsx"
-import TestsetDrawerButton from "../../../Drawers/TestsetDrawer"
-import RunButton from "@/components/NewPlayground/assets/RunButton"
+} from "../../../../assets/utilities/genericTransformer/types"
+
 const LoadTestsetModal = dynamic(() => import("../../../Modals/LoadTestsetModal"))
 
 const GenerationHeader = ({variantId}: GenerationHeaderProps) => {
@@ -45,69 +50,63 @@ const GenerationHeader = ({variantId}: GenerationHeaderProps) => {
     const [testsetData, setTestsetData] = useState<Record<string, any> | null>(null)
     const [isTestsetModalOpen, setIsTestsetModalOpen] = useState(false)
 
-    const wrappedSetTestsetData = useCallback((d: SetStateAction<Record<string, any> | null>) => {
-        const data = Array.isArray(d) ? d : [d]
+    const wrappedSetTestsetData = useCallback(
+        (d: SetStateAction<Record<string, any> | null>) => {
+            const data = Array.isArray(d) ? d : [d]
 
-        mutate(
-            (clonedState) => {
-                if (!clonedState) return clonedState
+            mutate(
+                (clonedState) => {
+                    if (!clonedState) return clonedState
 
-                // access the existing generation metadata to pull correct keys from testset rows
-                const generationMetadata = clonedState.generationData.inputs.__metadata
+                    // access the existing generation metadata to pull correct keys from testset rows
+                    const generationMetadata = clonedState.generationData.inputs.__metadata
 
-                // loop through the testset rows and create new generation rows from them
-                const newGenerationRows = data.map((row) => {
-                    const parentMetadata =
-                        getMetadataLazy<ArrayMetadata<ObjectMetadata>>(generationMetadata)
-                    const metadata = parentMetadata?.itemMetadata
+                    // loop through the testset rows and create new generation rows from them
+                    const newGenerationRows = data.map((row) => {
+                        const parentMetadata =
+                            getMetadataLazy<ArrayMetadata<ObjectMetadata>>(generationMetadata)
+                        const metadata = parentMetadata?.itemMetadata
 
-                    if (!metadata) return null
+                        if (!metadata) return null
 
-                    const inputKeys = Object.keys(metadata.properties)
-                    const newRow = createInputRow(inputKeys, metadata)
+                        const inputKeys = Object.keys(metadata.properties)
+                        const newRow = createInputRow(inputKeys, metadata)
 
-                    // set the values of the new generation row inputs to the values of the testset row
-                    for (const key of inputKeys) {
-                        const newRowProperty = newRow[key] as Enhanced<string>
-                        newRowProperty.value = row[key]
-                    }
+                        // set the values of the new generation row inputs to the values of the testset row
+                        for (const key of inputKeys) {
+                            const newRowProperty = newRow[key] as Enhanced<string>
+                            newRowProperty.value = row[key]
+                        }
 
-                    return newRow
-                })
+                        return newRow
+                    })
 
-                clonedState.generationData.inputs.value = newGenerationRows.filter(
-                    (row) => !!row,
-                ) as EnhancedObjectConfig<InputType<string[]>>[]
+                    clonedState.generationData.inputs.value = newGenerationRows.filter(
+                        (row) => !!row,
+                    ) as EnhancedObjectConfig<InputType<string[]>>[]
 
-                return clonedState
-            },
-            {
-                revalidate: false,
-            },
-        )
+                    return clonedState
+                },
+                {
+                    revalidate: false,
+                },
+            )
 
-        setTestsetData(d)
-    }, [])
+            setTestsetData(d)
+        },
+        [mutate],
+    )
 
     const clearGeneration = useCallback(() => {
         mutate(
             (clonedState) => {
                 if (!clonedState) return clonedState
-
-                const generationMetadata = clonedState.generationData.inputs.__metadata
-                const metadata =
-                    getMetadataLazy<ArrayMetadata<ObjectMetadata>>(generationMetadata)?.itemMetadata
-                if (!metadata) return clonedState
-
-                const inputKeys = Object.keys(metadata.properties)
-                const newRow = createInputRow(inputKeys, metadata)
-                clonedState.generationData.inputs.value = [newRow]
-
+                clearRuns(clonedState)
                 return clonedState
             },
             {revalidate: false},
         )
-    }, [])
+    }, [mutate])
 
     return (
         <section
