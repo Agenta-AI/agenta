9161eaff027ac51fa4548be34c9e7290d8b331e0
ashrafchowdury <ashraf.chowdury09@gmail.com>
2025-01-21 17:23:24 +0600

    enhance(frontend): added load testset modal in the comparison view

--- agenta-web/src/components/NewPlayground/Components/PlaygroundGenerations/assets/GenerationHeader/index.tsx
+++ agenta-web/src/components/NewPlayground/Components/PlaygroundGenerations/assets/GenerationHeader/index.tsx
@@ -1,24 +1,21 @@
-import {useCallback, useState} from "react"
-import dynamic from "next/dynamic"
+import {useCallback} from "react"
 import {Button, Typography} from "antd"
 import usePlayground from "@/components/NewPlayground/hooks/usePlayground"
-import {SetStateAction} from "jotai"
+
 import {createInputRow} from "@/components/NewPlayground/hooks/usePlayground/assets/inputHelpers"
 import {
     ArrayMetadata,
-    Enhanced,
-    EnhancedObjectConfig,
     ObjectMetadata,
 } from "@/components/NewPlayground/assets/utilities/genericTransformer/types"
 import {getMetadataLazy} from "@/components/NewPlayground/state"
-import {InputType} from "@/components/NewPlayground/assets/utilities/transformer/types"
+
 import {PlaygroundStateData} from "@/components/NewPlayground/hooks/usePlayground/types"
 import {GenerationHeaderProps} from "./types"
 import {useStyles} from "./styles"
 import clsx from "clsx"
 import TestsetDrawerButton from "../../../Drawers/TestsetDrawer"
 import RunButton from "@/components/NewPlayground/assets/RunButton"
-const LoadTestsetModal = dynamic(() => import("../../../Modals/LoadTestsetModal"))
+import LoadTestsetButton from "../../../Modals/LoadTestsetModal/assets/LoadTestsetButton"
 
 const GenerationHeader = ({variantId}: GenerationHeaderProps) => {
     const classes = useStyles()
@@ -28,7 +25,6 @@ const GenerationHeader = ({variantId}: GenerationHeaderProps) => {
             (state: PlaygroundStateData) => {
                 const inputRows = state.generationData.value
 
-                // TODO: use the results to get all the responses to save on the Testset
                 const results = inputRows.map((inputRow) =>
                     variantId ? inputRow?.__runs?.[variantId]?.__result : null,
                 )
@@ -41,53 +37,6 @@ const GenerationHeader = ({variantId}: GenerationHeaderProps) => {
             [variantId],
         ),
     })
-
-    const [testsetData, setTestsetData] = useState<Record<string, any> | null>(null)
-    const [isTestsetModalOpen, setIsTestsetModalOpen] = useState(false)
-
-    const wrappedSetTestsetData = useCallback((d: SetStateAction<Record<string, any> | null>) => {
-        const data = Array.isArray(d) ? d : [d]
-
-        mutate(
-            (clonedState) => {
-                if (!clonedState) return clonedState
-
-                // access the existing generation metadata to pull correct keys from testset rows
-                const generationMetadata = clonedState.generationData.__metadata
-
-                // loop through the testset rows and create new generation rows from them
-                const newGenerationRows = data.map((row) => {
-                    const parentMetadata =
-                        getMetadataLazy<ArrayMetadata<ObjectMetadata>>(generationMetadata)
-                    const metadata = parentMetadata?.itemMetadata
-
-                    if (!metadata) return null
-
-                    const inputKeys = Object.keys(metadata.properties)
-                    const newRow = createInputRow(inputKeys, metadata)
-
-                    // set the values of the new generation row inputs to the values of the testset row
-                    for (const key of inputKeys) {
-                        const newRowProperty = newRow[key] as Enhanced<string>
-                        newRowProperty.value = row[key]
-                    }
-
-                    return newRow
-                })
-
-                clonedState.generationData.value = newGenerationRows.filter(
-                    (row) => !!row,
-                ) as EnhancedObjectConfig<InputType<string[]>>[]
-
-                return clonedState
-            },
-            {
-                revalidate: false,
-            },
-        )
-
-        setTestsetData(d)
-    }, [])
 
     const clearGeneration = useCallback(() => {
         mutate(
@@ -121,9 +70,8 @@ const GenerationHeader = ({variantId}: GenerationHeaderProps) => {
                 <Button size="small" onClick={clearGeneration} disabled={isRunning}>
                     Clear
                 </Button>
-                <Button size="small" onClick={() => setIsTestsetModalOpen(true)}>
-                    Load Test set
-                </Button>
+
+                <LoadTestsetButton label="Load Test set" />
 
                 <TestsetDrawerButton
                     label="Add all to test set"
@@ -140,13 +88,6 @@ const GenerationHeader = ({variantId}: GenerationHeaderProps) => {
                     disabled={isRunning}
                 />
             </div>
-
-            <LoadTestsetModal
-                open={isTestsetModalOpen}
-                onCancel={() => setIsTestsetModalOpen(false)}
-                testsetData={testsetData}
-                setTestsetData={wrappedSetTestsetData}
-            />
         </section>
     )
 }
