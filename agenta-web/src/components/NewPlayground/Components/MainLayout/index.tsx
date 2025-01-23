63e495056d5b68386608843b676fda9fc65f5eeb
ashrafchowdury <ashraf.chowdury09@gmail.com>
2025-01-23 21:58:21 +0600

    fix(frontend): layout size

--- agenta-web/src/components/NewPlayground/Components/MainLayout/index.tsx
+++ agenta-web/src/components/NewPlayground/Components/MainLayout/index.tsx
@@ -116,7 +116,7 @@ const PlaygroundMainView = ({className, ...divProps}: BaseContainerProps) => {
             <div className="w-full max-h-full h-full grow relative overflow-hidden">
                 <Splitter className="h-full" layout={isComparisonView ? "vertical" : "horizontal"}>
                     <SplitterPanel
-                        defaultSize={isComparisonView ? "40%" : "30%"}
+                        defaultSize="50%"
                         min="20%"
                         max="70%"
                         className="!h-full"
@@ -164,7 +164,7 @@ const PlaygroundMainView = ({className, ...divProps}: BaseContainerProps) => {
                             "!overflow-y-hidden flex flex-col": isComparisonView,
                         })}
                         collapsible
-                        defaultSize={isComparisonView ? "60%" : "70%"}
+                        defaultSize="50%"
                     >
                         {isComparisonView && <GenerationComparisonHeader />}
 
