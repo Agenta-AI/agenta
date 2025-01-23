f470b177227fd57ea632ebab28d0bfcb0dc2ca99
Arda Erzin <ardaerzin@gmail.com>
2025-01-23 17:49:46 +0100

    enhance(frontend): set initial generation message role to user

--- agenta-web/src/components/NewPlayground/Components/PlaygroundVariantPropertyControl/assets/PromptMessageContent/index.tsx
+++ agenta-web/src/components/NewPlayground/Components/PlaygroundVariantPropertyControl/assets/PromptMessageContent/index.tsx
@@ -43,10 +43,10 @@ const PromptMessageContent = ({
                 handleLocalValueChange(value.textContent)
             }}
             // className={clsx([
-                // "border-0",
-                // "focus:ring-0",
-                // {"bg-[#f5f7fa] focus:bg-[#f5f7fa] hover:bg-[#f5f7fa]": isGenerationChatView},
-                // className,
+            // "border-0",
+            // "focus:ring-0",
+            // {"bg-[#f5f7fa] focus:bg-[#f5f7fa] hover:bg-[#f5f7fa]": isGenerationChatView},
+            // className,
             // ])}
             disabled={disabled}
             showBorder={false}
