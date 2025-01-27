1761c399f514dde65170669935aa2cfec5a3ceec
Arda Erzin <ardaerzin@gmail.com>
2025-01-27 15:52:12 +0300

    fix(frontend): fixes auth provider on initial load

--- agenta-web/src/lib/helpers/auth/AuthProvider.tsx
+++ agenta-web/src/lib/helpers/auth/AuthProvider.tsx
@@ -1,16 +1,22 @@
-import {useEffect, useCallback} from "react"
+import {useEffect, useCallback, useState} from "react"
 import SuperTokensReact, {SuperTokensWrapper} from "supertokens-auth-react"
 import {AuthProviderType} from "./types"
 import {isDemo} from "../utils"
 import {dynamicConfig} from "../dynamic"
-;(async () => {
-    if (typeof window !== "undefined" && isDemo()) {
-        const {frontendConfig} = await dynamicConfig("frontendConfig")
-        SuperTokensReact.init(frontendConfig())
-    }
-})()
 
 const AuthProvider: AuthProviderType = ({children, pageProps}) => {
+    const [isInitialized, setIsInitialized] = useState(false)
+    useEffect(() => {
+        const initSuperTokens = async () => {
+            const {frontendConfig} = await dynamicConfig("frontendConfig")
+            SuperTokensReact.init(frontendConfig())
+            setIsInitialized(true)
+        }
+        if (typeof window !== "undefined" && isDemo()) {
+            initSuperTokens()
+        }
+    }, [])
+
     const doRefresh = useCallback(async () => {
         if (isDemo() && pageProps.fromSupertokens === "needs-refresh") {
             const session = await import("supertokens-auth-react/recipe/session")
@@ -27,11 +33,13 @@ const AuthProvider: AuthProviderType = ({children, pageProps}) => {
         doRefresh()
     }, [doRefresh])
 
-    if (isDemo() && pageProps.fromSupertokens === "needs-refresh") {
+    if (!isDemo()) {
+        return <>{children}</>
+    } else if (isInitialized) {
+        return <SuperTokensWrapper>{children}</SuperTokensWrapper>
+    } else {
         return null
     }
-
-    return isDemo() ? <SuperTokensWrapper>{children}</SuperTokensWrapper> : <>{children}</>
 }
 
 export default AuthProvider
