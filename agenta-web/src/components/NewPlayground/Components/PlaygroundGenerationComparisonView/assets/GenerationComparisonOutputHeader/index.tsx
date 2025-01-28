6842eaf6b0ca6cc0137aca1368b986febcf21d99
ashrafchowdury <ashraf.chowdury09@gmail.com>
2025-01-28 12:53:12 +0600

    enhance(frontend): improved GenerationComparisonOutputHeader component to avoid unnecessary re-renders

--- agenta-web/src/components/NewPlayground/Components/PlaygroundGenerationComparisonView/assets/GenerationComparisonOutputHeader/index.tsx
+++ agenta-web/src/components/NewPlayground/Components/PlaygroundGenerationComparisonView/assets/GenerationComparisonOutputHeader/index.tsx
@@ -1,27 +1,37 @@
+import {memo, useCallback} from "react"
 import {Tag, Typography} from "antd"
 import clsx from "clsx"
 import {useStyles} from "../styles"
 import {GenerationComparisonOutputHeaderProps} from "./types"
 import Version from "@/components/NewPlayground/assets/Version"
 import usePlayground from "@/components/NewPlayground/hooks/usePlayground"
+import {PlaygroundStateData} from "@/components/NewPlayground/hooks/usePlayground/types"
 
 const GenerationComparisonOutputHeader: React.FC<GenerationComparisonOutputHeaderProps> = ({
     className,
     variantId,
     indexName,
 }) => {
-    const {variant} = usePlayground({variantId})
+    const {variantName, revision} = usePlayground({
+        stateSelector: useCallback(
+            (state: PlaygroundStateData) => {
+                const variant = state.variants.find((variant) => variant.id === variantId)
+                return {variantName: variant?.variantName, revision: variant?.revision}
+            },
+            [variantId],
+        ),
+    })
     const classes = useStyles()
 
     return (
         <div className={clsx(classes.title, className)}>
             <Typography>Output {indexName}</Typography>
             <Tag color="default" className="bg-[rgba(5,23,41,0.06)]" bordered={false}>
-                {variant?.variantName}
+                {variantName}
             </Tag>
-            <Version revision={variant?.revision as number} />
+            <Version revision={revision as number} />
         </div>
     )
 }
 
-export default GenerationComparisonOutputHeader
+export default memo(GenerationComparisonOutputHeader)
