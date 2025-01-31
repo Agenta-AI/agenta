49de6004af672f0511a25ecec9f481b5864fee37
Mahmoud Mabrouk <mahmoud@agenta.ai>
2025-01-31 14:02:34 +0100

    fix port

--- agenta-backend/agenta_backend/services/app_manager.py
+++ agenta-backend/agenta_backend/services/app_manager.py
@@ -91,16 +91,16 @@ async def start_variant(
         domain_name = os.environ.get("DOMAIN_NAME")
         if domain_name is None or domain_name == "http://localhost":
             # in the case of agenta running locally, the containers can access the host machine via this address
-            domain_name = (
-                f"http://host.docker.internal"
-            )
+            domain_name = f"http://host.docker.internal"
 
         env_vars = {} if env_vars is None else env_vars  # type: ignore
         env_vars.update(
             {
                 "AGENTA_BASE_ID": str(db_app_variant.base_id),
                 "AGENTA_APP_ID": str(db_app_variant.app_id),
-                "AGENTA_HOST": domain_name if os.environ.get("AGENTA_PORT", "80") == "80" else f"{domain_name}:{os.environ.get('AGENTA_PORT', '80')}",
+                "AGENTA_HOST": domain_name
+                if os.environ.get("AGENTA_PORT", "80") == "80"
+                else f"{domain_name}:{os.environ.get('AGENTA_PORT', '80')}",
                 "AGENTA_RUNTIME": "true",
             }
         )
