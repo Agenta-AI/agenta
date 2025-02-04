d0850460724e19c776df493c8bfd6bc80e8131ee
Arda Erzin <ardaerzin@gmail.com>
2025-02-04 16:24:30 +0100

    add logs

--- agenta-web/src/components/pages/overview/deployments/DeploymentDrawer/index.tsx
+++ agenta-web/src/components/pages/overview/deployments/DeploymentDrawer/index.tsx
@@ -53,6 +53,7 @@ const DeploymentDrawer = ({
     const appId = router.query.app_id as string
     const {currentApp} = useAppsData()
     const [selectedLang, setSelectedLang] = useState("python")
+    console.log("variantId", selectedEnvironment?.deployed_app_variant_id)
     const {data: uri} = useURI(appId, selectedEnvironment?.deployed_app_variant_id)
     const [variant] = useState<Variant | null>(
         variants.find(
