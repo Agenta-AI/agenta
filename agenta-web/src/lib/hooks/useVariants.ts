4f5fa2a94f0f1ddae00772bda92723a86c1c9236
Arda Erzin <ardaerzin@gmail.com>
2025-02-04 19:36:56 +0100

    fix(frontend): fix useVariants hook to use legacy version for custom apps

--- agenta-web/src/lib/hooks/useVariants.ts
+++ agenta-web/src/lib/hooks/useVariants.ts
@@ -2,4 +2,8 @@ import {useLegacyVariant, useLegacyVariants} from "./useLegacyVariant"
 import useStatelessVariants from "./useStatelessVariant"
 
 export const useVariants = (app) =>
-    !app ? () => {} : app.app_type.includes("old") ? useLegacyVariants : useStatelessVariants
+    !app
+        ? () => {}
+        : app.app_type.includes("old") || app.app_type.includes("custom")
+          ? useLegacyVariants
+          : useStatelessVariants
