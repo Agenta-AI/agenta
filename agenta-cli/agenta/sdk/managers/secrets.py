cd0198abb8c802ae98e6e9762c42a4f1d310612a
Abram <israelvictory87@gmail.com>
2025-02-07 15:34:20 +0100

    feat (sdk<>managers): implement build_llm_request method for authenticating standard & custom models

--- agenta-cli/agenta/sdk/managers/secrets.py
+++ agenta-cli/agenta/sdk/managers/secrets.py
@@ -36,3 +36,54 @@ class SecretsManager:
                 return secret["data"]["key"]
 
         return None
+
+    @staticmethod
+    def build_llm_request(model: str) -> Dict:
+        """
+        Builds the LLM request with appropriate kwargs based on the custom provider
+
+        Args:
+            model (str): The name of the model
+
+        Returns:
+            Dict: A dictionary containing all parameters needed for litellm.completion
+        """
+
+        # Check if model exists in mapping
+        provider_to_use = model_to_provider_mapping.get(model)
+        if not provider_to_use:
+            return None
+
+        provider_name = provider_to_use.lower().replace(" ", "")
+        llm_request_kwargs = {}
+
+        # Get secrets from route
+        secrets = SecretsManager.get_from_route()
+        if not secrets:
+            return None
+
+        for secret in secrets:
+            if secret.get("kind") == "provider_key":
+                secret_data = secret.get("data", {})
+                provider_info = secret_data.get("provider", {})
+
+                provider_slug = (
+                    provider_info.get("slug", secret_data.get("kind", ""))
+                    .lower()
+                    .replace(" ", "")
+                )  # converts the provider name from 'Open AI' to 'openai'
+
+                provider_extras = provider_info.get("extras", {})
+
+                if provider_slug == provider_name:
+                    # Extract API key if present
+                    # (for standard models -- openai/anthropic/gemini, etc)
+                    if "key" in provider_info:
+                        llm_request_kwargs["api_key"] = provider_info["key"]
+
+                    # Extract credentials for custom providers
+                    elif provider_extras:
+                        llm_request_kwargs.update(provider_extras)
+                break
+
+        return llm_request_kwargs
