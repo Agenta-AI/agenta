ec4eff132e296cf01bdabacbac8ac7396643679e
GitHub CI <team@agenta.ai>
2025-11-12 03:25:29 +0000

    AGE-3383-/-improve-subscription-banner-text

--- web/ee/src/components/pages/settings/Billing/Modals/PricingModal/assets/SubscriptionPlanDetails/index.tsx
+++ web/ee/src/components/pages/settings/Billing/Modals/PricingModal/assets/SubscriptionPlanDetails/index.tsx
@@ -1,20 +1,26 @@
-import dayjs from "dayjs"
+import dayjs from "@/oss/lib/helpers/dateTimeHelper/dayjs"
+import duration from "dayjs/plugin/duration"
+import relativeTime from "dayjs/plugin/relativeTime"
 
 import {SubscriptionType} from "@/oss/services/billing/types"
 
+dayjs.extend(duration)
+dayjs.extend(relativeTime)
+
 const SubscriptionPlanDetails = ({subscription}: {subscription: SubscriptionType}) => {
+    if (!subscription) return null
+
+    const end = dayjs.unix(subscription.period_end)
+    const now = dayjs()
+
+    const diffHumanized = dayjs.duration(end.diff(now)).humanize(true)
+
+    const trialText = `trial ends ${diffHumanized}`
+
     return (
         <>
             {subscription?.plan?.split("_")[2]}{" "}
-            <span className="lowercase">
-                {subscription.free_trial
-                    ? `trial ends in ${dayjs.unix(subscription.period_end).diff(dayjs(), "day")} ${
-                          dayjs.unix(subscription.period_end).diff(dayjs(), "day") === 1
-                              ? "day"
-                              : "days"
-                      }`
-                    : ""}
-            </span>
+            <span className="lowercase">{subscription.free_trial ? trialText : ""}</span>
         </>
     )
 }
