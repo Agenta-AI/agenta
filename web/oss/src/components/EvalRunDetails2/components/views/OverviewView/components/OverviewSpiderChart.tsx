e64ea76a8e6daa947e57adbdecdd7f9459dd30f8
Arda Erzin <ardaerzin@gmail.com>
2025-12-04 10:39:36 +0300

    spider chart alignments

--- web/oss/src/components/EvalRunDetails2/components/views/OverviewView/components/OverviewSpiderChart.tsx
+++ web/oss/src/components/EvalRunDetails2/components/views/OverviewView/components/OverviewSpiderChart.tsx
@@ -239,16 +239,12 @@ const OverviewSpiderChart = ({runIds}: OverviewSpiderChartProps) => {
     }
 
     return (
-        <div className="flex h-full w-full items-center justify-end">
-            <div className="w-full max-w-[520px]" style={{minHeight: 320, aspectRatio: "1 / 1"}}>
-                <EvaluatorMetricsSpiderChart
-                    className="h-full w-full"
-                    metrics={chartState.metrics as any}
-                    series={chartState.series as any}
-                    maxScore={chartState.maxScore}
-                />
-            </div>
-        </div>
+        <EvaluatorMetricsSpiderChart
+            className="h-full w-full"
+            metrics={chartState.metrics as any}
+            series={chartState.series as any}
+            maxScore={chartState.maxScore}
+        />
     )
 }
 
