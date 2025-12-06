244eb5cc0373a7fac9dd5e612da6bea305d16142
Juan Pablo Vega <jp@agenta.ai>
2025-12-06 17:19:30 -0300

    fix migrations tree

--- api/ee/databases/postgres/migrations/core/versions/cfa14a847972_drop_nodes.py
+++ api/ee/databases/postgres/migrations/core/versions/cfa14a847972_drop_nodes.py
@@ -12,7 +12,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "cfa14a847972"
-down_revision: Union[str, None] = "a1b2c3d4e5f6"
+down_revision: Union[str, None] = "b2c3d4e5f6a1"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
