3a548df1bc70e8aa6d0d85c0318d0760239f2601
Arda Erzin <ardaerzin@gmail.com>
2025-12-06 23:28:36 +0300

    refactor useTableManager to use action configs instead of rendering delete/export buttons
    - Replace deleteButton ReactNode with TableDeleteConfig object
    - Replace renderExportButton callback with TableExportConfig object
    - Remove settingsDropdownDelete in favor of unified deleteAction config
    - Move button rendering responsibility to InfiniteVirtualTableFeatureShell
    - Remove direct Button, Tooltip, and Trash icon imports from useTableManager
    - Update shellProps to pass deleteAction and exportAction instea

--- web/oss/src/pages/w/[workspace_id]/p/[project_id]/testsets/index.tsx
+++ web/oss/src/pages/w/[workspace_id]/p/[project_id]/testsets/index.tsx
@@ -173,7 +173,6 @@ const Testset = () => {
                 title={headerTitle}
                 filters={filtersNode}
                 primaryActions={createButton}
-                secondaryActions={table.deleteButton}
                 tableClassName="agenta-testsets-table"
                 className="flex-1 min-h-0"
                 exportFilename="testsets.csv"
