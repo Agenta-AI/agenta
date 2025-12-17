810c15774542e6382cf091d9212d823dff615a9f
Arda Erzin <ardaerzin@gmail.com>
2025-12-17 18:46:33 +0100

    Update dependencies and remove jotai-devtools
    - Upgrade jotai from 2.15.2 to 2.16.0
    - Remove jotai-devtools package and related imports/styles
    - Upgrade @tanstack/react-query and @tanstack/query-core from 5.90.11 to 5.90.12
    - Upgrade shiki from 3.19.0 to 3.20.0
    - Upgrade ESLint and related packages (@eslint/eslintrc, @eslint/js, @typescript-eslint/eslint-plugin, @typescript-eslint/parser, eslint-config-prettier, eslint-plugin-prettier, eslint-plugin-react-hooks, typescript-eslint)
    - Upgrade prettier

--- web/oss/src/pages/_app.tsx
+++ web/oss/src/pages/_app.tsx
@@ -4,7 +4,6 @@ import "@/oss/assets/custom-resize-handle.css"
 import "react-resizable/css/styles.css"
 import "@ag-grid-community/styles/ag-grid.css"
 import "@ag-grid-community/styles/ag-theme-alpine.css"
-import "jotai-devtools/styles.css"
 
 import AppPage from "@/oss/components/pages/_app"
 
