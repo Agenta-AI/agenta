59ccaa93d5cea29296d006d5e4c358a2de76f99a
Arda Erzin <ardaerzin@gmail.com>
2025-12-18 21:24:39 +0100

    Remove unused Ant Design v5 patch and global CSS imports from EE _app.tsx

--- web/ee/src/pages/_app.tsx
+++ web/ee/src/pages/_app.tsx
@@ -1,5 +1,3 @@
-import "@ant-design/v5-patch-for-react-19"
-import "@/oss/styles/globals.css"
 import "@/oss/assets/custom-resize-handle.css"
 import "react-resizable/css/styles.css"
 import "jotai-devtools/styles.css"
