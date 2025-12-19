59f4797ae8a4ee55932d3b079e4dd392995b3eae
Juan Pablo Vega <jp@agenta.ai>
2025-12-20 00:24:14 +0100

    fix typos in init

--- examples/python/evaluators/numpy/__init__.py
+++ examples/python/evaluators/numpy/__init__.py
@@ -4,10 +4,10 @@ Simple tests to verify NumPy availability and basic functionality.
 Requires: pip install numpy
 """
 
-from .numpy_available import evaluate as numpy_available
-from .numpy_dummy import evaluate as array_sum_match
+from .dependency_check import evaluate as numpy_available
+from .exact_match import evaluate as array_sum_match
 
 __all__ = [
-    "numpy_available",
-    "numpy_dummy",
+    "dependency_check",
+    "exact_match",
 ]
