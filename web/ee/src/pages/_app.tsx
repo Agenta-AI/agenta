e86f9bb56b9dcf15622daff4e52b84879843baf2
Arda Erzin <ardaerzin@gmail.com>
2025-12-23 12:16:18 +0100

    Remove unused jotai-devtools styles import from EE app entry point

--- web/ee/src/pages/_app.tsx
+++ web/ee/src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import "@/oss/assets/custom-resize-handle.css"
 import "@/oss/styles/globals.css"
 import "react-resizable/css/styles.css"
-import "jotai-devtools/styles.css"
 
 import AppPage from "@/oss/components/pages/_app"
 
