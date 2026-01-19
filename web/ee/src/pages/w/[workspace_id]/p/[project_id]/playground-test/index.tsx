7cd3b895bedcb5c6fbf9ee3273c7a6d22feec986
Arda Erzin <ardaerzin@gmail.com>
2026-01-20 00:42:05 +0100

    Add PlaygroundTest component with OSS/EE page routing
    - Add PlaygroundTest component wrapping @agenta/playground with OSS-specific providers
    - Inject EntityDrillInView, SharedGenerationResultUtils, LoadTestsetModal, CommitVariantChangesButton via PlaygroundUIProvider
    - Add OSSdrillInUIProvider and EntitySelectorProvider context wrappers
    - Create OSS page route at /playground-test
    - Create EE page route importing OSS implementation
    - Re-export all @agenta/playground components and types for convenience

---
+++ web/ee/src/pages/w/[workspace_id]/p/[project_id]/playground-test/index.tsx
@@ -0,0 +1,7 @@
+import PlaygroundTestPage from "@agenta/oss/src/pages/w/[workspace_id]/p/[project_id]/playground-test"
+
+// const PlaygroundTestPage = () => {
+//     return <PlaygroundTest />
+// }
+
+export default PlaygroundTestPage
---
+++ web/oss/src/pages/w/[workspace_id]/p/[project_id]/playground-test/index.tsx
@@ -0,0 +1,7 @@
+import PlaygroundTest from "@/oss/components/PlaygroundTest"
+
+const PlaygroundTestPage = () => {
+    return <PlaygroundTest />
+}
+
+export default PlaygroundTestPage
