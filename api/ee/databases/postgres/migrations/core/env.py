fb7c6400e6f84b0322f490c8dbce8fb9dc8d90f5
Juan Pablo Vega <jp@agenta.ai>
2026-02-10 14:33:27 +0100

    missing auto fixes

--- api/ee/databases/postgres/migrations/core/env.py
+++ api/ee/databases/postgres/migrations/core/env.py
@@ -1,10 +1,8 @@
-import os
 import asyncio
 from logging.config import fileConfig
 
-from sqlalchemy import pool
 from sqlalchemy.engine import Connection, create_engine
-from sqlalchemy.ext.asyncio import async_engine_from_config, create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine
 
 from alembic import context
 
--- api/ee/databases/postgres/migrations/tracing/env.py
+++ api/ee/databases/postgres/migrations/tracing/env.py
@@ -1,10 +1,9 @@
-import os
 import asyncio
 from logging.config import fileConfig
 
 from sqlalchemy import pool
-from sqlalchemy.engine import Connection, create_engine
-from sqlalchemy.ext.asyncio import async_engine_from_config, create_async_engine
+from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
