24686f6908098a5d2b1306f38c639fc5b0d60ab2
Mahmoud Mabrouk <mahmoud@agenta.ai>
2026-03-11 22:58:44 +0100

    fix(frontend): rename automation create button

--- web/oss/src/components/pages/settings/Automations/Automations.tsx
+++ web/oss/src/components/pages/settings/Automations/Automations.tsx
@@ -205,7 +205,7 @@ const Automations: React.FC = () => {
                     Automations
                 </Title>
                 <Button type="primary" size="small" icon={<PlusOutlined />} onClick={handleCreate}>
-                    Create Automation
+                    New Automation
                 </Button>
             </div>
 
