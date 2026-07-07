a3e7e4dc3535ac7ba4f9e11f1f5dcabd947caf27
ashrafchowdury <ashraf.chowdury09@gmail.com>
2026-07-07 19:48:23 +0600

    fix(useModelHarness): increase providerDropdownWidth for better UI layout

--- web/packages/agenta-entity-ui/src/DrillInView/SchemaControls/agentTemplate/useModelHarness.tsx
+++ web/packages/agenta-entity-ui/src/DrillInView/SchemaControls/agentTemplate/useModelHarness.tsx
@@ -398,7 +398,7 @@ export function useModelHarness({
         hasInspectModels ? (
             <SelectLLMProviderBase
                 showGroup
-                providerDropdownWidth={425}
+                providerDropdownWidth={580}
                 options={modelGroups}
                 value={modelId ?? undefined}
                 onChange={(v, option) => {
